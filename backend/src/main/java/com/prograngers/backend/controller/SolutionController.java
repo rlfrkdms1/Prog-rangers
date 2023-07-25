7bd57aff837dc41180122b9403641bea00032733
jidam <jidamine87593@gmail.com>
2023-07-25 14:21:19 +0900

    [#1]feat: SolutionController 추가

---
+++ backend/src/main/java/com/prograngers/backend/controller/SolutionController.java
@@ -0,0 +1,36 @@
+package com.prograngers.backend.controller;
+
+import com.prograngers.backend.entity.Solution;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/solutions")
+public class SolutionController {
+
+    private final SolutionService solutionService;
+
+    @PostMapping("/new-form")
+    public ResponseEntity<?> newForm(@RequestBody @Validated SolutionRequest solutionRequest, BindingResult bindingResult){
+
+        if (bindingResult.hasErrors()){
+            return ResponseEntity.badRequest().body(new ErrorResponse());
+        }
+
+        // 아직 API 설계 X
+
+        Solution solution = solutionService.save();
+
+        return ResponseEntity.ok().body(new SolutionResponse());
+    }
+
+
+
+}
