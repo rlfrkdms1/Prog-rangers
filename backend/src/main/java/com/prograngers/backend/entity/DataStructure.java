35179fa724dfceb6201f7f7e8f1a5a2a125a27f9
jidam <jidamine87593@gmail.com>
2023-07-25 14:00:52 +0900

    [#1]feat: DataStructure Entity 추가

---
+++ backend/src/main/java/com/prograngers/backend/entity/DataStructure.java
@@ -0,0 +1,22 @@
+package com.prograngers.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DataStructure {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+}
