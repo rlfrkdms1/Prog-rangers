e8c0483f9cb55bb10b91617ae2be7aacd2c5d60d
jidam <jidamine87593@gmail.com>
2023-07-25 13:59:28 +0900

    [#1]feat: Algorithm Entity 추가

---
+++ backend/src/main/java/com/prograngers/backend/entity/Algorithm.java
@@ -0,0 +1,21 @@
+package com.prograngers.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Algorithm {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+}
