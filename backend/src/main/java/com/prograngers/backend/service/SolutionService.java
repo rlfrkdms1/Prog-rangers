2d7b0d7a6ce77a624cce237d5339dbfa8ff1dc04
jidam <jidamine87593@gmail.com>
2023-07-25 17:11:31 +0900

    [#1]feat: SolutionService 정상로직 추가

--- backend/src/main/java/com/prograngers/backend/service/SolutionService.java
+++ backend/src/main/java/com/prograngers/backend/service/SolutionService.java
@@ -1,5 +1,6 @@
 package com.prograngers.backend.service;
 
+import com.prograngers.backend.entity.Member;
 import com.prograngers.backend.entity.Solution;
 import com.prograngers.backend.repository.SolutionRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -16,21 +18,38 @@ public class SolutionService {
 
     private final SolutionRepository solutionRepository;
 
-    public Solution  save(){
-        return new Solution();
+    public Solution  save(Solution solution){
+        Solution saved = solutionRepository.save(solution);
+        return saved;
     }
 
-    public Solution update(){
-        return new Solution();
+    public Solution update(Solution solution){
+        Optional<Solution> optionalTarget = solutionRepository.findById(solution.getId());
+        Solution target = optionalTarget.get();
+        if (target==null){
+            return null;
+        } else {
+            Solution updated = solutionRepository.save(solution);
+            return updated;
+        }
     }
 
-    public Solution delete(){
-        return new Solution();
+    public Solution delete(Solution solution){
+        Optional<Solution> optionalTarget = solutionRepository.findById(solution.getId());
+        Solution target = optionalTarget.get();
+        if (target==null){
+            return null;
+        } else {
+            solutionRepository.delete(solution);
+            return target;
+        }
     }
 
-    public List<Solution> index(Long id){
-        List<Solution> list = new ArrayList<>();
-        return list;
+    public List<Solution> index(Member member){
+
+        List<Solution> solutionList = (List<Solution>) solutionRepository.findAllByMember(member);
+
+        return solutionList;
     }
 
 }
