03655512f559be9760446eb9a2531c5f35288574
jidam <jidamine87593@gmail.com>
2023-07-29 12:51:57 +0900

    test: SolutionFixture 추가
    1. SolutionFixture 추가
    
    Related to : #1

---
+++ backend/src/test/java/com/prograngers/backend/SolutionFixture.java
@@ -0,0 +1,60 @@
+package com.prograngers.backend;
+
+import com.prograngers.backend.entity.*;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+public enum SolutionFixture {
+    SOLUTION(
+            1L,
+            new Problem(null,"문제제목","https://www.acmicpc.net/problem/1000",Judges.백준),
+            new Member(null,"이름","닉네임","email@naver.com",null,null,"password","01012345678"),
+            "풀이제목",
+            true,
+            "코드",
+            "설명",
+            null,
+            null,
+            LocalDate.now(),
+            new Algorithm(null,Algorithms.BFS),
+            new DataStructure(null, DataStructures.ARRAY),
+            Levels.ONE
+            );
+
+    private final Long id;
+    private final Problem problem;
+
+    private final Member member;
+
+    private final String title;
+
+    private final boolean isPublic;
+
+    private final String code;
+
+    private final String description;
+
+    private final List<Like> likes = new ArrayList<>();
+
+    private final Integer scraps;
+
+    private final Solution scrapId;
+
+    private final LocalDate date;
+
+    private final Algorithm algorithm;
+
+    private final DataStructure dataStructure;
+
+    private final Levels level;
+
+    public Solution getSolution(){
+        return new Solution(id,problem,member,title,isPublic,code,description,likes,scraps,scrapId,date,algorithm,dataStructure,level);
+    }
+}
