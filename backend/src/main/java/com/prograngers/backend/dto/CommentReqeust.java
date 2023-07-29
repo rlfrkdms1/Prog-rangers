fa17fd91a9fcb784f833b506ea3734791464155c
jidam <jidamine87593@gmail.com>
2023-07-29 21:13:51 +0900

    feat: CommentRequest 추가
    1. 댓글 작성을 위한 dto인 CommentReqeust 추가
    
    Related to : #6

---
+++ backend/src/main/java/com/prograngers/backend/dto/CommentReqeust.java
@@ -0,0 +1,21 @@
+package com.prograngers.backend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentReqeust {
+
+    @NotBlank(message="댓글 작성자의 닉네임이 없습니다")
+    String nickname;
+    Integer orderParent;
+    @NotBlank(message="댓글 내용이 없습니다")
+    String content;
+
+}
