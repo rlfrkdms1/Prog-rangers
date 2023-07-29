b72c6ee26dc61fdc952bb7f643ba849c160256f0
jidam <jidamine87593@gmail.com>
2023-07-29 17:43:37 +0900

    feat: Review 엔티티 추가
    1. Review Entity 추가
    
    Related to : #6

---
+++ backend/src/main/java/com/prograngers/backend/entity/Review.java
@@ -0,0 +1,54 @@
+package com.prograngers.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name="solution_id")
+    private Solution solution;
+
+    private Integer codeLineNumber;
+
+    private Integer orderParent;
+
+    private Integer orderChild;
+
+    private Long parentId;
+
+    private String mention;
+
+    private String content;
+
+    private LocalDate date;
+
+    public void updateMention(String mention) {
+        if (mention != null) {
+            this.mention = mention;
+        }
+    }
+
+        public void updateContent(String content){
+            if (content!=null){
+                this.content = content;
+        }
+    }
+
+}
