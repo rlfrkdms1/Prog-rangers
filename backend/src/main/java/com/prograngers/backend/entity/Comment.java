7fe9dcd6148e7a6d98dfed1d4998334c91d3dbdb
jidam <jidamine87593@gmail.com>
2023-07-29 17:34:52 +0900

    feat: Comment 엔티티 추가
    1. Comment Entity 추가
    
    Related to : #6

---
+++ backend/src/main/java/com/prograngers/backend/entity/Comment.java
@@ -0,0 +1,58 @@
+package com.prograngers.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Comment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="solution_id")
+
+    private Solution solution;
+    private Integer orderParent;
+    private Integer orderChild;
+    private String mention;
+
+    private String content;
+
+    private LocalDate date;
+
+    private Long parentId;
+
+    private Integer groupNumber;
+
+    private boolean fixed;
+
+    public void updateMention(String mention){
+        if (mention!=null){
+            this.mention = mention;
+        }
+    }
+
+    public void updateContent(String content){
+        if (content!=null){
+            this.content = content;
+        }
+    }
+
+    public void updateFixed(boolean fixed){
+        this.fixed = fixed;
+    }
+
+}
