73695c5cd1fa99a438dd117a7438112d2ba9bfef
jidam <jidamine87593@gmail.com>
2023-07-29 18:05:01 +0900

    feat: CommentRepository 추가
    1. CommentRepository 추가
    
    Related to : #6

--- backend/src/main/java/com/prograngers/backend/controller/SolutionController.java
+++ backend/src/main/java/com/prograngers/backend/controller/SolutionController.java
@@ -4,6 +4,7 @@ import com.prograngers.backend.dto.ScarpSolutionRequest;
 import com.prograngers.backend.dto.SolutionPatchRequest;
 import com.prograngers.backend.dto.SolutionRequest;
 import com.prograngers.backend.dto.SolutionUpdateForm;
+import com.prograngers.backend.entity.Comment;
 import com.prograngers.backend.entity.Review;
 import com.prograngers.backend.entity.Solution;
 import com.prograngers.backend.service.SolutionService;
---
+++ backend/src/main/java/com/prograngers/backend/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.prograngers.backend.repository;
+
+import com.prograngers.backend.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+
+}
