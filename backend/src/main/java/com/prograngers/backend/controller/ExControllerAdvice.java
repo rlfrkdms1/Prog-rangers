542e743463531bd526a0fa0012b8bb8a99c19387
jidam <jidamine87593@gmail.com>
2023-07-30 12:35:00 +0900

    refactor: ExControllerAdvice에서 메소드 명 변경
    1. 메소드 명으로 예외 타입을 알 수 있도록 변경했습니다.
    2. ex로 줄여쓰지 않고 exception으로 변경했습니다
    
    Related to : #1

--- backend/src/main/java/com/prograngers/backend/controller/ExControllerAdvice.java
+++ backend/src/main/java/com/prograngers/backend/controller/ExControllerAdvice.java
@@ -35,14 +35,14 @@ public class ExControllerAdvice {
 
      // 요청한 PathVariable에 대한 데이터가 db에 없는 경우 에러메세지와 함께 NoSuchElementException 던짐
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> noSuchElementEx(NotFoundException exception){
+    public ResponseEntity<ErrorResponse> notFoundException(NotFoundException exception){
         String message = exception.getMessage();
         ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode(), message);
         return new ResponseEntity(errorResponse,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> enumTypeEx(EnumTypeException exception){
+    public ResponseEntity<ErrorResponse> enumTypeException(EnumTypeException exception){
         String message = exception.getMessage();
         ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode(), message);
         return new ResponseEntity(errorResponse,HttpStatus.BAD_REQUEST);
