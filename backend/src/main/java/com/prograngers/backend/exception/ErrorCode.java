8a731cf950801d3281a4b6c00b049593dce3cbe5
jidam <jidamine87593@gmail.com>
2023-07-30 13:34:52 +0900

    refactor: SOLUTION_NOT_FOUND로 rename
    1. SOLUTION_NOT_EXISTS -> SOLUTION_NOT_FOUND로 rename
    
    Related to : #1

--- backend/src/main/java/com/prograngers/backend/exception/ErrorCode.java
+++ backend/src/main/java/com/prograngers/backend/exception/ErrorCode.java
@@ -3,11 +3,10 @@ package com.prograngers.backend.exception;
 public enum ErrorCode {
 
     INVALID_SOLUTION_BODY,
-    SOLUTION_NOT_EXISTS,
+    SOLUTION_NOT_FOUND,
+    PROBLEM_LINK_NOT_FOUND,
     ALGORITHM_NOT_EXISTS,
     DATASTRUCTURE_NOT_EXISTS,
-    LEVEL_NOT_EXISTS,
-
-    PROBLEM_LINK_NOT_FOUND;
+    LEVEL_NOT_EXISTS;
 
 }
