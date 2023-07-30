19cefd6afb6a2bc403058025648b30da46421c66
jidam <jidamine87593@gmail.com>
2023-07-30 13:12:26 +0900

    refactor: DataStructure
    1. 와일드카드 사용하지 않도록 수정
    
    Related to : #1

--- backend/src/main/java/com/prograngers/backend/entity/DataStructure.java
+++ backend/src/main/java/com/prograngers/backend/entity/DataStructure.java
@@ -1,6 +1,11 @@
 package com.prograngers.backend.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
