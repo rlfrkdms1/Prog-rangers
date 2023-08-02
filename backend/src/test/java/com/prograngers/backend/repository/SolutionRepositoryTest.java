973b9af1e3a2821f8bd5f19fd036612642a7c958
jidam <jidamine87593@gmail.com>
2023-08-02 21:59:11 +0900

    test: SolutionRepositoryTest 멤버이름으로 전부찾기 테스트 추가
    1. 멤버_이름으로_전부_찾기 테스트 추가
    
    Related to : #6

--- backend/src/test/java/com/prograngers/backend/repository/SolutionRepositoryTest.java
+++ backend/src/test/java/com/prograngers/backend/repository/SolutionRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.prograngers.backend.repository;
 
+import com.prograngers.backend.entity.Member;
 import com.prograngers.backend.entity.Problem;
 import com.prograngers.backend.entity.Solution;
 import com.prograngers.backend.entity.constants.AlgorithmConstant;
@@ -17,6 +18,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
@@ -102,6 +104,60 @@ class SolutionRepositoryTest {
                 () -> {
                     solutionRepository.findById(solution.getId()).orElseThrow(() -> new SolutionNotFoundException());
                 });
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("멤버_이름으로_전부_찾기_테스트")
+    void 멤버_이름으로_전부_찾기_테스트(){
+
+        Member member1 = Member.builder()
+                .name("장지담")
+                .build();
+
+        Member member2 = Member.builder()
+                .name("길가은")
+                .build();
+
+        Solution solution1 = Solution.builder()
+                .member(member1)
+                .level(LevelConstant.THREE)
+                .title("풀이 제목")
+                .algorithm(AlgorithmConstant.BFS)
+                .dataStructure(DataStructureConstant.ARRAY)
+                .code("int a=10")
+                .description("풀이 설명")
+                .date(LocalDate.now())
+                .problem(new Problem(null, "문제", "링크", JudgeConstant.백준))
+                .build();
+        Solution solution2 = Solution.builder()
+                .member(member1)
+                .level(LevelConstant.THREE)
+                .title("풀이 제목")
+                .algorithm(AlgorithmConstant.BFS)
+                .dataStructure(DataStructureConstant.ARRAY)
+                .code("int a=10")
+                .description("풀이 설명")
+                .date(LocalDate.now())
+                .problem(new Problem(null, "문제", "링크", JudgeConstant.백준))
+                .build();
+        Solution solution3 = Solution.builder()
+                .member(member2)
+                .level(LevelConstant.THREE)
+                .title("풀이 제목")
+                .algorithm(AlgorithmConstant.BFS)
+                .dataStructure(DataStructureConstant.ARRAY)
+                .code("int a=10")
+                .description("풀이 설명")
+                .date(LocalDate.now())
+                .problem(new Problem(null, "문제", "링크", JudgeConstant.백준))
+                .build();
+        solutionRepository.save(solution1);
+        solutionRepository.save(solution2);
+        solutionRepository.save(solution3);
+
+        List<Solution> result = solutionRepository.findAllByMember(member1);
+        Assertions.assertThat(result.size()).isEqualTo(2);
     }
 
 }
