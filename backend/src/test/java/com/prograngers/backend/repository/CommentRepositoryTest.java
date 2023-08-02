e3cda7b73d72279f597b387ae1a9dcc4af164dc0
jidam <jidamine87593@gmail.com>
2023-08-02 22:07:46 +0900

    test:  CommentRepositoryTest 삭제 테스트 추가
    1. CommentRepositoryTest 삭제 테스트 추가
    
    Related to : #6

--- backend/src/test/java/com/prograngers/backend/repository/CommentRepositoryTest.java
+++ backend/src/test/java/com/prograngers/backend/repository/CommentRepositoryTest.java
@@ -1,12 +1,15 @@
 package com.prograngers.backend.repository;
 
 import com.prograngers.backend.entity.Comment;
+import com.prograngers.backend.exception.notfound.CommentNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -47,6 +50,27 @@ class CommentRepositoryTest {
 
         // then
         Assertions.assertThat(updated).isEqualTo(comment);
+    }
+
+    @Test
+    void 삭제_테스트(){
+        // given
+        Comment comment = Comment.builder()
+                .content("댓글 내용")
+                .build();
+        commentRepository.save(comment);
+        Comment saved = commentRepository.findById(1L).orElseThrow(() -> new CommentNotFoundException());
+        Assertions.assertThat(saved.getId()).isEqualTo(1L);
+
+        // when
+        commentRepository.delete(comment);
+
+        // then
+        org.junit.jupiter.api.Assertions.assertThrows(
+                CommentNotFoundException.class,
+                ()-> commentRepository.findById(1L).orElseThrow(()->new CommentNotFoundException())
+        );
+
     }
 
 
