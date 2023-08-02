b82fa4e63eaa72ef7b348d143e6bae8c8382a0fe
jidam <jidamine87593@gmail.com>
2023-08-02 13:45:02 +0900

    refactor: SolutionController 라인 정리
    1. SolutionController 라인 정리
    
    Related to : #6

--- backend/src/main/java/com/prograngers/backend/controller/SolutionController.java
+++ backend/src/main/java/com/prograngers/backend/controller/SolutionController.java
@@ -18,7 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
