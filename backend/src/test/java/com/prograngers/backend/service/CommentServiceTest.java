0ba929d5e1b096ba69b3b7ed9cfc6e81bc8b21f7
jidam <jidamine87593@gmail.com>
2023-08-02 22:22:41 +0900

    test:  SolutionServiceTest 솔루션을로 댓글 찾기 테스트 추가
    1. 솔루션이 주어졌을 때 솔루션으로 댓글 찾는 테스트 추가
    
    Related to : #6

---
+++ backend/src/test/java/com/prograngers/backend/service/CommentServiceTest.java
@@ -0,0 +1,63 @@
+package com.prograngers.backend.service;
+
+import com.prograngers.backend.entity.Comment;
+import com.prograngers.backend.entity.Solution;
+import com.prograngers.backend.repository.CommentRepository;
+import com.prograngers.backend.repository.SolutionRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+
+
+@ExtendWith(MockitoExtension.class)
+@Slf4j
+class CommentServiceTest {
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @Mock
+    SolutionRepository solutionRepository;
+    @InjectMocks
+    private CommentService commentService;
+
+    @InjectMocks
+    private SolutionService solutionService;
+
+    @Test
+    void 솔루션으로_댓글_찾기_테스트(){
+
+        // given
+        Solution solution = Solution.builder()
+                .description("설명").build();
+        Comment comment1 = Comment.builder()
+                .content("내용1")
+                .solution(solution).build();
+        Comment comment2 = Comment.builder()
+                .content("내용2")
+                .solution(solution).build();
+
+        List<Comment> comments = new ArrayList<>();
+        comments.add(comment1);
+        comments.add(comment2);
+
+        given(commentRepository.findAllBySolution(solution)).willReturn(comments);
+
+        // when
+        List<Comment> bySolution = commentService.findBySolution(solution);
+
+        // then
+        Assertions.assertThat(bySolution.size()).isEqualTo(2);
+    }
+
+
+}
