b7dff85e097461a8b1cd308ec9ce6ba5a8d515d3
jidam <jidamine87593@gmail.com>
2023-08-03 15:11:23 +0900

    fix: ProblemController에 @GetMapping 추가
    1. @GetMapping 애노테이션이 빠져서 추가
    
    Related to : #6

--- backend/src/main/java/com/prograngers/backend/controller/ProblemController.java
+++ backend/src/main/java/com/prograngers/backend/controller/ProblemController.java
@@ -7,6 +7,7 @@ import com.prograngers.backend.service.ProblemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class ProblemController {
 
     private final ProblemService problemService;
 
+    @GetMapping
     public ResponseEntity<?> problems() {
         List<ProblemListResponse> problemListResponses = problemService.getProblemList();
         return ResponseEntity.ok(problemListResponses);
