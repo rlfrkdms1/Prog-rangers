9e26e21a43e98db18bb33a77e67f2df84495d256
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-05 22:54:56 +0900

    feat: 인터셉터 등록
    로그인 어노테이션 확인 및 관리 인터셉터 등록
    
    Related to: #3

---
+++ backend/src/main/java/com/prograngers/backend/WebConfig.java
@@ -0,0 +1,15 @@
+package com.prograngers.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .order(1);
+    }
+}
