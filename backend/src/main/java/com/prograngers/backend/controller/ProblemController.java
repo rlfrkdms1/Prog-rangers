ecfadf253e6f066c4ef552b0158948e8cc0ee200
jidam <jidamine87593@gmail.com>
2023-08-07 15:49:49 +0900

    feat: ProblemController 수정
    1. problems가 쿼리스트링을 받도록 수정
    
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/controller/ProblemController.java
+++ backend/src/main/java/com/prograngers/backend/controller/ProblemController.java
@@ -2,12 +2,14 @@ package com.prograngers.backend.controller;
 
 
 import com.prograngers.backend.dto.problem.ProblemListResponse;
+import com.prograngers.backend.entity.constants.AlgorithmConstant;
 import com.prograngers.backend.service.ProblemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,8 +23,8 @@ public class ProblemController {
     private final ProblemService problemService;
 
     @GetMapping
-    public ResponseEntity<?> problems() {
-        List<ProblemListResponse> problemListResponses = problemService.getProblemList();
+    public ResponseEntity<?> problems(@RequestParam String algorithm, String dataStructure, String sortBy) {
+        List<ProblemListResponse> problemListResponses = problemService.getProblemList(algorithm, dataStructure, sortBy);
         return ResponseEntity.ok(problemListResponses);
     }
 
