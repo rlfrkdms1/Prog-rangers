fed84f924287b14465f8845c08e05c6bdbdf4759
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-09 06:29:17 +0900

    feat: 회원 가입 기능 추가
    회원 가입 기능을 추가했습니다.
    
    Related to: #3

--- backend/src/main/java/com/prograngers/backend/MemberController.java
+++ backend/src/main/java/com/prograngers/backend/MemberController.java
@@ -1,6 +1,5 @@
 package com.prograngers.backend;
 
-import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
@@ -17,11 +16,25 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
-        LoginResult loginResult = memberService.login(loginRequest);
+    @PostMapping("/sign-up")
+    public ResponseEntity<String> singUp(@RequestBody SignUpRequest signUpRequest) {
+        AuthResult authResult = memberService.signUp(signUpRequest);
+        ResponseCookie cookie = ResponseCookie.from("refreshToken", authResult.getRefreshToken())
+                .httpOnly(true)
+                .maxAge(VALID_TIME_REFRESH_TOKEN)
+                .secure(true)
+                .path("/")
+                .build();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(authResult.getAccessToken());
+    }
 
-        ResponseCookie cookie = ResponseCookie.from("refreshToken", loginResult.getRefreshToken())
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        AuthResult authResult = memberService.login(loginRequest);
+
+        ResponseCookie cookie = ResponseCookie.from("refreshToken", authResult.getRefreshToken())
                 .httpOnly(true)
                 .maxAge(VALID_TIME_REFRESH_TOKEN)
                 .secure(true)
@@ -30,6 +43,6 @@ public class MemberController {
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .body(loginResult.getAccessToken());
+                .body(authResult.getAccessToken());
     }
 }
