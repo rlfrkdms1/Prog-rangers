4e2621ae2fee2f0f6114b93321ff82174064036d
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-09 06:30:34 +0900

    feat: 회원 가입 DTO 추가
    회원 가입시 기입하는 정보들입니다.
    
    Related to: #3

---
+++ backend/src/main/java/com/prograngers/backend/SignUpRequest.java
@@ -0,0 +1,35 @@
+package com.prograngers.backend;
+
+import com.prograngers.backend.entity.Member;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+public class SignUpRequest {
+
+    @NotBlank(message = "이름은 필수입니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+
+    @Email(message = "이메일 형식이 아닙니다.")
+    @NotBlank(message = "이메일은 필수입니다.")
+    private String email;
+
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private String nickName;
+
+    @NotBlank(message = "전화번호는 필수입니다.")
+    @Size(min = 11, max = 11, message = "전화번호는 11자리 입니다.")
+    private String phoneNumber;
+
+    public Member toMember(){
+        return Member.builder()
+                .name(this.username)
+                .email(this.email)
+                .password(this.password)
+                .phoneNumber(this.phoneNumber)
+                .nickname(this.nickName)
+                .build();
+    }
+}
