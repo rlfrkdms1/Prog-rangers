a54137f24f5e498b9ee89ca2d4d43f0db865247e
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-09 05:40:36 +0900

    feat: id로 member 찾는 기능 추가
    redis에서 id 로 member를 찾는 기능을 추가합니다.
    
    Related to: #3

--- backend/src/main/java/com/prograngers/backend/RefreshTokenRepository.java
+++ backend/src/main/java/com/prograngers/backend/RefreshTokenRepository.java
@@ -3,6 +3,6 @@ package com.prograngers.backend;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
-
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findById(Long memberId);
 }
