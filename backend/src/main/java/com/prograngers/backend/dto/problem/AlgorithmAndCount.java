272553a11a53e436160fc753cfe20f560590ead9
jidam <jidamine87593@gmail.com>
2023-08-08 16:49:34 +0900

    feat: ProblemService 매핑 수정
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/dto/problem/AlgorithmAndCount.java
+++ backend/src/main/java/com/prograngers/backend/dto/problem/AlgorithmAndCount.java
@@ -1,16 +1,10 @@
 package com.prograngers.backend.dto.problem;
 
 import com.prograngers.backend.entity.constants.AlgorithmConstant;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class AlgorithmAndCount {
-    AlgorithmConstant algorithm;
-    Integer count;
+
+public interface AlgorithmAndCount {
+    AlgorithmConstant getAlgorithm();
+
+    Integer getCount();
 }
--- backend/src/main/java/com/prograngers/backend/dto/problem/DataStructureAndCount.java
+++ backend/src/main/java/com/prograngers/backend/dto/problem/DataStructureAndCount.java
@@ -1,17 +1,9 @@
 package com.prograngers.backend.dto.problem;
 
-import com.prograngers.backend.entity.constants.AlgorithmConstant;
 import com.prograngers.backend.entity.constants.DataStructureConstant;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class DataStructureAndCount {
-    DataStructureConstant dataStructure;
-    Integer count;
+
+public interface DataStructureAndCount {
+    DataStructureConstant getDataStructure();
+    Integer getCount();
 }
