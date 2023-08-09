621d49ea06987866bb4118aa4d0dec276415bf16
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-09 09:21:42 +0900

    feat: 유효기간 필드 추가
    유효 기간 필드를 추가했습니다.
    
    Related to: #3

--- backend/src/main/java/com/prograngers/backend/AuthResult.java
+++ backend/src/main/java/com/prograngers/backend/AuthResult.java
@@ -1,15 +1,15 @@
 package com.prograngers.backend;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class AuthResult {
 
     private String accessToken;
     private String refreshToken;
+    private Long refreshTokenExpiredAt;
 
-    public AuthResult(String accessToken, String refreshToken) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
+
 }
