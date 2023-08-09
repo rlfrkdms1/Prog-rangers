177872ea41c5da0cb4ea318480d3f1d9d7c90f37
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-09 09:02:31 +0900

    refactor: cookie 생성 메서드 추출
    쿠키를 생성하는 로직이 같아 메서드를 추출해 코드의 중복을 줄였습니다.
    
    Related to: #3

--- backend/src/main/java/com/prograngers/backend/MemberController.java
+++ backend/src/main/java/com/prograngers/backend/MemberController.java
@@ -1,45 +1,63 @@
 package com.prograngers.backend;
 
+import com.prograngers.backend.exception.NotExistTokenException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.prograngers.backend.AuthConstant.VALID_TIME_REFRESH_TOKEN;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+    private static final String REFRESH_TOKEN = "refreshToken";
 
     @PostMapping("/sign-up")
     public ResponseEntity<String> singUp(@RequestBody SignUpRequest signUpRequest) {
         AuthResult authResult = memberService.signUp(signUpRequest);
-        ResponseCookie cookie = ResponseCookie.from("refreshToken", authResult.getRefreshToken())
-                .httpOnly(true)
-                .maxAge(VALID_TIME_REFRESH_TOKEN)
-                .secure(true)
-                .path("/")
-                .build();
+        ResponseCookie cookie = createCookieWithRefreshToken(authResult.getRefreshToken());
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
                 .body(authResult.getAccessToken());
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
-        AuthResult authResult = memberService.login(loginRequest);
-
-        ResponseCookie cookie = ResponseCookie.from("refreshToken", authResult.getRefreshToken())
+    private static ResponseCookie createCookieWithRefreshToken(String refreshToken) {
+        ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN, refreshToken)
                 .httpOnly(true)
                 .maxAge(VALID_TIME_REFRESH_TOKEN)
                 .secure(true)
                 .path("/")
                 .build();
+        return cookie;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        AuthResult authResult = memberService.login(loginRequest);
+        ResponseCookie cookie = createCookieWithRefreshToken(authResult.getRefreshToken());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(authResult.getAccessToken());
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<String> reissue(@CookieValue(name = REFRESH_TOKEN) String refreshToken) {
+        if (refreshToken == null) {
+            throw new NotExistTokenException();
+        }
+        AuthResult authResult = memberService.reissue(refreshToken);
+        ResponseCookie cookie = createCookieWithRefreshToken(authResult.getRefreshToken());
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
