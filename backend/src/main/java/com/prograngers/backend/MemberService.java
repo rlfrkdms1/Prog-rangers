2a3eff688ea98fd1e62c09378f4036074ed74d50
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-09 09:04:51 +0900

    feat: 비밀번호 암/복호화 기능 변경
    비밀번호를 db에 저장할 때, db에서 꺼낼 때 사용하는 암/복호화 로직을 변경하였습니다.
    
    Related to: #3

--- backend/src/main/java/com/prograngers/backend/MemberService.java
+++ backend/src/main/java/com/prograngers/backend/MemberService.java
@@ -2,21 +2,23 @@ package com.prograngers.backend;
 
 import com.prograngers.backend.entity.Member;
 import com.prograngers.backend.exception.InvalidPasswordException;
+import com.prograngers.backend.exception.RefreshTokenNotFoundException;
 import com.prograngers.backend.exception.notfound.MemberNotFoundException;
 import com.prograngers.backend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final Encrypt encrypt;
     private final JwtTokenProvider jwtTokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
 
@@ -24,34 +26,29 @@ public class MemberService {
     public AuthResult login(LoginRequest loginRequest) {
         //회원 검증
         Member member = findByEmail(loginRequest.getEmail());
-        member.getPassword().matches(loginRequest.getPassword());
-        validPassword(loginRequest.getPassword(), member.getPassword());
+        validPassword(member.getPassword(), loginRequest.getPassword());
         //access token 발급
-        String accessToken = jwtTokenProvider.createAccessToken(member.getId());
+        return issueToken(member.getId());
+    }
+
+    private AuthResult issueToken(Long memberId) {
+        String accessToken = jwtTokenProvider.createAccessToken(memberId);
         //refresh token 발급, 저장, 쿠키 생성
         RefreshToken refreshToken = RefreshToken.builder()
-                .memberId(member.getId())
+                .memberId(memberId)
                 .refreshToken(UUID.randomUUID().toString())
                 .build();
         refreshTokenRepository.save(refreshToken);
         return new AuthResult(accessToken, refreshToken.getRefreshToken());
-
     }
 
     @Transactional
     public AuthResult signUp(SignUpRequest signUpRequest) {
         Member member = signUpRequest.toMember();
-        member.encodePassword(passwordEncoder.encode(member.getPassword()));
+        member.encodePassword(encrypt.encryptAES256(member.getPassword()));
         memberRepository.save(member);
         //access token 발급
-        String accessToken = jwtTokenProvider.createAccessToken(member.getId());
-        //refresh token 발급, 저장, 쿠키 생성
-        RefreshToken refreshToken = RefreshToken.builder()
-                .memberId(member.getId())
-                .refreshToken(UUID.randomUUID().toString())
-                .build();
-        refreshTokenRepository.save(refreshToken);
-        return new AuthResult(accessToken, refreshToken.getRefreshToken());
+        return issueToken(member.getId());
     }
 
     public Member findByEmail(String email) {
@@ -59,7 +56,7 @@ public class MemberService {
     }
 
     public void validPassword(String savedPassword, String inputPassword) {
-        if (!passwordEncoder.matches(inputPassword, savedPassword)) {
+        if (!encrypt.decryptAES256(savedPassword).equals(inputPassword)) {
             throw new InvalidPasswordException();
         }
     }
