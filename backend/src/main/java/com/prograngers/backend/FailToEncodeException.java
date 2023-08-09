67b1da31a2e2eb5858435adcb413e6eb681c35b8
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-09 09:16:39 +0900

    feat: 비밀 번호 암호화 실패 에러 추가
    비밀번호 암호화에 실패 했을 때의 예외 입니다.
    
    Related to: #3

---
+++ backend/src/main/java/com/prograngers/backend/FailToEncodeException.java
@@ -0,0 +1,11 @@
+package com.prograngers.backend;
+
+import com.prograngers.backend.exception.UnAuthorizationException;
+
+import static com.prograngers.backend.exception.ErrorCode.FAIL_TO_ENCODED;
+
+public class FailToEncodeException extends UnAuthorizationException {
+    public FailToEncodeException() {
+        super(FAIL_TO_ENCODED, "비밀번호를 암호화하는데 실패했습니다.");
+    }
+}
