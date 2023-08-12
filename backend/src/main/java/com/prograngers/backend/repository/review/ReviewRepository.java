a76e1f18838af4cadf070b8ad276dcf8cf912c08
jidam <jidamine87593@gmail.com>
2023-08-12 17:27:38 +0900

    feat: ReviewRepository 리포지토리 구조 수정
    ReviewRepository 리포지토리 구성을 수정했습니다
    
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/repository/review/ReviewRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/review/ReviewRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>, QueryDslReviewRepository{
     List<Review> findAllBySolution(Solution solution);
 
     List<Review> findAllByCodeLineNumber(Integer codeLineNumber);
