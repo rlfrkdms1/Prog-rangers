ea42be173cfab1e3a777c1fa580a47e8468b1ddb
jidam <jidamine87593@gmail.com>
2023-08-13 01:00:28 +0900

    feat: 문제에 태그 다는 기능 추가
    1. 리포지토리에서 문제에 달 태그를 계산하는 코드 추가
    
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/repository/problem/QueryDslProblemRepositoryImpl.java
+++ backend/src/main/java/com/prograngers/backend/repository/problem/QueryDslProblemRepositoryImpl.java
@@ -1,16 +1,10 @@
 package com.prograngers.backend.repository.problem;
 
 import com.prograngers.backend.entity.Problem;
-import com.prograngers.backend.entity.QProblem;
-import com.prograngers.backend.entity.QSolution;
 import com.prograngers.backend.entity.Solution;
 import com.prograngers.backend.entity.constants.AlgorithmConstant;
 import com.prograngers.backend.entity.constants.DataStructureConstant;
 import com.prograngers.backend.repository.problem.dto.ProblemResponse;
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.Tuple;
-import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +12,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.prograngers.backend.entity.QProblem.*;
 import static com.prograngers.backend.entity.QSolution.*;
@@ -26,27 +23,44 @@ import static com.prograngers.backend.entity.QSolution.*;
 @RequiredArgsConstructor
 @Repository
 @Slf4j
-public class QueryDslProblemRepositoryImpl implements QueryDslProblemRepository{
+public class QueryDslProblemRepositoryImpl implements QueryDslProblemRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
     public List<ProblemResponse> searchByAlgorithmAndDataStructureOrderByDateDesc(
-            int page, DataStructureConstant dataStructure, AlgorithmConstant algorithm, String orderBy){
+            int page, DataStructureConstant dataStructure, AlgorithmConstant algorithm, String orderBy) {
 
-        // 연관관계가 없는 테이블을 조인하기 위한 세타 조인
+        // 양방향 연관관계로 변경
         List<Problem> results = jpaQueryFactory
-                .select(problem)
-                .from(problem, solution)
-                .where(problem.id.eq(solution.problem.id),dataStructureEq(dataStructure),algorithmEq(algorithm))
+                .selectFrom(problem)
+                // solution을 조회해서 자료구조, 알고리즘을 알아내야 해서 성능을 위해 패치조인
+                .join(problem.solutions, solution).fetchJoin()
+                .where(dataStructureEq(dataStructure), algorithmEq(algorithm))
                 .fetch();
 
         List<ProblemResponse> problemResponses = new ArrayList<>();
 
-        for (Problem result : results ){
+        for (Problem result : results) {
             ProblemResponse problemResponse = ProblemResponse.builder()
                     .title(result.getTitle())
                     .ojName(result.getOjName())
+                    .tags(new ArrayList<>())
                     .build();
+
+            List<Solution> solutions = result.getSolutions();
+            HashMap<Object,Integer> hm = new HashMap<>();
+
+            for (Solution solution : solutions) {
+                hm.put(solution.getAlgorithm(),hm.getOrDefault(solution.getAlgorithm(),1)+1);
+                hm.put(solution.getDataStructure(),hm.getOrDefault(solution.getDataStructure(),1)+1);
+            }
+            List<Map.Entry<Object, Integer>> collect = hm.entrySet().stream()
+                    .sorted(Map.Entry.comparingByValue())
+                    .collect(Collectors.toList());
+            for (Map.Entry<Object, Integer> entry : collect) {
+                problemResponse.getTags().add(entry.getKey());
+            }
+
             problemResponses.add(problemResponse);
         }
 
@@ -55,10 +69,11 @@ public class QueryDslProblemRepositoryImpl implements QueryDslProblemRepository{
 
 
     private BooleanExpression dataStructureEq(DataStructureConstant dataStructure) {
-        return dataStructure!=null? solution.dataStructure.eq(dataStructure) : null;
+        return dataStructure != null ? solution.dataStructure.eq(dataStructure) : null;
     }
+
     private BooleanExpression algorithmEq(AlgorithmConstant algorithm) {
-        return algorithm!=null? solution.algorithm.eq(algorithm):null;
+        return algorithm != null ? solution.algorithm.eq(algorithm) : null;
     }
 
 }
