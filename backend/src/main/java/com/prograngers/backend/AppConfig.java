378765690838870e1204fc5ad2dc735b8f9480d8
jidam <jidamine87593@gmail.com>
2023-08-12 18:24:08 +0900

    feat: Querydsl 설정 완료
    서버 돌아가도록 되도록 설정 완료
    
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/AppConfig.java
+++ backend/src/main/java/com/prograngers/backend/AppConfig.java
@@ -3,6 +3,7 @@ package com.prograngers.backend;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,6 +12,7 @@ public class AppConfig {
 
     private final EntityManager em;
 
+    @Bean
     public JPAQueryFactory jpaQueryFactory(){
         return new JPAQueryFactory(em);
     }
