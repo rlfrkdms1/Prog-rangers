30ab59c1b8a7acd731cd6412ca62a673c9effd70
jidam <jidamine87593@gmail.com>
2023-08-13 14:49:53 +0900

    feat: SolutionListResponse 에 생성자 추가
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/dto/SolutionListResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/SolutionListResponse.java
@@ -1,13 +1,19 @@
 package com.prograngers.backend.dto;
 
 import com.prograngers.backend.entity.constants.JudgeConstant;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SolutionListResponse {
     String problemName;
     JudgeConstant ojName;
