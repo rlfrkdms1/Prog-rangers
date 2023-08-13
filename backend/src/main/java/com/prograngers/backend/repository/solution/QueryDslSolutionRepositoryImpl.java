044791318098c8d8d49aa7b66c783772db365294
jidam <jidamine87593@gmail.com>
2023-08-13 15:14:45 +0900

    feat: QueryDslSolutionRepositoryImpl 에 getSolutionList 추가
    언어, 알고리즘, 자료구조에 따른 필터링과 정렬기준에 따라 정렬해서 반환하는 메서드 추가
    
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/repository/solution/QueryDslSolutionRepositoryImpl.java
+++ backend/src/main/java/com/prograngers/backend/repository/solution/QueryDslSolutionRepositoryImpl.java
@@ -1,15 +1,60 @@
 package com.prograngers.backend.repository.solution;
 
+import com.prograngers.backend.entity.QSolution;
 import com.prograngers.backend.entity.Solution;
 import com.prograngers.backend.entity.constants.AlgorithmConstant;
 import com.prograngers.backend.entity.constants.DataStructureConstant;
 import com.prograngers.backend.entity.constants.LanguageConstant;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.prograngers.backend.entity.QSolution.*;
+
+@RequiredArgsConstructor
+@Repository
+@Slf4j
 public class QueryDslSolutionRepositoryImpl implements QueryDslSolutionRepository{
+
+    private final JPAQueryFactory jpaQueryFactory;
     @Override
     public List<Solution> getSolutionList(LanguageConstant language, AlgorithmConstant algorithm, DataStructureConstant dataStructure, String sortBy) {
-        return null;
+        List<Solution> result = jpaQueryFactory
+                .selectFrom(solution)
+                .where(languageEq(language), algorithmEq(algorithm), dataStructureEq(dataStructure))
+                .orderBy(sortByWhat(sortBy))
+                .fetch();
+        return result;
+    }
+
+    private OrderSpecifier<?> sortByWhat(String sortBy) {
+        if (sortBy.equals("newest")){
+            return solution.date.desc();
+        }
+        if (sortBy.equals("likes")){
+            return solution.likes.size().desc();
+        }
+        if (sortBy.equals("scraps")){
+            return solution.scraps.desc();
+        }
+        else return  null;
+    }
+
+    private BooleanExpression dataStructureEq(DataStructureConstant dataStructure) {
+        return dataStructure!=null?solution.dataStructure.eq(dataStructure):null;
+    }
+
+    private BooleanExpression algorithmEq(AlgorithmConstant algorithm) {
+        return algorithm!=null?solution.algorithm.eq(algorithm):null;
+    }
+
+    private BooleanExpression languageEq(LanguageConstant language) {
+        return language!=null?solution.language.eq(language):null;
     }
 }
