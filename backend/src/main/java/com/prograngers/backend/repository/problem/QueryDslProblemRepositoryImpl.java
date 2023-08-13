ac84e38a1182a9cbc72a0974d0cca3509c093324
jidam <jidamine87593@gmail.com>
2023-08-13 16:58:20 +0900

    refactor: Problem 목록조회 정적 팩토리 메소드 사용하도록 변경
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/repository/problem/QueryDslProblemRepositoryImpl.java
+++ backend/src/main/java/com/prograngers/backend/repository/problem/QueryDslProblemRepositoryImpl.java
@@ -28,10 +28,6 @@ public class QueryDslProblemRepositoryImpl implements QueryDslProblemRepository 
 
     public List<ProblemResponse> searchByAlgorithmAndDataStructureOrderByDateDesc(
             int page, DataStructureConstant dataStructure, AlgorithmConstant algorithm, String orderBy) {
-
-//        QProblem subProblem = new QProblem("subProblem");
-//        QSolution subSolution = new QSolution("subSolution");
-
         // 양방향 연관관계로 변경
         List<Problem> results = jpaQueryFactory
                 .selectFrom(problem)
@@ -42,42 +38,7 @@ public class QueryDslProblemRepositoryImpl implements QueryDslProblemRepository 
                 .offset((page-1)*4)
                 .limit(4)
                 .fetch();
-
-        // 반환할 dto 리스트
-        List<ProblemResponse> problemResponses = new ArrayList<>();
-
-        // 결과를  for문 돌면서 반환 dto를 만든다
-        for (Problem result : results) {
-            ProblemResponse problemResponse = ProblemResponse.builder()
-                    .title(result.getTitle())
-                    .ojName(result.getOjName())
-                    .tags(new ArrayList<>())
-                    .build();
-
-            List<Solution> solutions = result.getSolutions();
-            HashMap<Object,Integer> hm = new HashMap<>();
-
-            for (Solution solution : solutions) {
-                hm.put(solution.getAlgorithm(),hm.getOrDefault(solution.getAlgorithm(),1)+1);
-                hm.put(solution.getDataStructure(),hm.getOrDefault(solution.getDataStructure(),1)+1);
-            }
-
-            List<Object> keySet = new ArrayList<>(hm.keySet());
-            keySet.sort((num1,num2)->hm.get(num2).compareTo(hm.get(num1)));
-
-            for (int i=0; i<keySet.size(); i++){
-                if (i==3) break;
-                Object tag = keySet.get(i);
-                if (tag!=null){
-                    problemResponse.getTags().add(keySet.get(i));
-                }
-            }
-
-
-            problemResponses.add(problemResponse);
-        }
-
-        return problemResponses;
+        return  ProblemResponse.toDto(results);
     }
 
     private OrderSpecifier<?> orderByWhat(String orderBy) {
--- backend/src/main/java/com/prograngers/backend/repository/problem/dto/ProblemResponse.java
+++ backend/src/main/java/com/prograngers/backend/repository/problem/dto/ProblemResponse.java
@@ -1,5 +1,7 @@
 package com.prograngers.backend.repository.problem.dto;
 
+import com.prograngers.backend.entity.Problem;
+import com.prograngers.backend.entity.Solution;
 import com.prograngers.backend.entity.constants.AlgorithmConstant;
 import com.prograngers.backend.entity.constants.DataStructureConstant;
 import com.prograngers.backend.entity.constants.JudgeConstant;
@@ -8,6 +10,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Getter
@@ -18,4 +21,43 @@ public class ProblemResponse {
     JudgeConstant ojName;
 
     List<Object> tags = new ArrayList<>();
+
+
+    public static List<ProblemResponse> toDto(List<Problem> results) {
+        // 반환할 dto 리스트
+        List<ProblemResponse> problemResponses = new ArrayList<>();
+
+        // 결과를  for문 돌면서 반환 dto를 만든다
+        for (Problem result : results) {
+            ProblemResponse problemResponse = ProblemResponse.builder()
+                    .title(result.getTitle())
+                    .ojName(result.getOjName())
+                    .tags(new ArrayList<>())
+                    .build();
+
+            List<Solution> solutions = result.getSolutions();
+            HashMap<Object,Integer> hm = new HashMap<>();
+
+            for (Solution solution : solutions) {
+                hm.put(solution.getAlgorithm(),hm.getOrDefault(solution.getAlgorithm(),1)+1);
+                hm.put(solution.getDataStructure(),hm.getOrDefault(solution.getDataStructure(),1)+1);
+            }
+
+            List<Object> keySet = new ArrayList<>(hm.keySet());
+            keySet.sort((num1,num2)->hm.get(num2).compareTo(hm.get(num1)));
+
+            for (int i=0; i<keySet.size(); i++){
+                if (i==3) break;
+                Object tag = keySet.get(i);
+                if (tag!=null){
+                    problemResponse.getTags().add(keySet.get(i));
+                }
+            }
+
+
+            problemResponses.add(problemResponse);
+        }
+
+        return problemResponses;
+    }
 }
