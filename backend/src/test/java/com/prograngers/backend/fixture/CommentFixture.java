e3399ce6add7550302ab9f0f7fbcf122df162ad0
jidam <jidamine87593@gmail.com>
2023-08-13 22:25:50 +0900

    feat: Comment 픽스처 생성
    Related to : #9

---
+++ backend/src/test/java/com/prograngers/backend/fixture/CommentFixture.java
@@ -0,0 +1,28 @@
+package com.prograngers.backend.fixture;
+
+import com.prograngers.backend.entity.Comment;
+import com.prograngers.backend.entity.Member;
+import com.prograngers.backend.entity.Solution;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum
+CommentFixture {
+    댓글1("댓글내용1"),
+    댓글2("댓글내용2"),
+    댓글3("댓글내용3");
+    private final String content;
+
+    public Comment.CommentBuilder 기본_빌더_생성() {
+        return Comment.builder()
+                .content(this.content);
+    }
+
+    public Comment 댓글_생성(Long id, Solution solution, Member member) {
+        return 기본_빌더_생성()
+                .id(id)
+                .solution(solution)
+                .member(member)
+                .build();
+    }
+}
