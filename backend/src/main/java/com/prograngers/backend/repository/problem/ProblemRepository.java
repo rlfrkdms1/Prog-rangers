733294818819a989f5d6ad241515af189ec2a618
jidam <jidamine87593@gmail.com>
2023-08-13 17:06:35 +0900

    refactor: ProblemRepository 사용하지 않는 메서드 삭제
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/repository/problem/ProblemRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/problem/ProblemRepository.java
@@ -1,11 +1,7 @@
 package com.prograngers.backend.repository.problem;
 
 import com.prograngers.backend.entity.Problem;
-import com.prograngers.backend.entity.constants.AlgorithmConstant;
-import com.prograngers.backend.entity.constants.DataStructureConstant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
