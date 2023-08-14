466a163933a8689ca205ea3086aba8f9fbb68bd7
jidam <jidamine87593@gmail.com>
2023-08-14 16:58:57 +0900

    refactor: QueryDslSolutionRepositoryImpl 라인 정리
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/repository/solution/QueryDslSolutionRepositoryImpl.java
+++ backend/src/main/java/com/prograngers/backend/repository/solution/QueryDslSolutionRepositoryImpl.java
@@ -1,12 +1,10 @@
 package com.prograngers.backend.repository.solution;
 
-import com.prograngers.backend.entity.QSolution;
 import com.prograngers.backend.entity.Solution;
 import com.prograngers.backend.entity.constants.AlgorithmConstant;
 import com.prograngers.backend.entity.constants.DataStructureConstant;
 import com.prograngers.backend.entity.constants.LanguageConstant;
 import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -20,43 +18,43 @@ import static com.prograngers.backend.entity.QSolution.*;
 @RequiredArgsConstructor
 @Repository
 @Slf4j
-public class QueryDslSolutionRepositoryImpl implements QueryDslSolutionRepository{
+public class QueryDslSolutionRepositoryImpl implements QueryDslSolutionRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
+
     @Override
     public List<Solution> getSolutionList(int page, Long problemId, LanguageConstant language, AlgorithmConstant algorithm, DataStructureConstant dataStructure, String sortBy) {
         List<Solution> result = jpaQueryFactory
                 .selectFrom(solution)
-                .where(solution.problem.id.eq(problemId),languageEq(language), algorithmEq(algorithm), dataStructureEq(dataStructure))
+                .where(solution.problem.id.eq(problemId), languageEq(language), algorithmEq(algorithm), dataStructureEq(dataStructure))
                 .orderBy(sortByWhat(sortBy))
-                .offset((page-1)*4)
+                .offset((page - 1) * 4)
                 .limit(4)
                 .fetch();
         return result;
     }
 
     private OrderSpecifier<?> sortByWhat(String sortBy) {
-        if (sortBy.equals("newest")){
+        if (sortBy.equals("newest")) {
             return solution.date.desc();
         }
-        if (sortBy.equals("likes")){
+        if (sortBy.equals("likes")) {
             return solution.likes.size().desc();
         }
-        if (sortBy.equals("scraps")){
+        if (sortBy.equals("scraps")) {
             return solution.scraps.desc();
-        }
-        else return  null;
+        } else return null;
     }
 
     private BooleanExpression dataStructureEq(DataStructureConstant dataStructure) {
-        return dataStructure!=null?solution.dataStructure.eq(dataStructure):null;
+        return dataStructure != null ? solution.dataStructure.eq(dataStructure) : null;
     }
 
     private BooleanExpression algorithmEq(AlgorithmConstant algorithm) {
-        return algorithm!=null?solution.algorithm.eq(algorithm):null;
+        return algorithm != null ? solution.algorithm.eq(algorithm) : null;
     }
 
     private BooleanExpression languageEq(LanguageConstant language) {
-        return language!=null?solution.language.eq(language):null;
+        return language != null ? solution.language.eq(language) : null;
     }
 }
