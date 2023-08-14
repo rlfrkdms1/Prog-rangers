58fcb0371f4d53cec92e530d98b262059af37049
jidam <jidamine87593@gmail.com>
2023-08-14 12:39:13 +0900

    fix: 예외 생성자 파라미터 위치 수정
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/exception/notfound/ProblemNotFoundException.java
+++ backend/src/main/java/com/prograngers/backend/exception/notfound/ProblemNotFoundException.java
@@ -6,6 +6,6 @@ import static com.prograngers.backend.exception.ErrorCode.PROBLEM_NOT_FOUND;
 public class ProblemNotFoundException extends NotFoundException {
 
     public ProblemNotFoundException() {
-        super("문제를 찾을 수 없습니다.", PROBLEM_NOT_FOUND);
+        super("문제를 찾을 수 없습니다",PROBLEM_NOT_FOUND);
     }
 }
