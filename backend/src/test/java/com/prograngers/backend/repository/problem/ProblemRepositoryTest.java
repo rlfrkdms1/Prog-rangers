8ba53c546c597c3846bac3b798f020bfb2e8be4f
jidam <jidamine87593@gmail.com>
2023-08-14 16:52:27 +0900

    test: 문제 목록 조회 알고리즘, 자료구조 필터 테스트 추가
    Related to : #9

--- backend/src/test/java/com/prograngers/backend/repository/problem/ProblemRepositoryTest.java
+++ backend/src/test/java/com/prograngers/backend/repository/problem/ProblemRepositoryTest.java
@@ -20,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static com.prograngers.backend.entity.constants.AlgorithmConstant.*;
+import static com.prograngers.backend.entity.constants.DataStructureConstant.*;
 import static com.prograngers.backend.fixture.MemberFixture.길가은1;
 import static com.prograngers.backend.fixture.ProblemFixture.문제1;
 import static com.prograngers.backend.fixture.ProblemFixture.문제2;
@@ -60,15 +62,15 @@ class ProblemRepositoryTest {
         problemRepository.save(problem3);
 
         // 풀이 : 문제 - 풀이를 join해서 가져오기 때문에 problem.s
-        Solution solution1 = 풀이1.일반_솔루션_생성(null, problem1, member, 0, AlgorithmConstant.BFS, DataStructureConstant.QUEUE);
-        Solution solution2 = 풀이1.일반_솔루션_생성(null, problem1, member, 0, AlgorithmConstant.BFS, DataStructureConstant.QUEUE);
-        Solution solution3 = 풀이1.일반_솔루션_생성(null, problem1, member, 0, AlgorithmConstant.BFS, DataStructureConstant.QUEUE);
-        Solution solution4 = 풀이1.일반_솔루션_생성(null, problem1, member, 0, AlgorithmConstant.BFS, DataStructureConstant.QUEUE);
-        Solution solution5 = 풀이1.일반_솔루션_생성(null, problem1, member, 0, AlgorithmConstant.BFS, DataStructureConstant.QUEUE);
-        Solution solution6 = 풀이1.일반_솔루션_생성(null, problem2, member, 0, AlgorithmConstant.BFS, DataStructureConstant.QUEUE);
-        Solution solution7 = 풀이1.일반_솔루션_생성(null, problem2, member, 0, AlgorithmConstant.BFS, DataStructureConstant.QUEUE);
-        Solution solution8 = 풀이1.일반_솔루션_생성(null, problem2, member, 0, AlgorithmConstant.BFS, DataStructureConstant.QUEUE);
-        Solution solution9 = 풀이1.일반_솔루션_생성(null, problem3, member, 0, AlgorithmConstant.BFS, DataStructureConstant.QUEUE);
+        Solution solution1 = 풀이1.일반_솔루션_생성(null, problem1, member, 0, BFS, QUEUE);
+        Solution solution2 = 풀이1.일반_솔루션_생성(null, problem1, member, 0, BFS, QUEUE);
+        Solution solution3 = 풀이1.일반_솔루션_생성(null, problem1, member, 0, BFS, QUEUE);
+        Solution solution4 = 풀이1.일반_솔루션_생성(null, problem1, member, 0, BFS, QUEUE);
+        Solution solution5 = 풀이1.일반_솔루션_생성(null, problem1, member, 0, BFS, QUEUE);
+        Solution solution6 = 풀이1.일반_솔루션_생성(null, problem2, member, 0, BFS, QUEUE);
+        Solution solution7 = 풀이1.일반_솔루션_생성(null, problem2, member, 0, BFS, QUEUE);
+        Solution solution8 = 풀이1.일반_솔루션_생성(null, problem2, member, 0, BFS, QUEUE);
+        Solution solution9 = 풀이1.일반_솔루션_생성(null, problem3, member, 0, BFS, QUEUE);
         solutionRepository.save(solution1);
         solutionRepository.save(solution2);
         solutionRepository.save(solution3);
@@ -79,26 +81,67 @@ class ProblemRepositoryTest {
         solutionRepository.save(solution8);
         solutionRepository.save(solution9);
 
-
-        List<Problem> findAll = problemRepository.findAll();
-        log.info("findAll.size() = {}",findAll.size());
-
         // when
         List<Problem> result = problemRepository.searchByAlgorithmAndDataStructureOrderByDateDesc(
                 1, null, null, "date"
         );
-
-        log.info("result.size() = {}",result.size());
-
-        for (Problem problem : result) {
-            log.info("problem title : {}",problem.getTitle());
-        }
 
         // then
         Assertions.assertThat(result.get(0).getTitle()).isEqualTo("문제제목3");
         Assertions.assertThat(result.get(1).getTitle()).isEqualTo("문제제목2");
         Assertions.assertThat(result.get(2).getTitle()).isEqualTo("문제제목1");
     }
+
+    @DisplayName("문제 목록 조회 시 풀이의 알고리즘, 자료구조 필터에 따라 조회한다")
+    @Test
+    void 문제_목록_필터(){
+        // given
+        // 회원
+        Member member = 길가은1.getMember();
+        memberRepository.save(member);
+
+        // 문제
+        Problem problem1 = 문제1.아이디_값_지정_문제_생성(null);
+        Problem problem2 = 문제1.아이디_값_지정_문제_생성(null);
+        Problem problem3 = 문제1.아이디_값_지정_문제_생성(null);
+        Problem problem4 = 문제1.아이디_값_지정_문제_생성(null);
+
+        problemRepository.save(problem1);
+        problemRepository.save(problem2);
+        problemRepository.save(problem3);
+        problemRepository.save(problem4);
+
+        // 풀이 : 문제 - 풀이를 join해서 가져오기 때문에 problem.s
+        Solution solution1 = 풀이1.일반_솔루션_생성(null, problem1, member, 0, BFS, QUEUE);
+        Solution solution2 = 풀이1.일반_솔루션_생성(null, problem2, member, 0, DFS, QUEUE);
+        Solution solution3 = 풀이1.일반_솔루션_생성(null, problem3, member, 0, BFS, ARRAY);
+        Solution solution4 = 풀이1.일반_솔루션_생성(null, problem4, member, 0, DFS, ARRAY);
+
+        solutionRepository.save(solution1);
+        solutionRepository.save(solution2);
+        solutionRepository.save(solution3);
+        solutionRepository.save(solution4);
+
+        // when
+        List<Problem> result1 = problemRepository.searchByAlgorithmAndDataStructureOrderByDateDesc(
+                1, null, BFS, "date"
+        );
+        List<Problem> result2 = problemRepository.searchByAlgorithmAndDataStructureOrderByDateDesc(
+                1, null, DFS, "date"
+        );
+        List<Problem> result3 = problemRepository.searchByAlgorithmAndDataStructureOrderByDateDesc(
+                1, QUEUE, BFS, "date"
+        );
+
+        // then
+        Assertions.assertThat(result1).contains(problem1,problem3);
+        Assertions.assertThat(result1).doesNotContain(problem2,problem4);
+        Assertions.assertThat(result2).contains(problem2,problem4);
+        Assertions.assertThat(result2).doesNotContain(problem1,problem3);
+        Assertions.assertThat(result3).contains(problem1);
+        Assertions.assertThat(result3).doesNotContain(problem2,problem3,problem4);
+    }
+
 
 
 }
