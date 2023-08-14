1d8c608320282cf25470eebb3fd483febe71597a
jidam <jidamine87593@gmail.com>
2023-08-14 17:45:46 +0900

    test: ProblemRepositoryTest 문제 목록 조회 페이징 테스트 추가
    Related to : #9

--- backend/src/test/java/com/prograngers/backend/repository/problem/ProblemRepositoryTest.java
+++ backend/src/test/java/com/prograngers/backend/repository/problem/ProblemRepositoryTest.java
@@ -139,4 +139,63 @@ class ProblemRepositoryTest {
         Assertions.assertThat(result3).contains(problem1);
         Assertions.assertThat(result3).doesNotContain(problem2, problem3, problem4);
     }
+
+    @DisplayName("문제 목록 조회 시 페이지에 맞는 문제를 가져온다")
+    @Test
+    void 문제_목록_조회_페이징(){
+        // given
+        // 문제 : 문제3 ~ 문제1 순서로 최신
+        Problem problem1 = 문제1.아이디_값_지정_문제_생성(null);
+        Problem problem2 = 문제2.아이디_값_지정_문제_생성(null);
+        Problem problem3 = 문제2.아이디_값_지정_문제_생성(null);
+        Problem problem4 = 문제2.아이디_값_지정_문제_생성(null);
+        Problem problem5 = 문제2.아이디_값_지정_문제_생성(null);
+        Problem problem6 = 문제3.아이디_값_지정_문제_생성(null);
+        Problem problem7 = 문제3.아이디_값_지정_문제_생성(null);
+        Problem problem8 = 문제3.아이디_값_지정_문제_생성(null);
+        Problem problem9 = 문제3.아이디_값_지정_문제_생성(null);
+
+        problemRepository.save(problem1);
+        problemRepository.save(problem2);
+        problemRepository.save(problem3);
+        problemRepository.save(problem4);
+        problemRepository.save(problem5);
+        problemRepository.save(problem6);
+        problemRepository.save(problem7);
+        problemRepository.save(problem8);
+        problemRepository.save(problem9);
+
+        //  풀이 : 문제를 풀이랑 조인해서 가져오기 때문에 풀이도 필요하다
+        Solution solution1 = 풀이1.일반_솔루션_생성(null, problem1, null, 0, null, null);
+        Solution solution2 = 풀이1.일반_솔루션_생성(null, problem2, null, 0, null, null);
+        Solution solution3 = 풀이1.일반_솔루션_생성(null, problem3, null, 0, null, null);
+        Solution solution4 = 풀이1.일반_솔루션_생성(null, problem4, null, 0, null, null);
+        Solution solution5 = 풀이1.일반_솔루션_생성(null, problem5, null, 0, null, null);
+        Solution solution6 = 풀이1.일반_솔루션_생성(null, problem6, null, 0, null, null);
+        Solution solution7 = 풀이1.일반_솔루션_생성(null, problem7, null, 0, null, null);
+        Solution solution8 = 풀이1.일반_솔루션_생성(null, problem8, null, 0, null, null);
+        Solution solution9 = 풀이1.일반_솔루션_생성(null, problem9, null, 0, null, null);
+
+        solutionRepository.save(solution1);
+        solutionRepository.save(solution2);
+        solutionRepository.save(solution3);
+        solutionRepository.save(solution4);
+        solutionRepository.save(solution5);
+        solutionRepository.save(solution6);
+        solutionRepository.save(solution7);
+        solutionRepository.save(solution8);
+        solutionRepository.save(solution9);
+
+
+        // when
+        List<Problem> result1 = problemRepository.searchByAlgorithmAndDataStructureOrderByDateDesc(1, null, null,"date");
+        List<Problem> result2 = problemRepository.searchByAlgorithmAndDataStructureOrderByDateDesc(2, null, null,"date");
+        List<Problem> result3 = problemRepository.searchByAlgorithmAndDataStructureOrderByDateDesc(3, null, null,"date");
+
+        // then
+        Assertions.assertThat(result1).contains(problem9,problem8,problem7,problem6).doesNotContain(problem1,problem2,problem3,problem4,problem5);
+        Assertions.assertThat(result2).contains(problem2,problem3,problem4,problem5).doesNotContain(problem1,problem6,problem7,problem8,problem9);
+        Assertions.assertThat(result3).contains(problem1)
+                .doesNotContain(problem2,problem3,problem4,problem5,problem6,problem7,problem8,problem9);
+    }
 }
