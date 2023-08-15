ca4e237f16b46cd4feefa4ee583af101dabd21fd
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-16 03:53:46 +0900

    bug: 순환 참조 오류 해결
    webconfig -> querydsl predicate argument resolver -> enable web mvc configuration 순환 참조 해결입니다.
    
    Related to: #13

--- backend/src/main/java/com/prograngers/backend/config/WebConfig.java
+++ backend/src/main/java/com/prograngers/backend/config/WebConfig.java
@@ -1,9 +1,10 @@
 package com.prograngers.backend.config;
 
+import com.prograngers.backend.controller.auth.AuthInterceptor;
+import com.prograngers.backend.controller.auth.LoggedInArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,15 +14,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    private final HandlerInterceptor interceptor;
-    private final List<HandlerMethodArgumentResolver> resolvers;
+    private final AuthInterceptor authInterceptor;
+    private final LoggedInArgumentResolver loggedInArgumentResolver;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor);
+        registry.addInterceptor(authInterceptor);
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.addAll(this.resolvers);
+        resolvers.add(loggedInArgumentResolver);
     }
 }
