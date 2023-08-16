4227ba190ddd7e9f8120923ed125320b60f82c7b
jidam <jidamine87593@gmail.com>
2023-08-16 15:37:17 +0900

    refactor: dto에서는 dto로 변환만 하고 핵심 비즈니스 로직은 서비스에 있도록 수정
    ProblemService로 알고리즘, 자료구조 3개를 찾는 로직을 옮겼다
    
    Related to : #9

--- backend/src/main/java/com/prograngers/backend/dto/problem/response/ProblemResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/problem/response/ProblemResponse.java
@@ -21,41 +21,13 @@ public class ProblemResponse {
     List<Object> tags = new ArrayList<>();
 
 
-    public static List<ProblemResponse> from(List<Problem> results) {
-        // 반환할 dto 리스트
-        List<ProblemResponse> problemResponses = new ArrayList<>();
+    public static ProblemResponse from(Problem problem) {
+        ProblemResponse problemResponse = ProblemResponse.builder()
+                .title(problem.getTitle())
+                .ojName(problem.getOjName())
+                .tags(new ArrayList<>())
+                .build();
 
-        // 결과를  for문 돌면서 반환 dto를 만든다
-        for (Problem result : results) {
-            ProblemResponse problemResponse = ProblemResponse.builder()
-                    .title(result.getTitle())
-                    .ojName(result.getOjName())
-                    .tags(new ArrayList<>())
-                    .build();
-
-            List<Solution> solutions = result.getSolutions();
-            HashMap<Object,Integer> solutionAlgorithmCountMap = new HashMap<>();
-
-            for (Solution solution : solutions) {
-                solutionAlgorithmCountMap.put(solution.getAlgorithm(),solutionAlgorithmCountMap.getOrDefault(solution.getAlgorithm(),1)+1);
-                solutionAlgorithmCountMap.put(solution.getDataStructure(),solutionAlgorithmCountMap.getOrDefault(solution.getDataStructure(),1)+1);
-            }
-
-            List<Object> keySet = new ArrayList<>(solutionAlgorithmCountMap.keySet());
-            keySet.sort((num1,num2)->solutionAlgorithmCountMap.get(num2).compareTo(solutionAlgorithmCountMap.get(num1)));
-
-            for (int i=0; i<keySet.size(); i++){
-                if (i==3) break;
-                Object tag = keySet.get(i);
-                if (tag!=null){
-                    problemResponse.getTags().add(keySet.get(i));
-                }
-            }
-
-
-            problemResponses.add(problemResponse);
-        }
-
-        return problemResponses;
+        return problemResponse;
     }
 }
--- backend/src/main/java/com/prograngers/backend/service/ProblemService.java
+++ backend/src/main/java/com/prograngers/backend/service/ProblemService.java
@@ -2,6 +2,7 @@ package com.prograngers.backend.service;
 
 import com.prograngers.backend.dto.problem.response.ProblemResponse;
 import com.prograngers.backend.entity.Problem;
+import com.prograngers.backend.entity.Solution;
 import com.prograngers.backend.entity.constants.AlgorithmConstant;
 import com.prograngers.backend.entity.constants.DataStructureConstant;
 import com.prograngers.backend.repository.problem.ProblemRepository;
@@ -10,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -26,7 +29,29 @@ public class ProblemService {
             DataStructureConstant dataStructure,
             String sortBy) {
         List<Problem> problems = problemRepository.findAll(page, dataStructure, algorithm, sortBy);
-        List<ProblemResponse> problemResponses = ProblemResponse.from(problems);
+        // 반환할 dto 리스트
+        List<ProblemResponse> problemResponses = new ArrayList<>();
+        // 결과를  for문 돌면서 반환 dto를 만든다
+        for (Problem problem : problems) {
+            ProblemResponse problemResponse = ProblemResponse.from(problem);
+            List<Solution> solutions = problem.getSolutions();
+            HashMap<Object, Integer> solutionAlgorithmCountMap = new HashMap<>();
+            for (Solution solution : solutions) {
+                solutionAlgorithmCountMap.put(solution.getAlgorithm(), solutionAlgorithmCountMap.getOrDefault(solution.getAlgorithm(), 1) + 1);
+                solutionAlgorithmCountMap.put(solution.getDataStructure(), solutionAlgorithmCountMap.getOrDefault(solution.getDataStructure(), 1) + 1);
+            }
+            List<Object> keySet = new ArrayList<>(solutionAlgorithmCountMap.keySet());
+            keySet.sort((num1, num2) -> solutionAlgorithmCountMap.get(num2).compareTo(solutionAlgorithmCountMap.get(num1)));
+
+            for (int i = 0; i < keySet.size(); i++) {
+                if (i == 3) break;
+                Object tag = keySet.get(i);
+                if (tag != null) {
+                    problemResponse.getTags().add(keySet.get(i));
+                }
+            }
+            problemResponses.add(problemResponse);
+        }
         return problemResponses;
     }
 }
