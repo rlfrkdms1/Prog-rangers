2e73c7ac14bfcadcb5351ab97fd04b828dc3e0fc
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-17 03:37:05 +0900

    chore: database init.sql 작성
    초기 작성으로 회의 전 임시 sql 문입니다.
    
    Related to: #18

---
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,106 @@
+CREATE TABLE IF NOT EXISTS `convenience` (
+    `id`            bigint       NOT NULL AUTO_INCREMENT,
+    `is_free`       TINYINT(1)   NOT NULL,
+    `price`         int          NOT NULL,
+    `usage`         varchar(255) NOT NULL,
+    `room_id`       bigint,
+    `studycafe_id`  bigint,
+    `name`          varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB
+    AUTO_INCREMENT = 1
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `comment` (
+    `id`           bigint       NOT NULL AUTO_INCREMENT,
+    `date`         date,
+    `fixed`        TINYINT(1)   NOT NULL, -- is_fixed는 별로 ?
+    `content`      varchar(255),
+    `mention`      varchar(255),
+    `group_number` int,
+    `order_child`  int,
+    `order_parent` int,
+    `member_id`    bigint,
+    `parent_id`    bigint,
+    `solution_id`  bigint,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `likes` (
+    `id`          bigint NOT NULL AUTO_INCREMENT,
+    `member_id`   bigint,
+    `solution_id` bigint,
+     PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `member` (
+    `id`           bigint       NOT NULL AUTO_INCREMENT,
+    `email`        varchar(255),
+    `github`       varchar(255),
+    `introduction` varchar(255),
+    `name`         varchar(255),
+    `nickname`     varchar(255),
+    `password`     varchar(255),
+    `phone_number` varchar(255),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `problem` (
+    `id`           bigint       NOT NULL AUTO_INCREMENT,
+    `link`    varchar(255),
+    `oj_name` varchar(255),
+    `title`   varchar(255),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `review` (
+    `id`               bigint       NOT NULL AUTO_INCREMENT,
+    `code_line_number` int,
+    `date`             date,
+    `order_child`      int,
+    `order_parent`     int,
+    `content`          varchar(255),
+    `mention`          varchar(255),
+    `member_id`        bigint,
+    `parent_id`        bigint,
+    `solution_id`      bigint,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `solution` (
+    `id`                bigint       NOT NULL AUTO_INCREMENT,
+    `date`              date,
+    `is_public`         TINYINT(1)   NOT NULL,
+    `scraps`            int,
+    `algorithm_id`      bigint,
+    `data_structure_id` bigint,
+    `member_id`         bigint,
+    `problem_id`        bigint,
+    `scrap_id`          bigint,
+    `code`              varchar(255),
+    `description`       varchar(255),
+    `level`             varchar(255),
+    `title`             varchar(255),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+
+
