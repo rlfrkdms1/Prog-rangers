c2261f7bc1065d376cfd95454f601c2c8b37a9e2
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-16 23:20:39 +0900

    feat: oauth 관련 constant 분리
    oauth 관련 constant를 분리했습니다.
    
    Related to: #13

---
+++ backend/src/main/java/com/prograngers/backend/service/auth/OauthConstant.java
@@ -0,0 +1,12 @@
+package com.prograngers.backend.service.auth;
+
+public class OauthConstant {
+
+    public static final String GRANT_TYPE = "grant_type";
+    public static final String CLIENT_ID = "client_id";
+    public static final String REDIRECT_URI = "redirect_uri";
+    public static final String CODE = "code";
+    public static final String CLIENT_SECRET = "client_secret";
+    public static final String BEARER_FORMAT = "Bearer %s";
+
+}
