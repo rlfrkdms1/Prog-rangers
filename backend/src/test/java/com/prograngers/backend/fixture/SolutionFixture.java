e01ed20f83a9394d6a5f52a98333e0723ecc2067
jidam <jidamine87593@gmail.com>
2023-08-21 10:17:42 +0900

    fix: enum 타입 수정에 따른 Fixture 수정
    Related to : #23

--- backend/src/test/java/com/prograngers/backend/fixture/SolutionFixture.java
+++ backend/src/test/java/com/prograngers/backend/fixture/SolutionFixture.java
@@ -14,23 +14,23 @@ import java.util.List;
 
 @AllArgsConstructor
 public enum SolutionFixture {
-    풀이1("풀이제목1", true, "풀이코드1", "풀이설명1", null, LevelConstant.ONE, LocalDateTime.now()),
-    풀이2("풀이제목2", true, "풀이코드2", "풀이설명2", null, LevelConstant.ONE, LocalDateTime.now().plusDays(1)),
-    풀이3("풀이제목3", true, "풀이코드3", "풀이설명3", null, LevelConstant.ONE, LocalDateTime.now().plusDays(2)),
+    풀이1("풀이제목1", true, "풀이코드1", "풀이설명1", null, 1, LocalDateTime.now()),
+    풀이2("풀이제목2", true, "풀이코드2", "풀이설명2", null, 1, LocalDateTime.now().plusDays(1)),
+    풀이3("풀이제목3", true, "풀이코드3", "풀이설명3", null,1, LocalDateTime.now().plusDays(2)),
 
-    풀이4("풀이제목4", true, "풀이코드4", "풀이설명4", null, LevelConstant.ONE, LocalDateTime.now().plusDays(3)),
-    풀이5("풀이제목5", true, "풀이코드5", "풀이설명5", null, LevelConstant.ONE, LocalDateTime.now().plusDays(4)),
-    풀이6("풀이제목6", true, "풀이코드6", "풀이설명6", null, LevelConstant.ONE, LocalDateTime.now().plusDays(5)),
+    풀이4("풀이제목4", true, "풀이코드4", "풀이설명4", null, 1, LocalDateTime.now().plusDays(3)),
+    풀이5("풀이제목5", true, "풀이코드5", "풀이설명5", null, 1, LocalDateTime.now().plusDays(4)),
+    풀이6("풀이제목6", true, "풀이코드6", "풀이설명6", null, 1, LocalDateTime.now().plusDays(5)),
 
-    풀이7("풀이제목7", true, "풀이코드7", "풀이설명7", null, LevelConstant.ONE, LocalDateTime.now().plusDays(6)),
-    풀이8("풀이제목8", true, "풀이코드8", "풀이설명8", null, LevelConstant.ONE, LocalDateTime.now().plusDays(7)),
-    풀이9("풀이제목9", true, "풀이코드9", "풀이설명9", null, LevelConstant.ONE, LocalDateTime.now().plusDays(8));
+    풀이7("풀이제목7", true, "풀이코드7", "풀이설명7", null, 1, LocalDateTime.now().plusDays(6)),
+    풀이8("풀이제목8", true, "풀이코드8", "풀이설명8", null,1, LocalDateTime.now().plusDays(7)),
+    풀이9("풀이제목9", true, "풀이코드9", "풀이설명9", null, 1, LocalDateTime.now().plusDays(8));
     private final String title;
     private final boolean isPublic;
     private final String code;
     private final String description;
     private final List<Likes> likes;
-    private final Integer level
+    private final Integer level;
     private final LocalDateTime date;
 
     public Solution.SolutionBuilder 기본_정보_빌더_생성() {
