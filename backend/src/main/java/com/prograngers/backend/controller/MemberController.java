714291fb220fae19b228864f66734ae10e5a4fef
jidam <jidamine87593@gmail.com>
2023-08-23 21:48:21 +0900

    feat: MemberController reqeustmapping 수정, 타인 프로필 보기 기능 추가
    1. RequestMapping을 members로 수정
    2. 타인 프로필 보기 위한 컨트롤러 메소드 추가
    
    Related to : #42

--- backend/src/main/java/com/prograngers/backend/controller/MemberController.java
+++ backend/src/main/java/com/prograngers/backend/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,23 +19,30 @@ import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/prog-rangers/mypage/account-settings")
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
-    private static final String MEMBER_ACCOUNT_SETTINGS_REDIRECT_URI = "/prog-rangers/mypage/account-settings";
+    private static final String MEMBER_ACCOUNT_SETTINGS_REDIRECT_URI = "/prog-rangers/members/mypage/account-settings";
 
-    @GetMapping
+    @GetMapping("/mypage/account-settings")
     @Login
     public MemberAccountInfoResponse showAccountInfo(@LoggedInMember Long memberId) {
         return memberService.getMemberAccount(memberId);
     }
 
-    @PostMapping
+    @PostMapping("/mypage/account-settings")
     @Login
     public ResponseEntity<Void> updateMemberAccountInfo(@LoggedInMember Long memberId, UpdateMemberAccountInfoRequest updateMemberAccountInfoRequest) {
         memberService.updateMemberAccountInfo(memberId, updateMemberAccountInfoRequest);
         return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(MEMBER_ACCOUNT_SETTINGS_REDIRECT_URI)).build();
+    }
+
+    // 타인 프로필 보기
+    @GetMapping("profile/{memberId}")
+    public ResponseEntity<?> memberProfile(@PathVariable Long memberId){
+        MemberProfileResponse memberProfileResponse = memberService.getMemberProfile(memberId);
+        return ResponseEntity.ok().body(memberProfileResponse);
     }
 
 }
