e1029246639995e37695c2fcce6047068e5717bb
jidam <jidamine87593@gmail.com>
2023-08-23 18:20:16 +0900

    add frontend

---
+++ frontend/Prog-rangers-main/src/components/AfterLoginNav.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import { navStyle, linkDefault } from '../styles/headerStyle';
+
+const AfterLoginNav = () => {
+  return (
+    <div className="AfterLoginNav" css={navStyle}>
+      <Link to="problems" css={linkDefault}>
+        풀이 보러가기
+      </Link>
+      <Link to="registerReview" css={linkDefault}>
+        풀이 쓰러가기
+      </Link>
+      <Link to="myPage" css={linkDefault}>
+        마이페이지
+      </Link>
+    </div>
+  );
+};
+
+export default AfterLoginNav;
