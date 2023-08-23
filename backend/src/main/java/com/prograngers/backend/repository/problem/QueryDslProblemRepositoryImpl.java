e026766de9a85317a99e854f42e9e4b8c5c84da6
jidam <jidamine87593@gmail.com>
2023-08-23 10:50:45 +0900

    fix: Problem 목록보기에서 solution의 가장 최근 날짜에 따라 정렬되도록 수정
    Related to : #24

--- backend/src/main/java/com/prograngers/backend/repository/problem/QueryDslProblemRepositoryImpl.java
+++ backend/src/main/java/com/prograngers/backend/repository/problem/QueryDslProblemRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.prograngers.backend.repository.problem;
 
 import com.prograngers.backend.entity.Problem;
+import com.prograngers.backend.entity.QSolution;
 import com.prograngers.backend.entity.constants.AlgorithmConstant;
 import com.prograngers.backend.entity.constants.DataStructureConstant;
 import com.prograngers.backend.entity.constants.SortConstant;
@@ -52,20 +53,17 @@ public class QueryDslProblemRepositoryImpl implements QueryDslProblemRepository 
                 .where(dataStructureEq(dataStructure), algorithmEq(algorithm))
                 .fetchCount();
 
+
         log.info("size : {}",size);
 
         return new PageImpl<>(results,pageable,size);
     }
 
     private OrderSpecifier<?> orderCondition(SortConstant orderBy) {
-        if (orderBy.equals(NEWEST)) { // date 인 경우
-            log.info("orderByDate");
-            return problem.id.desc();
-            // size-1의 solution (제일 마지막 solution)의 날짜  기준으로 정렬
-//            NumberExpression<Integer> size = problem.solutions.size().subtract(1);
-//           return problem.solutions.get(size).date.desc();
-        } else if (orderBy.equals(SOLUTIONS)){ // solution 개수인 경우
-            // solution 개수에 따라 내림차순으로 정렬
+        if (orderBy.equals(NEWEST)) {
+            return
+                    solution.createdDate.desc();
+        } else if (orderBy.equals(SOLUTIONS)){
             log.info("orderBySolutionCount");
             return problem.solutions.size().desc();
         }
