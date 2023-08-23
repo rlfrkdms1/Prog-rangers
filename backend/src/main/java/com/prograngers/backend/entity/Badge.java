768383f2c8729d59b5ba5777867267e85f373aa8
jidam <jidamine87593@gmail.com>
2023-08-23 22:26:21 +0900

    feat: Badge Entity와 BadgeConstant 추가
    Related to : #42

---
+++ backend/src/main/java/com/prograngers/backend/entity/Badge.java
@@ -0,0 +1,26 @@
+package com.prograngers.backend.entity;
+
+import com.prograngers.backend.entity.constants.BadgeConstant;
+import com.prograngers.backend.entity.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Badge {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    BadgeConstant badgeType;
+}
