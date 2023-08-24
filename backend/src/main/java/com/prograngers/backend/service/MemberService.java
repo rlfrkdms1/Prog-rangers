7393c95b87e9ea9113502038304d37b6a6c2df66
jidam <jidamine87593@gmail.com>
2023-08-24 11:06:11 +0900

    feat: MemberService에 프로필 보기 기능 추가
    Related to : #46

--- backend/src/main/java/com/prograngers/backend/service/MemberService.java
+++ backend/src/main/java/com/prograngers/backend/service/MemberService.java
@@ -1,21 +1,34 @@
 package com.prograngers.backend.service;
 
+import com.prograngers.backend.dto.member.response.MemberProfileResponse;
 import com.prograngers.backend.dto.request.UpdateMemberAccountInfoRequest;
 import com.prograngers.backend.dto.response.member.MemberAccountInfoResponse;
+import com.prograngers.backend.entity.Badge;
+import com.prograngers.backend.entity.Solution;
 import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.exception.badrequest.BlankNicknameException;
 import com.prograngers.backend.exception.notfound.MemberNotFoundException;
 import com.prograngers.backend.exception.unauthorization.AlreadyExistNicknameException;
 import com.prograngers.backend.exception.unauthorization.IncorrectPasswordException;
+import com.prograngers.backend.repository.badge.BadgeRepository;
+import com.prograngers.backend.repository.follow.FollowRepository;
 import com.prograngers.backend.repository.member.MemberRepository;
+import com.prograngers.backend.repository.solution.SolutionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final BadgeRepository badgeRepository;
+
+    private final SolutionRepository solutionRepository;
+
+    private final FollowRepository followRepository;
 
     public MemberAccountInfoResponse getMemberAccount(Long memberId){
         return MemberAccountInfoResponse.from(findById(memberId));
@@ -54,5 +67,15 @@ public class MemberService {
     private void validNicknameDuplication(String nickname) {
         if(memberRepository.findByNickname(nickname).isPresent())
             throw new AlreadyExistNicknameException();
+    }
+
+    public MemberProfileResponse getMemberProfile(Long memberId) {
+        Member member = findById(memberId);
+        List<Badge> badges = badgeRepository.findAllByMember(member);
+        List<Solution> solutions = solutionRepository.findProfileSolutions(memberId);
+        Long follow = followRepository.getFollow(member);
+        Long following = followRepository.getFollowing(member);
+
+        MemberProfileResponse.from(member,badges,solutions);
     }
 }
