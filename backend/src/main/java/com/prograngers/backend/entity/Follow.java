5e29c3ef925f05ea1aa8fcb1efcdcfacffdd2d2e
jidam <jidamine87593@gmail.com>
2023-08-24 10:31:31 +0900

    feat: Follow Entity 추가
    Related to : #42

---
+++ backend/src/main/java/com/prograngers/backend/entity/Follow.java
@@ -0,0 +1,20 @@
+package com.prograngers.backend.entity;
+
+import com.prograngers.backend.entity.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+    private Long targetId;
+}
