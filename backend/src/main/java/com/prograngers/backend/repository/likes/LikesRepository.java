6b02ab71e23791d370e95f47a38a70d83cbcc6dd
jidam <jidamine87593@gmail.com>
2023-08-25 15:41:22 +0900

    feat: 좋아요 누르기, 취소 리포지토리 추가
    Related to : #55

--- backend/src/main/java/com/prograngers/backend/repository/likes/LikesRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/likes/LikesRepository.java
@@ -2,10 +2,12 @@ package com.prograngers.backend.repository.likes;
 
 import com.prograngers.backend.entity.Likes;
 import com.prograngers.backend.entity.Solution;
+import com.prograngers.backend.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface LikesRepository extends JpaRepository<Likes,Long> {
     public List<Likes> findAllBySolution(Solution solution);
+    public Likes findByMemberAndSolution(Member member, Solution solution);
 }
