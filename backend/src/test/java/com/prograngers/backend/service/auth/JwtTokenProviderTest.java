d064cf7ae00690ae2efee8d574613c933fbff3a0
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-26 00:09:55 +0900

    test: 토큰을 만들때 issuer가 없으면 예외 반환 테스트
    Related to: #56

---
+++ backend/src/test/java/com/prograngers/backend/service/auth/JwtTokenProviderTest.java
@@ -0,0 +1,30 @@
+package com.prograngers.backend.service.auth;
+
+
+import com.prograngers.backend.exception.unauthorization.MissingIssuerTokenException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class JwtTokenProviderTest {
+
+    private final JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(
+            "testtoken1111111111111111test1111111111111111test1111111111111111",
+            360000
+    );
+
+    private final FakeJwtTokenProvider fakeJwtTokenProvider = new FakeJwtTokenProvider(
+            "testtoken1111111111111111test1111111111111111test1111111111111111",
+            360000
+    );
+
+    @Test
+    @DisplayName("issuer를 넣지 않으면 ")
+    public void 토큰_생성자가_없으면_예외를_반환한다(){
+        String accessTokenWithoutIssuer = fakeJwtTokenProvider.createAccessTokenWithoutIssuer(1L);
+        assertThatThrownBy(() -> jwtTokenProvider.validToken(accessTokenWithoutIssuer)).isExactlyInstanceOf(MissingIssuerTokenException.class);
+
+    }
+
+}
