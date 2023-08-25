d4507d6dea445597c2e5278addd0ba4cca42ec7d
jidam <jidamine87593@gmail.com>
2023-08-25 15:41:30 +0900

    feat: 좋아요 누르기, 취소 서비스  추가
    Related to : #55

---
+++ backend/src/main/java/com/prograngers/backend/service/LikesService.java
@@ -0,0 +1,39 @@
+package com.prograngers.backend.service;
+
+import com.prograngers.backend.entity.Likes;
+import com.prograngers.backend.entity.Solution;
+import com.prograngers.backend.entity.member.Member;
+import com.prograngers.backend.exception.notfound.MemberNotFoundException;
+import com.prograngers.backend.exception.notfound.SolutionNotFoundException;
+import com.prograngers.backend.repository.likes.LikesRepository;
+import com.prograngers.backend.repository.member.MemberRepository;
+import com.prograngers.backend.repository.solution.SolutionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class LikesService {
+    private final LikesRepository likesRepository;
+    private final SolutionRepository solutionRepository;
+    private final MemberRepository memberRepository;
+
+    public void pushLike(Long memberId, Long solutionId) {
+        Solution targetSolution = solutionRepository.findById(solutionId).orElseThrow(SolutionNotFoundException::new);
+        Member targetMember = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+        Likes likes = Likes.builder()
+                .solution(targetSolution)
+                .member(targetMember)
+                .build();
+        likesRepository.save(likes);
+    }
+
+    public void cancelLike(Long memberId, Long solutionId) {
+        Solution targetSolution = solutionRepository.findById(solutionId).orElseThrow(SolutionNotFoundException::new);
+        Member targetMember = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+        Likes targetLikes = likesRepository.findByMemberAndSolution(targetMember, targetSolution);
+        likesRepository.delete(targetLikes);
+    }
+}
