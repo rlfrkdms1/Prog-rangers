d7d1ec5a26222a10a5dd8ada888742dca1836dd0
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-26 00:13:52 +0900

    test: issuer가 잘못된 경우 예외 반환 테스트
    Related to: #56

--- backend/src/test/java/com/prograngers/backend/service/auth/JwtTokenProviderTest.java
+++ backend/src/test/java/com/prograngers/backend/service/auth/JwtTokenProviderTest.java
@@ -1,6 +1,7 @@
 package com.prograngers.backend.service.auth;
 
 
+import com.prograngers.backend.exception.unauthorization.IncorrectIssuerTokenException;
 import com.prograngers.backend.exception.unauthorization.MissingIssuerTokenException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,11 +21,19 @@ class JwtTokenProviderTest {
     );
 
     @Test
-    @DisplayName("issuer를 넣지 않으면 ")
+    @DisplayName("issuer를 넣지 않으면 MissingIssuerTokenException을 반환한다. ")
     public void 토큰_생성자가_없으면_예외를_반환한다(){
         String accessTokenWithoutIssuer = fakeJwtTokenProvider.createAccessTokenWithoutIssuer(1L);
-        assertThatThrownBy(() -> jwtTokenProvider.validToken(accessTokenWithoutIssuer)).isExactlyInstanceOf(MissingIssuerTokenException.class);
+        assertThatThrownBy(() -> jwtTokenProvider.validToken(accessTokenWithoutIssuer))
+                .isExactlyInstanceOf(MissingIssuerTokenException.class);
+    }
 
+    @Test
+    @DisplayName("issuer가 잘못되면 IncorrectIssuerTokenException을 반환한다.")
+    public void 토큰_생성자가_틀리면_예외를_반환한다(){
+        String accessTokenWithWrongIssuer = fakeJwtTokenProvider.createAccessTokenWithIssuer(1L, "fake");
+        assertThatThrownBy(() -> jwtTokenProvider.validToken(accessTokenWithWrongIssuer))
+                .isExactlyInstanceOf(IncorrectIssuerTokenException.class);
     }
 
 }
