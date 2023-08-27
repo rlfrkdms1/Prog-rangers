39b9fbdffcfdb556c175900d520ce2dc1253414e
jidam <jidamine87593@gmail.com>
2023-08-27 19:10:48 +0900

    test: MemberFixture 수정
    Related to : #59

--- backend/src/test/java/com/prograngers/backend/fixture/MemberFixture.java
+++ backend/src/test/java/com/prograngers/backend/fixture/MemberFixture.java
@@ -3,44 +3,19 @@ package com.prograngers.backend.fixture;
 import com.prograngers.backend.entity.member.Member;
 import lombok.AllArgsConstructor;
 
+import static com.prograngers.backend.entity.member.Member.*;
+
 @AllArgsConstructor
 public enum MemberFixture {
-    길가은1(1L, "길가은1", "rlfrkdms1", "rlfrkdms1@naver.com", "password1"),
-    길가은2(2L, "길가은2", "rlfrkdms2", "rlfrkdms2@naver.com", "password2"),
-    길가은3(3L, "길가은3", "rlfrkdms3", "rlfrkdms3@naver.com", "password3");
 
-    private final Long id;
-    private final String name;
+    장지담("jidam99");
     private final String nickname;
-    private final String email;
 
-    private final String password;
-
-    public Member getMember() {
+    public MemberBuilder 기본_정보_빌더_생성(){
         return Member.builder()
-                .id(this.id)
-                .nickname(this.nickname)
-                .email(this.email)
-                .password(this.password)
-                .build();
+                .nickname(nickname);
     }
-
-    public Member 아이디_값_지정_멤버_생성(Long id){
-        return Member.builder()
-                .id(id)
-                .nickname(this.nickname)
-                .email(this.email)
-                .password(this.password)
-                .build();
+    public Member 기본_정보_멤버_생성(){
+        return 기본_정보_빌더_생성().build();
     }
-
-    public Member 아이디_값_지정_멤버_생성(){
-        return Member.builder()
-                .id(null)
-                .nickname(this.nickname)
-                .email(this.email)
-                .password(this.password)
-                .build();
-    }
-
 }
