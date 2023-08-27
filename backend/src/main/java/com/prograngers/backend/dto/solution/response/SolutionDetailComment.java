5c5de00a86a29035ff06721587de7cd9d478b16e
jidam <jidamine87593@gmail.com>
2023-08-27 14:26:47 +0900

    feat:  Solution 상세보기에서 댓글 dto 변경
    1. 댓글 상태가 추가됨에 따라 변경
    
    Related to : #58

--- backend/src/main/java/com/prograngers/backend/dto/solution/response/SolutionDetailComment.java
+++ backend/src/main/java/com/prograngers/backend/dto/solution/response/SolutionDetailComment.java
@@ -1,6 +1,7 @@
 package com.prograngers.backend.dto.solution.response;
 
 
+import com.prograngers.backend.entity.comment.CommentStatusContant;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,8 @@ public class SolutionDetailComment {
     String nickname;
     String content;
     String mention;
+
+    CommentStatusContant status;
 
     List<SolutionDetailComment> replies;
 }
