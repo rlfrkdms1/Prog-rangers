6db010cc2af7bd93805d6f87b424abd3d8e63352
jidam <jidamine87593@gmail.com>
2023-08-28 21:14:48 +0900

    test: 픽스처 수정에 따른 SolutionRepositoryTest 수정 완료
    Related to : #59

--- backend/src/test/java/com/prograngers/backend/repository/solution/SolutionRepositoryTest.java
+++ backend/src/test/java/com/prograngers/backend/repository/solution/SolutionRepositoryTest.java
@@ -3,12 +3,12 @@ package com.prograngers.backend.repository.solution;
 import com.prograngers.backend.TestConfig;
 import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.entity.problem.Problem;
-import com.prograngers.backend.entity.solution.DataStructureConstant;
 import com.prograngers.backend.entity.solution.Solution;
 import com.prograngers.backend.repository.member.MemberRepository;
 import com.prograngers.backend.repository.problem.ProblemRepository;
 import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +22,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.prograngers.backend.entity.constants.SortConstant.NEWEST;
+import static com.prograngers.backend.entity.constants.SortConstant.SCRAPS;
 import static com.prograngers.backend.entity.solution.AlgorithmConstant.BFS;
 import static com.prograngers.backend.entity.solution.AlgorithmConstant.DFS;
-import static com.prograngers.backend.entity.solution.DataStructureConstant.*;
 import static com.prograngers.backend.entity.solution.DataStructureConstant.ARRAY;
 import static com.prograngers.backend.entity.solution.DataStructureConstant.LIST;
 import static com.prograngers.backend.entity.solution.DataStructureConstant.QUEUE;
@@ -50,9 +50,6 @@ class SolutionRepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
-
-    @Autowired
-    private EntityManager em;
 
     @DisplayName("멤버 이름으로 풀이를 전부 찾을 수 있다")
     @Test
@@ -137,120 +134,123 @@ class SolutionRepositoryTest {
         assertThat(result2).contains(solution1, solution2).doesNotContain(solution3, solution4);
         assertThat(result3).contains(solution1).doesNotContain(solution2, solution3, solution4);
     }
-//
-//    @DisplayName("언어로 풀이 목록을 필터링해서 가져올 수 있다")
-//    @Test
-//    void 풀이_목록_조회_필터링_언어() {
-//        // given
-//        // 회원
-//        Member member = 멤버_저장(길가은1.아이디_값_지정_멤버_생성());
-//        // 문제
-//        Problem problem1 = 문제_저장(문제1.아이디_값_지정_문제_생성());
-//
-//        // 풀이
-//        Solution solution1 = 풀이_저장(풀이1.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE, JAVA));
-//        Solution solution2 = 풀이_저장(풀이1.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE, JAVA));
-//        Solution solution3 = 풀이_저장(풀이1.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE, CPP));
-//        Solution solution4 = 풀이_저장(풀이1.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE, PYTHON));
-//
-//        // when
-//        List<Solution> result1 = solutionRepository
-//                .getSolutionList(PageRequest.of(0, 4),  problem1.getId(), JAVA, null, null, NEWEST).getContent();
-//        List<Solution> result2 = solutionRepository
-//                .getSolutionList(PageRequest.of(0, 4), problem1.getId(), CPP, null, null, NEWEST).getContent();
-//        List<Solution> result3 = solutionRepository
-//                .getSolutionList(PageRequest.of(0, 4), problem1.getId(), PYTHON, null, null, NEWEST).getContent();
-//
-//        // then
-//        assertThat(result1).contains(solution1, solution2).doesNotContain(solution3, solution4);
-//        assertThat(result2).contains(solution3).doesNotContain(solution1, solution2, solution4);
-//        assertThat(result3).contains(solution4).doesNotContain(solution1, solution2, solution3);
-//    }
-//
-//    @DisplayName("페이지에 맞게 문제 목록을 조회할 수 있다")
-//    @Test
-//    void 문제_목록_조회_페이지() {
-//        // given
-//        // 회원
-//        Member member = 멤버_저장(길가은1.아이디_값_지정_멤버_생성());
-//
-//        // 문제
-//        Problem problem1 = 문제_저장(문제1.아이디_값_지정_문제_생성());
-//
-//        // 풀이 : solution9 ~ 1 순서로 최신
-//        Solution solution1 = 풀이_저장(풀이1.언어_포함_솔루션_생성(null, problem1, member, BFS, QUEUE,JAVA));
-//        Solution solution2 = 풀이_저장(풀이2.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA));
-//        Solution solution3 = 풀이_저장(풀이3.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA));
-//        Solution solution4 = 풀이_저장(풀이4.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA));
-//        Solution solution5 = 풀이_저장(풀이5.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA));
-//        Solution solution6 = 풀이_저장(풀이6.언어_포함_솔루션_생성(null, problem1, member, BFS, QUEUE,JAVA));
-//        Solution solution7 = 풀이_저장(풀이7.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA));
-//        Solution solution8 = 풀이_저장(풀이8.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA));
-//        Solution solution9 = 풀이_저장(풀이9.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA));
-//
-//        // when
-//        List<Solution> result1 = solutionRepository.getSolutionList(PageRequest.of(0, 4), problem1.getId(), null, null, null, NEWEST).getContent();
-//        List<Solution> result2 = solutionRepository.getSolutionList(PageRequest.of(1, 4), problem1.getId(), null, null, null, NEWEST).getContent();
-//        List<Solution> result3 = solutionRepository.getSolutionList(PageRequest.of(2, 4), problem1.getId(), null, null, null, NEWEST).getContent();
-//        List<Solution> result4 = solutionRepository.getSolutionList(PageRequest.of(3, 4), problem1.getId(), null, null, null, NEWEST).getContent();
-//
-//        // then
-//        org.junit.jupiter.api.Assertions
-//                        .assertAll(
-//                                ()-> assertThat(result1)
-//                                        .containsExactly(solution9,solution8,solution7,solution6)
-//                                        .doesNotContain(solution1,solution2,solution3,solution4,solution5),
-//                                ()->assertThat(result2)
-//                                        .containsExactly(solution5,solution4,solution3,solution2)
-//                                        .doesNotContain(solution1,solution6,solution7,solution8,solution9),
-//                                ()->assertThat(result3)
-//                                        .containsExactly(solution1)
-//                                        .doesNotContain(solution2,solution3,solution4,solution5,solution6,solution7,solution8,solution9),
-//                                ()->assertThat(result4.size()).isEqualTo(0)
-//                        );
-//    }
-//
-//    @DisplayName("스크랩 수에 따라 문제 목록을 정렬해서 조회할 수 있다")
-//    @Test
-//    void 스크랩_문제_정렬(){
-//        // given
-//        // 회원
-//        Member member = 멤버_저장(길가은1.아이디_값_지정_멤버_생성());
-//        // 문제
-//        Problem problem1 = 문제_저장(문제1.아이디_값_지정_문제_생성());
-//        // 풀이
-//        /**
-//         *  solution 3 : 스크랩 한 풀이 3개
-//         *  solution 2 : 스크랩 한 풀이 2개
-//         *  solution 1 : 스크랩 한 풀이 1개
-//         *  스크랩 수가 같으면 날짜수느로 정렬 (solution 9~4 순서)
-//         */
-//        Solution solution1 = 풀이_저장(풀이1.언어_포함_솔루션_생성(null, problem1, member, BFS, QUEUE,JAVA));
-//        Solution solution2 = 풀이_저장(풀이2.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA));
-//        Solution solution3 = 풀이_저장(풀이3.언어_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA));
-//        Solution solution4 = 풀이_저장(풀이4.스크랩_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA,solution1));
-//        Solution solution5 = 풀이_저장(풀이5.스크랩_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA,solution2));
-//        Solution solution6 = 풀이_저장(풀이6.스크랩_포함_솔루션_생성(null, problem1, member, BFS, QUEUE,JAVA,solution2));
-//        Solution solution7 = 풀이_저장(풀이7.스크랩_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA,solution3));
-//        Solution solution8 = 풀이_저장(풀이8.스크랩_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA,solution3));
-//        Solution solution9 = 풀이_저장(풀이9.스크랩_포함_솔루션_생성(null, problem1, member,  BFS, QUEUE,JAVA,solution3));
-//
-//        // when
-//        List<Solution> result1 = solutionRepository
-//                .getSolutionList(PageRequest.of(0, 4), problem1.getId(), null, null, null, SCRAPS)
-//                .getContent();
-//        List<Solution> result2 = solutionRepository
-//                .getSolutionList(PageRequest.of(1, 4), problem1.getId(), null, null, null, SCRAPS)
-//                .getContent();
-//        List<Solution> result3 = solutionRepository
-//                .getSolutionList(PageRequest.of(2, 4), problem1.getId(), null, null, null, SCRAPS)
-//                .getContent();
-//
-//        // then
-//        Assertions.assertThat(result1).containsExactly(solution3,solution2,solution1,solution9);
-//        Assertions.assertThat(result2).containsExactly(solution8,solution7,solution6,solution5);
-//        Assertions.assertThat(result3).containsExactly(solution4);
-//    }
+
+    @DisplayName("언어로 풀이 목록을 필터링해서 가져올 수 있다")
+    @Test
+    void 풀이_목록_조회_필터링_언어() {
+        // given
+        // 회원
+        Member member1 = 저장(장지담.기본_정보_생성());
+        // 문제
+        Problem problem1 = 백준_문제.기본_정보_생성();
+
+        // 풀이
+        Solution solution1 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now(),BFS, QUEUE,JAVA,1));
+        Solution solution2 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now(),DFS, QUEUE,JAVA,1));
+        Solution solution3 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now(),BFS, ARRAY,CPP,1));
+        Solution solution4 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now(),DFS, ARRAY,PYTHON,1));
+
+        // when
+        List<Solution> result1 = solutionRepository
+                .getSolutionList(PageRequest.of(0, 4),  problem1.getId(), JAVA, null, null, NEWEST).getContent();
+        List<Solution> result2 = solutionRepository
+                .getSolutionList(PageRequest.of(0, 4), problem1.getId(), CPP, null, null, NEWEST).getContent();
+        List<Solution> result3 = solutionRepository
+                .getSolutionList(PageRequest.of(0, 4), problem1.getId(), PYTHON, null, null, NEWEST).getContent();
+
+        // then
+        assertThat(result1).contains(solution1, solution2).doesNotContain(solution3, solution4);
+        assertThat(result2).contains(solution3).doesNotContain(solution1, solution2, solution4);
+        assertThat(result3).contains(solution4).doesNotContain(solution1, solution2, solution3);
+    }
+
+    @DisplayName("페이지에 맞게 문제 목록을 조회할 수 있다")
+    @Test
+    void 문제_목록_조회_페이지() {
+        // given
+        // 회원
+        Member member1 = 저장(장지담.기본_정보_생성());
+
+        // 문제
+        Problem problem1 = 백준_문제.기본_정보_생성();
+
+        // 풀이 : solution9 ~ 1 순서로 최신
+        // 풀이
+        Solution solution1 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now(),BFS, QUEUE,JAVA,1));
+        Solution solution2 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now().plusDays(1),DFS, QUEUE,JAVA,1));
+        Solution solution3 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now().plusDays(2),BFS, ARRAY,CPP,1));
+        Solution solution4 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now().plusDays(3),DFS, ARRAY,PYTHON,1));
+        Solution solution5 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now().plusDays(4),BFS, QUEUE,JAVA,1));
+        Solution solution6 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now().plusDays(5),DFS, QUEUE,JAVA,1));
+        Solution solution7 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now().plusDays(6),BFS, ARRAY,CPP,1));
+        Solution solution8 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now().plusDays(7),DFS, ARRAY,PYTHON,1));
+        Solution solution9 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now().plusDays(8),DFS, ARRAY,PYTHON,1));
+
+        // when
+        List<Solution> result1 = solutionRepository.getSolutionList(PageRequest.of(0, 4), problem1.getId(), null, null, null, NEWEST).getContent();
+        List<Solution> result2 = solutionRepository.getSolutionList(PageRequest.of(1, 4), problem1.getId(), null, null, null, NEWEST).getContent();
+        List<Solution> result3 = solutionRepository.getSolutionList(PageRequest.of(2, 4), problem1.getId(), null, null, null, NEWEST).getContent();
+        List<Solution> result4 = solutionRepository.getSolutionList(PageRequest.of(3, 4), problem1.getId(), null, null, null, NEWEST).getContent();
+
+        // then
+        org.junit.jupiter.api.Assertions
+                        .assertAll(
+                                ()-> assertThat(result1)
+                                        .containsExactly(solution9,solution8,solution7,solution6)
+                                        .doesNotContain(solution1,solution2,solution3,solution4,solution5),
+                                ()->assertThat(result2)
+                                        .containsExactly(solution5,solution4,solution3,solution2)
+                                        .doesNotContain(solution1,solution6,solution7,solution8,solution9),
+                                ()->assertThat(result3)
+                                        .containsExactly(solution1)
+                                        .doesNotContain(solution2,solution3,solution4,solution5,solution6,solution7,solution8,solution9),
+                                ()->assertThat(result4.size()).isEqualTo(0)
+                        );
+    }
+
+    @DisplayName("스크랩 수에 따라 문제 목록을 정렬해서 조회할 수 있다")
+    @Test
+    void 스크랩_문제_정렬(){
+        // given
+        // 회원
+        Member member1 = 저장(장지담.기본_정보_생성());
+
+        // 문제
+        Problem problem1 = 백준_문제.기본_정보_생성();
+        // 풀이
+
+        /**
+         *  solution 3 : 스크랩 한 풀이 3개
+         *  solution 2 : 스크랩 한 풀이 2개
+         *  solution 1 : 스크랩 한 풀이 1개
+         *  스크랩 수가 같으면 날짜수느로 정렬 (solution 9~4 순서)
+         */
+        Solution solution1 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now(),BFS, QUEUE,JAVA,1));
+        Solution solution2 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now().plusDays(1),DFS, QUEUE,JAVA,1));
+        Solution solution3 = 저장(퍼블릭_풀이.기본_정보_생성(problem1,member1,LocalDateTime.now().plusDays(2),BFS, ARRAY,CPP,1));
+        Solution solution4 = 저장(퍼블릭_풀이.스크랩_생성(problem1,member1,LocalDateTime.now().plusDays(3),DFS, ARRAY,PYTHON,1,solution1));
+        Solution solution5 = 저장(퍼블릭_풀이.스크랩_생성(problem1,member1,LocalDateTime.now().plusDays(4),BFS, QUEUE,JAVA,1,solution2));
+        Solution solution6 = 저장(퍼블릭_풀이.스크랩_생성(problem1,member1,LocalDateTime.now().plusDays(5),DFS, QUEUE,JAVA,1,solution2));
+        Solution solution7 = 저장(퍼블릭_풀이.스크랩_생성(problem1,member1,LocalDateTime.now().plusDays(6),BFS, ARRAY,CPP,1,solution3));
+        Solution solution8 = 저장(퍼블릭_풀이.스크랩_생성(problem1,member1,LocalDateTime.now().plusDays(7),DFS, ARRAY,PYTHON,1,solution3));
+        Solution solution9 = 저장(퍼블릭_풀이.스크랩_생성(problem1,member1,LocalDateTime.now().plusDays(8),DFS, ARRAY,PYTHON,1,solution3));
+
+        // when
+        List<Solution> result1 = solutionRepository
+                .getSolutionList(PageRequest.of(0, 4), problem1.getId(), null, null, null, SCRAPS)
+                .getContent();
+        List<Solution> result2 = solutionRepository
+                .getSolutionList(PageRequest.of(1, 4), problem1.getId(), null, null, null, SCRAPS)
+                .getContent();
+        List<Solution> result3 = solutionRepository
+                .getSolutionList(PageRequest.of(2, 4), problem1.getId(), null, null, null, SCRAPS)
+                .getContent();
+
+        // then
+        Assertions.assertThat(result1).containsExactly(solution3,solution2,solution1,solution9);
+        Assertions.assertThat(result2).containsExactly(solution8,solution7,solution6,solution5);
+        Assertions.assertThat(result3).containsExactly(solution4);
+    }
 
     Member 저장(Member member) {
         return memberRepository.save(member);
