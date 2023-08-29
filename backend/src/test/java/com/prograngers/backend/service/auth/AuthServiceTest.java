3812c8791c6e149ac8e739fb9365440495aa64ee
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-30 03:43:28 +0900

    test: 닉네임 중복검사 테스트
    Related to: #56

--- backend/src/test/java/com/prograngers/backend/service/auth/AuthServiceTest.java
+++ backend/src/test/java/com/prograngers/backend/service/auth/AuthServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static com.prograngers.backend.support.fixture.MemberFixture.길가은;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
@@ -30,12 +31,21 @@ class AuthServiceTest {
 
     @Test
     @DisplayName("같은 닉네임을 가진 회원이 이미 존재할 경우 중복 검사시 예외를 반환한다.")
-    public void 닉네임_중복검사를_할_수_있다(){
+    public void 닉네임이_중복검사시_예외를_반환할_수_있다(){
         given(memberRepository.findByNickname("rlfrkdms1")).willReturn(Optional.of(길가은.기본_정보_생성()));
         assertAll(
                 () -> assertThatThrownBy(() -> authService.validNicknameDuplication("rlfrkdms1")).isExactlyInstanceOf(AlreadyExistNicknameException.class),
                 () -> verify(memberRepository).findByNickname("rlfrkdms1")
         );
     }
+
+    @Test
+    public void 닉네임_중복검사를_할_수_있다() {
+        given(memberRepository.findByNickname("rlfrkdms1")).willReturn(Optional.empty());
+        authService.validNicknameDuplication("rlfrkdms1");
+        verify(memberRepository).findByNickname("rlfrkdms1");
+    }
+
+
 
 }
