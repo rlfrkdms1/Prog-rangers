c914ca4066df4d5f745f739766c62f6dbdb8bba0
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-03 07:29:26 +0900

    feat: 알림 엔티티 추가
    Related to: #86

---
+++ backend/src/main/java/com/prograngers/backend/entity/Notification.java
@@ -0,0 +1,62 @@
+package com.prograngers.backend.entity;
+
+import com.prograngers.backend.entity.comment.Comment;
+import com.prograngers.backend.entity.member.Member;
+import com.prograngers.backend.entity.solution.Solution;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Builder
+@Entity
+@Getter
+@NoArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "type", nullable = false)
+    private NotificationType type;
+    @CreatedDate
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private LocalDateTime createAt;
+
+    @Column(name = "id_read", nullable = false)
+    private boolean isRead;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private Member receiver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "solution_id", nullable = false)
+    private Solution solution;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+}
