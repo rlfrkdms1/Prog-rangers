1470ea8d3c7d311db96dc0fadbcb27590bff8ac9
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-04 16:09:25 +0900

    refactor: @Enumerated 누락 수정
    Related to: #86

--- backend/src/main/java/com/prograngers/backend/entity/Notification.java
+++ backend/src/main/java/com/prograngers/backend/entity/Notification.java
@@ -5,6 +5,8 @@ import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.entity.solution.Solution;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -34,8 +36,10 @@ public class Notification {
     @Column(name = "title", nullable = false)
     private String title;
 
+    @Enumerated(value = EnumType.STRING)
     @Column(name = "type", nullable = false)
     private NotificationType type;
+
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
