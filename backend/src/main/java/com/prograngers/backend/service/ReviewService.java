f8e42f99aee00aff476d92d7dbd96baac8c32245
jidam <jidamine87593@gmail.com>
2023-09-06 22:04:10 +0900

    refactor: createdAt으로 수정에 따른 수정
    Related to : #106

--- backend/src/main/java/com/prograngers/backend/service/ReviewService.java
+++ backend/src/main/java/com/prograngers/backend/service/ReviewService.java
@@ -43,7 +43,7 @@ public class ReviewService {
 
             // codeLineNumber에 해당하는 review들을 찾는다
             List<com.prograngers.backend.entity.Review> reviews = reviewRepository
-                    .findAllByCodeLineNumberOrderByCreatedDateAsc(codeLineNumber);
+                    .findAllByCodeLineNumberOrderByCreatedAtAsc(codeLineNumber);
             log.info("코드라인 : {}", codeLineNumber);
             List<Review> reviewRespons = new ArrayList<>();
 
