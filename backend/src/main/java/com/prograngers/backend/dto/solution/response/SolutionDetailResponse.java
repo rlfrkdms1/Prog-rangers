eaab901c7ffa79e9a25a66517474ce7e1dedd5dd
jidam <jidamine87593@gmail.com>
2023-09-07 15:39:40 +0900

    refactor: SolutionDetailResponse 리팩터링
    1. from에서 메서드 추출
    2. 스트림 사용하도록 리팩터링
    
    Related to : #106

--- backend/src/main/java/com/prograngers/backend/dto/solution/response/SolutionDetailResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/solution/response/SolutionDetailResponse.java
@@ -27,22 +27,42 @@ public class SolutionDetailResponse {
     private static final String SCRAP_PATH = "http://localhost:8080/prog-rangers/solutions/";
 
     public static SolutionDetailResponse from(Problem problem, Solution solution, List<Comment> comments,
-                                                  boolean scraped, int scrapCount,
-                                                  boolean pushedLike, int likeCount,
-                                                  boolean isMine
-    ) {
-
-
+                                                  boolean scraped, int scrapCount, boolean pushedLike, int likeCount, boolean isMine) {
         SolutionDetailResponse response = new SolutionDetailResponse();
+        String scrapLink = getScrapLink(solution);
+        SolutionDetailSolution responseSolution = getResponseSolution(solution, scraped, scrapCount, pushedLike, likeCount, scrapLink);
+        List<SolutionDetailComment> commentResponseList = new ArrayList<>();
+        addCommentsToResponse(comments, commentResponseList);
+        response.comments = commentResponseList;
+        response.solution = responseSolution;
+        response.isMine = isMine;
+        response.problem= SolutionDetailProblem.from(problem);
+        return response;
+    }
 
+    private static void addCommentsToResponse(List<Comment> comments, List<SolutionDetailComment> commentResponseList) {
+        // 먼저 부모가 없는 댓글들을 전부 더한다
+        comments.stream().filter(comment -> comment.getParentId()==null)
+                .forEach(comment-> commentResponseList.add( new SolutionDetailComment(
+                        comment.getMember().getPhoto(),
+                        comment.getId(),
+                        comment.getMember().getNickname(),
+                        comment.getContent(),
+                        comment.getStatus(),
+                        new ArrayList<>()
+                )));
+        // 부모가 있는 댓글들을 더한다
+        comments.stream().filter((comment)->comment.getParentId()!=null)
+                .forEach((comment)->{
+                    for (SolutionDetailComment parentComment : commentResponseList){
+                        if (parentComment.getId().equals(comment.getParentId())){
+                            parentComment.getReplies().add(new SolutionDetailComment(comment.getMember().getPhoto(), comment.getId(), comment.getMember().getNickname(), comment.getContent(), comment.getStatus(), null));
+                        }
+                    }
+                });
+    }
 
-
-        // 스크랩 한 풀이면 스크랩 한 풀이의 링크 찾기
-        String scrapLink = null;
-        if (solution.getScrapSolution() != null) {
-            scrapLink = SCRAP_PATH + solution.getScrapSolution().getId();
-        }
-
+    private static SolutionDetailSolution getResponseSolution(Solution solution, boolean scraped, int scrapCount, boolean pushedLike, int likeCount, String scrapLink) {
         SolutionDetailSolution responseSolution = new SolutionDetailSolution(
                 solution.getId(),
                 solution.getMember().getNickname(),
@@ -58,48 +78,15 @@ public class SolutionDetailResponse {
                 pushedLike,
                 scraped
         );
+        return responseSolution;
+    }
 
-        List<SolutionDetailComment> commentResponseList = new ArrayList<>();
-
-        // 먼저 부모가 없는 댓글들을 전부 더한다
-        comments.stream().filter(comment -> comment.getParentId()==null)
-                .forEach(comment->commentResponseList.add( new SolutionDetailComment(
-                        comment.getMember().getPhoto(),
-                        comment.getId(),
-                        comment.getMember().getNickname(),
-                        comment.getContent(),
-                        comment.getStatus(),
-                        new ArrayList<>()
-                )));
-
-        log.info("부모가 있는 댓글 더하기 전");
-        // 부모가 있는 댓글들을 더한다
-        for (Comment comment : comments){
-            Long parentId = comment.getParentId();
-            if (parentId!=null){
-                for (SolutionDetailComment parentComment : commentResponseList){
-                    if (parentComment.getId().equals(parentId)){
-                        parentComment.getReplies().add(
-                                new SolutionDetailComment(
-                                        comment.getMember().getPhoto(),
-                                        comment.getId(),
-                                        comment.getMember().getNickname(),
-                                        comment.getContent(),
-                                        comment.getStatus(),
-                                        null
-                                )
-                        );
-                    }
-                }
-            }
+    private static String getScrapLink(Solution solution) {
+        // 스크랩 한 풀이면 스크랩 한 풀이의 링크 찾기
+        String scrapLink = null;
+        if (solution.getScrapSolution() != null) {
+            scrapLink = SCRAP_PATH + solution.getScrapSolution().getId();
         }
-        log.info("부모가 있는 댓글 더하기 후");
-
-        response.comments = commentResponseList;
-        response.solution = responseSolution;
-        response.isMine = isMine;
-        response.problem= SolutionDetailProblem.from(problem);
-
-        return response;
+        return scrapLink;
     }
 }
