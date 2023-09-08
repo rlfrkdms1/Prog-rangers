eaf662467d8f8ffd200f62487972c37fcdf1ac60
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-09 01:31:20 +0900

    feat: 뱃지 조회 기능 추가
    Related to: #113

--- backend/src/main/java/com/prograngers/backend/service/DashBoardService.java
+++ backend/src/main/java/com/prograngers/backend/service/DashBoardService.java
@@ -4,10 +4,13 @@ import com.prograngers.backend.dto.response.dashboard.MyRecentSolutionInfo;
 import com.prograngers.backend.dto.response.dashboard.NotificationInfo;
 import com.prograngers.backend.dto.response.notification.NotificationResponse;
 import com.prograngers.backend.entity.Notification;
+import com.prograngers.backend.entity.badge.Badge;
+import com.prograngers.backend.entity.badge.BadgeConstant;
 import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.entity.solution.Solution;
 import com.prograngers.backend.exception.notfound.MemberNotFoundException;
 import com.prograngers.backend.repository.NotificationRepository;
+import com.prograngers.backend.repository.badge.BadgeRepository;
 import com.prograngers.backend.repository.member.MemberRepository;
 import com.prograngers.backend.repository.solution.SolutionRepository;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +28,7 @@ public class DashBoardService {
     private final NotificationRepository notificationRepository;
     private final MemberRepository memberRepository;
     private final SolutionRepository solutionRepository;
+    private final BadgeRepository badgeRepository;
 
 
     public void createDashBoard(Long memberId) {
@@ -35,6 +39,9 @@ public class DashBoardService {
         //최근 풀이
         List<Solution> solutions = solutionRepository.findTop3ByMemberOrderByCreatedDateDesc(member);
         List<MyRecentSolutionInfo> myRecentSolutionInfos = solutions.stream().map(solution -> MyRecentSolutionInfo.of(solution, solution.getProblem())).collect(Collectors.toList());
+        //뱃지 찾기
+        List<Badge> badges = badgeRepository.findAllByMember(member);
+        List<BadgeConstant> badgeInfos = badges.stream().map(Badge::getBadgeType).collect(Collectors.toList());
 
     }
 
