7d9648c413355f8f4d13921500632eb463481e57
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-09 01:28:55 +0900

    feat: 나의 최근 풀이 찾는 기능 추가
    Related to: #113

--- backend/src/main/java/com/prograngers/backend/service/DashBoardService.java
+++ backend/src/main/java/com/prograngers/backend/service/DashBoardService.java
@@ -1,12 +1,15 @@
 package com.prograngers.backend.service;
 
+import com.prograngers.backend.dto.response.dashboard.MyRecentSolutionInfo;
 import com.prograngers.backend.dto.response.dashboard.NotificationInfo;
 import com.prograngers.backend.dto.response.notification.NotificationResponse;
 import com.prograngers.backend.entity.Notification;
 import com.prograngers.backend.entity.member.Member;
+import com.prograngers.backend.entity.solution.Solution;
 import com.prograngers.backend.exception.notfound.MemberNotFoundException;
 import com.prograngers.backend.repository.NotificationRepository;
 import com.prograngers.backend.repository.member.MemberRepository;
+import com.prograngers.backend.repository.solution.SolutionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +24,8 @@ public class DashBoardService {
 
     private final NotificationRepository notificationRepository;
     private final MemberRepository memberRepository;
+    private final SolutionRepository solutionRepository;
+
 
     public void createDashBoard(Long memberId) {
         Member member = findMemberById(memberId);
@@ -28,6 +33,9 @@ public class DashBoardService {
         List<Notification> notifications = notificationRepository.findTop9ByReceiverOrderByCreatedAtDesc(member);
         List<NotificationInfo> notificationInfoList = notifications.stream().map(notification -> NotificationInfo.of(notification, notification.getSolution())).collect(Collectors.toList());
         //최근 풀이
+        List<Solution> solutions = solutionRepository.findTop3ByMemberOrderByCreatedDateDesc(member);
+        List<MyRecentSolutionInfo> myRecentSolutionInfos = solutions.stream().map(solution -> MyRecentSolutionInfo.of(solution, solution.getProblem())).collect(Collectors.toList());
+
     }
 
     private Member findMemberById(Long memberId){
