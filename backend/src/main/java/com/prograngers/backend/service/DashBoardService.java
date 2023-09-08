c46fc728e3683f44282cb980eeacb461b7178b41
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-09 01:14:39 +0900

    feat: 대시보드 서비스 추가 및 회원 찾기 기능 추가
    Related to: #113

---
+++ backend/src/main/java/com/prograngers/backend/service/DashBoardService.java
@@ -0,0 +1,27 @@
+package com.prograngers.backend.service;
+
+import com.prograngers.backend.dto.response.notification.NotificationResponse;
+import com.prograngers.backend.entity.member.Member;
+import com.prograngers.backend.exception.notfound.MemberNotFoundException;
+import com.prograngers.backend.repository.NotificationRepository;
+import com.prograngers.backend.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class DashBoardService {
+
+    private final NotificationRepository notificationRepository;
+    private final MemberRepository memberRepository;
+
+    public void createDashBoard(Long memberId) {
+        Member member = findMemberById(memberId);
+    }
+
+    private Member findMemberById(Long memberId){
+        return memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+    }
+}
