7be11663101cc30094d5ff3e459f1ce01029b5fd
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-09 09:46:52 +0900

    feat: 팔로우의 최근 풀이 찾는 기능 추가
    Related to: #113

--- backend/src/main/java/com/prograngers/backend/repository/solution/SolutionRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/solution/SolutionRepository.java
@@ -2,8 +2,13 @@ package com.prograngers.backend.repository.solution;
 
 import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.entity.solution.Solution;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -13,5 +18,7 @@ public interface SolutionRepository extends JpaRepository<Solution, Long>, Query
     List<Solution> findAllByScrapSolution(Solution solution);
     List<Solution> findTop3ByMemberOrderByCreatedDateDesc(Member member);
 
-
+    @Query("select s from Solution s join Follow f on s.member.id = f.followingId where f.followerId = :memberId order by s.createdDate desc limit 5")
+    List<Solution> findFollowingsRecentSolutions(@Param("memberId") Long memberId);
+    List<Solution> findAllByMemberAndCreatedDateBetween(Member member, LocalDateTime startDate, LocalDateTime endDate);
 }
