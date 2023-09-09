235e04e70e4c74d97c9f87c1d44ecd9f7ba1fd8e
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-09 13:03:51 +0900

    feat: 해당하는 달의 solution 조회 기능 추가
    Related to: #113

--- backend/src/main/java/com/prograngers/backend/repository/solution/SolutionRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/solution/SolutionRepository.java
@@ -21,4 +21,7 @@ public interface SolutionRepository extends JpaRepository<Solution, Long>, Query
     @Query("select s from Solution s join Follow f on s.member.id = f.followingId where f.followerId = :memberId order by s.createdDate desc limit 5")
     List<Solution> findFollowingsRecentSolutions(@Param("memberId") Long memberId);
     List<Solution> findAllByMemberAndCreatedDateBetween(Member member, LocalDateTime startDate, LocalDateTime endDate);
+
+    @Query("select distinct function('date_format', s.createdDate, '%d') from Solution s where s.member.id = :memberId and function('date_format', s.createdDate, '%m') = :month")
+    List<Integer> findAllByMonth(Long memberId, int month);
 }
