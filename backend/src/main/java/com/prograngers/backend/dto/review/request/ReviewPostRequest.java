95227bbe205816557d2f1e8c2b98979dd0cee5c8
jidam <jidamine87593@gmail.com>
2023-09-12 11:14:13 +0900

    feat: ReviewPostRequest에 정적 팩토리 메서드 추가
    Related to : #118

--- backend/src/main/java/com/prograngers/backend/dto/review/request/ReviewPostRequest.java
+++ backend/src/main/java/com/prograngers/backend/dto/review/request/ReviewPostRequest.java
@@ -1,5 +1,9 @@
 package com.prograngers.backend.dto.review.request;
 
+import com.prograngers.backend.entity.Review;
+import com.prograngers.backend.entity.member.Member;
+import com.prograngers.backend.entity.solution.Solution;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -7,13 +11,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReviewPostRequest {
+
+    @Nullable
+    private Long parentId;
     @NotNull
     private int codeLineNumber;
     @NotBlank
     private String content;
+
+    public Review toReview(Member member, Solution solution) {
+        return Review.builder()
+                .member(member)
+                .solution(solution)
+                .content(content)
+                .codeLineNumber(codeLineNumber)
+                .parentId(parentId)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
 }
