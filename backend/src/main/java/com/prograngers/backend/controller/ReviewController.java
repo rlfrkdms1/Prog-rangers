bc210b7fc45c61236393a9dd07ef299b89e06d5f
jidam <jidamine87593@gmail.com>
2023-09-12 11:02:02 +0900

    feat: ReviewController 리뷰 작성 추가
    Related to : #118

--- backend/src/main/java/com/prograngers/backend/controller/ReviewController.java
+++ backend/src/main/java/com/prograngers/backend/controller/ReviewController.java
@@ -1,13 +1,20 @@
 package com.prograngers.backend.controller;
 
+import com.prograngers.backend.controller.auth.LoggedInMember;
+import com.prograngers.backend.controller.auth.Login;
 import com.prograngers.backend.dto.review.response.SolutionReviewsResponse;
 import com.prograngers.backend.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("prog-rangers/solutions/{solutionId}")
@@ -16,10 +23,25 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
+    private final String REDIRECT_PATH = "http://localhost:8080/prog-rangers/solutions";
+
     // 상세보기 한 줄 리뷰
     @GetMapping("/reviews")
     public ResponseEntity<?> solutionReviews(@PathVariable Long solutionId){
         SolutionReviewsResponse reviewDetail = reviewService.getReviewDetail(solutionId);
         return ResponseEntity.ok().body(reviewDetail);
     }
+
+    @PostMapping("/reviews")
+    @Login
+    public ResponseEntity<?> writeReview(@PathVariable Long solutionId, @RequestBody ReviewPostRequest reviewPostRequest, @LoggedInMember Long memberId){
+        reviewService.writeReview(reviewPostRequest,memberId);
+        // 풀이 상세보기로 리다이렉트
+        return redirect(solutionId);
+    }
+
+    private ResponseEntity<Object> redirect(Long solutionId) {
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(REDIRECT_PATH + "/" + solutionId)).build();
+    }
+
 }
