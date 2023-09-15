ffabd9b5d5ece0b1b53ec882190f714343039d37
g2Min <jimin9320@gmail.com>
2023-09-15 09:39:43 +0900

    fix: kakao 주소수정

--- frontend/Prog-rangers-main/src/components/SignUp/OAuth.js
+++ frontend/Prog-rangers-main/src/components/SignUp/OAuth.js
@@ -1,3 +1,4 @@
-const CLIENT_ID = "d7d727c879817f72882dbf6b12c1a268";
-const REDIRECT_URI = "http://localhost:3000/login/kakao"
-const KAKAO_AUTH_URL = "https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code";
+export const KAKAO_CLIENT_ID = "d7d727c879817f72882dbf6b12c1a268";
+export const KAKAO_REDIRECT_URI = "http://localhost:3000/login/kakao"
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+ 
--- frontend/Prog-rangers-main/src/components/SignUp/SocialLoginButtons.jsx
+++ frontend/Prog-rangers-main/src/components/SignUp/SocialLoginButtons.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Kakao from '../../assets/icons/signin-kakao-logo.svg';
 import Naver from '../../assets/icons/signin-naver-logo.svg';
 import Google from '../../assets/icons/signin-google-logo.svg';
-import { KAKAO_AUTH_URL } from './OAuth';
+import { KAKAO_AUTH_URL, Link } from './OAuth';
 
 const Wrapper = css`
   display: flex;
@@ -20,7 +20,12 @@ const Wrapper = css`
   }
 `;
 
-export const SocialLoginButtons = () => {
+export const SocialLoginButtons = () => {  
+  const onClickKakao = () => {
+    window.location.href = KAKAO_AUTH_URL;
+    console.log(KAKAO_AUTH_URL);
+  }
+
   return (
   <div css={css`
     margin-top: 98px; 
@@ -39,7 +44,7 @@ export const SocialLoginButtons = () => {
     `}>소셜 로그인
     </div>
     <div css={css`display: flex; flex-direction: column; justify-content: space-between; height: 200px;`}>
-      <div css={css`${Wrapper} background-color: #FAE100;`} href={KAKAO_AUTH_LOGIN}>
+      <div css={css`${Wrapper} background-color: #FAE100;`} onClick={onClickKakao}>
         <img src={Kakao} alt="kakao_logo" css={css`margin-left: 100px;`}/>
         <div css={css` text-align: center; margin-left: 73px; color: #391E1F;`}>카카오톡으로 로그인</div>
       </div>
