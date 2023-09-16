60fb763341c1706d6684088d689862ed0afb5c7c
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-17 05:52:10 +0900

    refactor: @Param 어노테이션 변경
    Related to: #124

--- backend/src/main/java/com/prograngers/backend/repository/solution/SolutionRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/solution/SolutionRepository.java
@@ -2,9 +2,9 @@ package com.prograngers.backend.repository.solution;
 
 import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.entity.solution.Solution;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
