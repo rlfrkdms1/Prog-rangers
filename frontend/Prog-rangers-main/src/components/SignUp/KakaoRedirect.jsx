7642fe0ae59ed68f1e5178f37749b2d98e68cc2d
g2Min <jimin9320@gmail.com>
2023-09-18 00:09:19 +0900

    feat: 소셜로그인 test

--- frontend/Prog-rangers-main/src/components/SignUp/KakaoRedirect.jsx
+++ frontend/Prog-rangers-main/src/components/SignUp/KakaoRedirect.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useLocation, useNavigate } from 'react-router-dom';
 import { KAKAO_CLIENT_ID, KAKAO_REDIRECT_URI, KAKAO_AUTH_URL } from "./OAuth";
+import axios from "axios";
+import qs from "qs";
 
 export const KakaoReirect = () => {
   const location = useLocation();
@@ -8,17 +10,37 @@ export const KakaoReirect = () => {
   // let code = new URL(window.location.href).searchParams.get("code");
   const KAKAO_CODE = location.search.split('=')[1];
 
+  // const getToken = async () => {
+  //   let access_token = {
+  //     code: KAKAO_CODE,
+  //   };
+
+  //   try{
+  //     fetch("http://localhost:3000/prog-rangers/login/kakao",{
+  //       method: "POST",
+  //       // headers: {
+  //       //   "Content-Type": "application/x-www-form-urlencoded;",
+  //       // },
+  //       body : JSON.stringify(access_token)
+  //     })
+  //       // .then((res) => res.json())
+
+  //       navigate("/");
+  //     // window.Kakao.init(KAKAO_CLIENT_ID);
+  //     // window.Kakao.Auth.setAccessToken(res.data.access_token);
+  //   } catch(err){
+  //     console.log(err);
+  //   }
+  // };
+
+  const getToken = async() => {
+    const Toss = await axios.get(`http://localhost:3000/prog-rangers/login/kakao`)
+  }
 
   useEffect(() => {
-    fetch(`http://localhost:3000/signin/kakao/redirect?code=${KAKAO_CODE}`, { 
-      method: 'GET',
-    })
-      .then(res => res.json())
-      .then(data => {
-        localStorage.setItem('token', data.token);
-        navigate('/');
-      });
+    getToken();
   }, []);
+
   return(
     <>
     </>
