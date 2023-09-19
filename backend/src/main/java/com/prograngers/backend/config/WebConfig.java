eec135eac08d5bb9802565ec32c3e47d96a363e7
rlfrkdms1 <83744709+rlfrkdms1@users.noreply.github.com>
2023-09-20 02:24:38 +0900

    feat: 프론트 로컬 도메인 추가

--- backend/src/main/java/com/prograngers/backend/config/WebConfig.java
+++ backend/src/main/java/com/prograngers/backend/config/WebConfig.java
@@ -20,6 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     private final AuthInterceptor authInterceptor;
     private final LoggedInArgumentResolver loggedInArgumentResolver;
     private static final String FRONTEND_DOMAIN = "http://ec2-13-124-131-171.ap-northeast-2.compute.amazonaws.com:3000";
+    private static final String FRONT_LOCALHOST = "http://localhost:3000";
     private static final String CORS_ALLOWED_METHODS = "GET,POST,HEAD,PUT,PATCH,DELETE,TRACE,OPTIONS";
 
 
@@ -37,7 +38,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods(CORS_ALLOWED_METHODS.split(","))
-                .allowedOrigins(FRONTEND_DOMAIN)
+                .allowedOrigins(FRONTEND_DOMAIN, FRONT_LOCALHOST)
                 .exposedHeaders(HttpHeaders.SET_COOKIE, HttpHeaders.LOCATION)
                 .allowCredentials(true);
     }
