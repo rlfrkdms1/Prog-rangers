05c9981e2a1c72967863ce6d756c21e8f17d061b
rlfrkdms1 <83744709+rlfrkdms1@users.noreply.github.com>
2023-09-20 04:17:59 +0900

    fix: import 문 누락

--- backend/src/main/java/com/prograngers/backend/service/auth/oauth/KakaoOauth.java
+++ backend/src/main/java/com/prograngers/backend/service/auth/oauth/KakaoOauth.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import lombok.extern.slf4j.Slf4j;
 
 import static com.prograngers.backend.service.auth.oauth.MultiValueMapConverter.convertToMultiValueMap;
 import static com.prograngers.backend.service.auth.OauthConstant.BEARER_FORMAT;
