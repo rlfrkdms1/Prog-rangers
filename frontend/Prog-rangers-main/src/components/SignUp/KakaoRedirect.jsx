16c0ce9bc3ae8a22bdab74b749efae2dbf73b19e
g2Min <jimin9320@gmail.com>
2023-09-20 04:29:14 +0900

    test: kakao 소셜로그인

--- frontend/Prog-rangers-main/src/components/SignUp/KakaoRedirect.jsx
+++ frontend/Prog-rangers-main/src/components/SignUp/KakaoRedirect.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { useLocation, useNavigate } from 'react-router-dom';
 import { KAKAO_CLIENT_ID, KAKAO_REDIRECT_URI, KAKAO_AUTH_URL } from "./OAuth";
 import axios from "axios";
@@ -11,19 +11,36 @@ export const KakaoReirect = () => {
   const KAKAO_CODE = location.search.split('=')[1];
 
   // const getToken = async () => {
-  //   let access_token = {
-  //     code: KAKAO_CODE,
-  //   };
+
+  //   const headers = { 
+  //     'Content-Type' : 'application/json',
+  //     'Authorization' : 'authToken'
+  //   }
 
   //   try{
-  //     fetch("http://localhost:3000/prog-rangers/login/kakao",{
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/x-www-form-urlencoded;",
-  //       },
-  //       body : JSON.stringify(access_token)
-  //     })
-  //     navigate("/");
+  //     // const res = await axios.post(`http://13.124.131.171:8080/prog-rangers/login/kakao`, {
+  //     //   code: KAKAO_CODE
+  //     // }, {
+  //     //   headers: headers
+  //     // });
+  //     // console.log(res);
+      
+  //     const access_token = {
+  //       code: KAKAO_CODE
+  //     }
+  //     fetch(`http://13.124.131.171:8080/prog-rangers/login/kakao`,{
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/x-www-form-urlencoded;",
+  //         },
+  //         body : JSON.stringify(access_token),
+  //       })
+  //         .then(res => res.json())
+  //         .thens(data => {
+  //           localStorage.setItem('token', data.token);
+  //           navigate("/");
+  //         })
+
   //     // window.Kakao.init(KAKAO_CLIENT_ID);
   //     // window.Kakao.Auth.setAccessToken(res.data.access_token);
   //   } catch(err){
@@ -31,25 +48,42 @@ export const KakaoReirect = () => {
   //   }
   // };
 
-  const getToken = async() => {
-    const Toss = await axios.get(`http://13.124.131.171:8080/prog-rangers/login/kakao`, {
-      params: { code: KAKAO_CODE },
-    }, {
-      withCredentials : true 
-    })
-      .then((res) => {
-        console.log(res.data);
-      })
-    // console.log(Toss.data);
-    if(!Toss){
-      alert("존재하지 않는 회원입니다.");
-    } else{
-      navigate(`/`);
-    }
-  }
+  // const getToken = async() => {
+  //   const Toss = await axios.get(`http://13.124.131.171:8080/prog-rangers/login/kakao`, {
+  //     params: { code: KAKAO_CODE },
+  //   }, {
+  //     withCredentials : true 
+  //   })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //     })
+  //   // console.log(Toss.data);
+  //   if(!Toss){
+  //     alert("존재하지 않는 회원입니다.");
+  //   } else{
+  //     navigate(`/`);
+  //   }
+  // }
 
   useEffect(() => {
-    getToken();
+    fetch(`http://13.124.131.171:8080/prog-rangers/login/kakao?code=${KAKAO_CODE}`,{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded;",
+        },
+      })
+      .then(res =>{
+          return res.json();
+        })
+      .then(data => {
+        console.log(data);
+        localStorage.setItem('token', data.accessToken);
+        navigate("/");
+      })
+      .catch(error =>{
+        console.log('Error:', error);
+      })
+
   }, []);
 
   return(
