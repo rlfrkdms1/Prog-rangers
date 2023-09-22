1192b55c17ea4814335df181c089b889a51dcd48
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-22 14:18:22 +0900

    feat: import문 수정
    Related to: #126

--- backend/src/main/java/com/prograngers/backend/controller/CommentController.java
+++ backend/src/main/java/com/prograngers/backend/controller/CommentController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,9 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.net.URI;
-import java.net.URISyntaxException;
 
 @RestController
 @Slf4j
