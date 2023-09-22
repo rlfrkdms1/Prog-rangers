3b6bf1bd90a9bba06d09a21039ee783824cfd494
jidam <jidamine87593@gmail.com>
2023-09-22 22:27:16 +0900

    feat: 내 풀이 상세보기 dto 변경
    문제 관련 필드를 dto로 묶음
    
    Related to : #132

--- backend/src/main/java/com/prograngers/backend/dto/solution/response/MySolutionDetailResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/solution/response/MySolutionDetailResponse.java
@@ -2,29 +2,50 @@ package com.prograngers.backend.dto.solution.response;
 
 import com.prograngers.backend.entity.comment.Comment;
 import com.prograngers.backend.entity.problem.JudgeConstant;
-import com.prograngers.backend.entity.review.Review;
+import com.prograngers.backend.entity.problem.Problem;
 import com.prograngers.backend.entity.solution.Solution;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MySolutionDetailResponse {
-    private String problemTitle;
-    private JudgeConstant ojName;
-    private List<Object> tags;
 
+    // 문제
+    private MySolutionDetailProblem problem;
+    // 풀이
     private String description;
-
     private String[] code;
-
     private int likes;
     private int  scraps;
+    // 댓글
+    private List<SolutionDetailComment> comments;
 
-    private List<Comment> comments;
+    // 추천 풀이
+    private List<MySolutionRecommendedSolution> recommendedSolutionTitle;
 
-    private List<String> recommendedSolutionTitle;
-
+    // 사이드바 이 문제에 대한 풀이 3가지, 스크랩한 풀이 3가지
     private List<MySolutionDetailSolution> mySolutionList;
-
     private List<MySolutionDetailSolution> myScrapSolutionList;
-
+    public static MySolutionDetailResponse from(
+            MySolutionDetailProblem problem, Solution solution, List<SolutionDetailComment> comments, int likeCount, int scrapCount,
+            List<MySolutionDetailSolution> solutions, List<MySolutionDetailSolution> scraps){
+        return MySolutionDetailResponse.builder()
+                .problem(problem)
+                .description(solution.getDescription())
+                .code(solution.getCode().split("\n"))
+                .likes(likeCount)
+                .scraps(scrapCount)
+                .comments(comments)
+                .mySolutionList(solutions)
+                .myScrapSolutionList(scraps)
+                .build();
+    }
 }
