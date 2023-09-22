e005b00a770491dd1b67befe1bcb9ea6efd317f0
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-22 13:50:38 +0900

    feat: memberID로 모든 comment찾는 기능 추가
    Related to: #126

--- backend/src/main/java/com/prograngers/backend/repository/comment/CommentRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/comment/CommentRepository.java
@@ -2,6 +2,8 @@ package com.prograngers.backend.repository.comment;
 
 import com.prograngers.backend.entity.comment.Comment;
 import com.prograngers.backend.entity.solution.Solution;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,5 +14,8 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long>  {
 
     List<Comment> findAllBySolution(Solution solution);
+
+    @Query("select c from Comment c join fetch c.member join fetch c.solution where c.member.id = :memberId ")
+    Slice<Comment> findPageByMemberId(Pageable pageable, @Param("memberId") Long memberId);
 
 }
