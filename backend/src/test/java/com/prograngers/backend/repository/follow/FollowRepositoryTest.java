a28e43c6e46da67dc5c1e3bbe2a6ffba6d8ce186
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-24 02:11:56 +0900

    test: 팔로우 내역 찾기 테스트 추가
    Related to: #137

--- backend/src/test/java/com/prograngers/backend/repository/follow/FollowRepositoryTest.java
+++ backend/src/test/java/com/prograngers/backend/repository/follow/FollowRepositoryTest.java
@@ -1,20 +1,18 @@
 package com.prograngers.backend.repository.follow;
 
-import com.prograngers.backend.TestConfig;
 import com.prograngers.backend.entity.Follow;
 import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.repository.member.MemberRepository;
 import com.prograngers.backend.support.RepositoryTest;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+import static com.prograngers.backend.support.fixture.MemberFixture.길가은;
+import static com.prograngers.backend.support.fixture.MemberFixture.이수빈;
 import static com.prograngers.backend.support.fixture.MemberFixture.장지담;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,13 +33,13 @@ class FollowRepositoryTest {
         // given
         Member member1 = 저장(장지담.기본_정보_생성());
         Member member2 = 저장(장지담.기본_정보_생성());
-        Follow member1FollowMember2_1 = 저장(createFollow(member1, member2));
-        Follow member1FollowMember2_2 = 저장(createFollow(member1, member2));
-        Follow member1FollowMember2_3 = 저장(createFollow(member1, member2));
-        Follow member2FollowMember1_1 = 저장(createFollow(member2, member1));
-        Follow member2FollowMember1_2 = 저장(createFollow(member2, member1));
-        Follow member2FollowMember1_3 = 저장(createFollow(member2, member1));
-        Follow member2FollowMember1_4 = 저장(createFollow(member2, member1));
+        Follow member1FollowMember2_1 = 저장(팔로우_생성(member1, member2));
+        Follow member1FollowMember2_2 = 저장(팔로우_생성(member1, member2));
+        Follow member1FollowMember2_3 = 저장(팔로우_생성(member1, member2));
+        Follow member2FollowMember1_1 = 저장(팔로우_생성(member2, member1));
+        Follow member2FollowMember1_2 = 저장(팔로우_생성(member2, member1));
+        Follow member2FollowMember1_3 = 저장(팔로우_생성(member2, member1));
+        Follow member2FollowMember1_4 = 저장(팔로우_생성(member2, member1));
 
         // when
         Long follow = followRepository.getFollowCount(member1);
@@ -54,10 +52,28 @@ class FollowRepositoryTest {
         );
     }
 
-    private Follow createFollow(Member member1, Member member2) {
-        return Follow.builder().followerId(member1.getId()).followingId(member2.getId()).build();
+    @Test
+    @DisplayName("팔로우를 하면 팔로워와 팔로잉의 아이디로 팔로우 내역을 찾을 수 있다.")
+    void 팔로우_내역_찾기(){
+        Member member1 = 저장(길가은.기본_정보_생성());
+        Member member2 = 저장(장지담.기본_정보_생성());
+        Member member3 = 저장(이수빈.기본_정보_생성());
+
+        Follow follow1 = 저장(팔로우_생성(member1, member2));
+        Follow follow2 = 저장(팔로우_생성(member1, member3));
+
+        Follow followRecord1 = followRepository.findByFollowerIdAndFollowingId(member1.getId(), member2.getId()).get();
+        Follow followRecord2 = followRepository.findByFollowerIdAndFollowingId(member1.getId(), member3.getId()).get();
+
+        assertAll(
+                () -> assertThat(followRecord1).isEqualTo(follow1),
+                () -> assertThat(followRecord2).isEqualTo(follow2)
+        );
     }
 
+    private Follow 팔로우_생성(Member member1, Member member2) {
+        return Follow.builder().followerId(member1.getId()).followingId(member2.getId()).build();
+    }
 
     private Member 저장(Member member) {
         return memberRepository.save(member);
