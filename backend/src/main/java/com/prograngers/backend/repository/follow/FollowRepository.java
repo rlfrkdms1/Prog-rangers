5a2c3116b2c4892649c771d449a670e2f6d697da
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-24 00:37:12 +0900

    feat: followerId, followingId로 follow 찾는 기능 추가
    Related to: #137

--- backend/src/main/java/com/prograngers/backend/repository/follow/FollowRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/follow/FollowRepository.java
@@ -4,6 +4,11 @@ import com.prograngers.backend.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FollowRepository extends JpaRepository<Follow,Long>,  QueryDslFollowRepository{
+
+    Optional<Follow> findByFollowerIdAndFollowingId(Long followerId, Long followingId);
+
 }
