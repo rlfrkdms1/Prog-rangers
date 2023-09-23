442eb26e22da03cc721bac90fe88f6e85caa88a8
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-24 00:01:16 +0900

    test: status에 대한 테스트도 가능하도록 추가
    Related to: #126

--- backend/src/test/java/com/prograngers/backend/repository/comment/CommentRepositoryTest.java
+++ backend/src/test/java/com/prograngers/backend/repository/comment/CommentRepositoryTest.java
@@ -17,7 +17,9 @@ import org.springframework.data.domain.Slice;
 import java.time.LocalDateTime;
 
 import static com.prograngers.backend.entity.solution.LanguageConstant.JAVA;
+import static com.prograngers.backend.support.fixture.CommentFixture.삭제된_댓글;
 import static com.prograngers.backend.support.fixture.CommentFixture.생성된_댓글;
+import static com.prograngers.backend.support.fixture.CommentFixture.수정된_댓글;
 import static com.prograngers.backend.support.fixture.MemberFixture.길가은;
 import static com.prograngers.backend.support.fixture.MemberFixture.장지담;
 import static com.prograngers.backend.support.fixture.ProblemFixture.백준_문제;
@@ -38,14 +40,15 @@ class CommentRepositoryTest {
     private MemberRepository memberRepository;
 
     @Test
-    @DisplayName("주어진 회원이 작성한 댓글을 페이징해 조회할 수 있다.")
+    @DisplayName("주어진 회원이 삭제한 댓글 외 작성한 댓글을 페이징해 조회할 수 있다.")
     void 내_댓글_조회() {
         Member member1 = 저장(길가은.기본_정보_생성());
         Member member2 = 저장(장지담.기본_정보_생성());
         Problem problem = 저장(백준_문제.기본_정보_생성());
         Solution solution = 저장(공개_풀이.기본_정보_생성(problem, member1, LocalDateTime.now(), JAVA, 1));
-        Comment comment1 = 저장(생성된_댓글.기본_정보_생성(member1, solution, LocalDateTime.of(2023, 9, 10, 10, 10)));
+        Comment comment1 = 저장(수정된_댓글.기본_정보_생성(member1, solution, LocalDateTime.of(2023, 9, 10, 10, 10)));
         Comment comment3 = 저장(생성된_댓글.기본_정보_생성(member1, solution, LocalDateTime.of(2023, 9, 11, 10, 10)));
+        저장(삭제된_댓글.기본_정보_생성(member1, solution, LocalDateTime.of(2023, 9, 12, 10, 10)));
         저장(생성된_댓글.기본_정보_생성(member2, solution, LocalDateTime.of(2023, 9, 12, 10, 10)));
         저장(생성된_댓글.기본_정보_생성(member2, solution, LocalDateTime.of(2023, 9, 10, 15, 10)));
         Comment comment2 = 저장(생성된_댓글.기본_정보_생성(member1, solution, LocalDateTime.of(2023, 9, 10, 20, 10)));
@@ -56,8 +59,8 @@ class CommentRepositoryTest {
                 () -> assertThat(comments.getContent()).containsExactly(comment1, comment2, comment3),
                 () -> assertThat(comments.hasNext()).isFalse()
         );
-
     }
+
 
     private Member 저장(Member member) {
         return memberRepository.save(member);
