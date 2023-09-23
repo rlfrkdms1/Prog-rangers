7b8d01f567042db45540a7621b490f8d2301b357
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-23 23:54:52 +0900

    refactor: 시간 순으로 정렬되도록 수정
    Related to: #126

--- backend/src/main/java/com/prograngers/backend/repository/comment/CommentRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/comment/CommentRepository.java
@@ -15,7 +15,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long>  {
 
     List<Comment> findAllBySolution(Solution solution);
 
-    @Query("select c from Comment c join fetch c.member join fetch c.solution s join fetch s.problem join fetch s.member where c.member.id = :memberId and c.status != com.prograngers.backend.entity.comment.CommentStatusConstant.DELETED")
+    @Query("select c from Comment c join fetch c.member join fetch c.solution s join fetch s.problem join fetch s.member where c.member.id = :memberId and c.status != com.prograngers.backend.entity.comment.CommentStatusConstant.DELETED order by c.createdAt")
     Slice<Comment>findMyPageByMemberId(Pageable pageable, @Param("memberId")Long memberId);
 
 }
