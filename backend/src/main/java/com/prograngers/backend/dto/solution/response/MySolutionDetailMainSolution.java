aa5cdbeed275daf8bf7a7888f1a51b760e52995b
jidam <jidamine87593@gmail.com>
2023-09-23 19:55:29 +0900

    feat: 내 풀이 상세보기 메인 풀이 dto 추가
    Related to : #132

---
+++ backend/src/main/java/com/prograngers/backend/dto/solution/response/MySolutionDetailMainSolution.java
@@ -0,0 +1,24 @@
+package com.prograngers.backend.dto.solution.response;
+
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public class MySolutionDetailMainSolution {
+    private List<Object> tags;
+    private String description;
+    private String[] code;
+    private int likes;
+    private int  scraps;
+
+    public static MySolutionDetailMainSolution from(List<Object> tags, String description, String[] code, int likes, int scraps){
+        return MySolutionDetailMainSolution.builder()
+                .tags(tags)
+                .description(description)
+                .code(code)
+                .likes(likes)
+                .scraps(scraps)
+                .build();
+    }
+}
