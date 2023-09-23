9d56f7d537ba175babb7d6eb48050a5561ef62e0
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-24 03:21:47 +0900

    test: 팔로우 기능 테스트
    Related to: #137

--- backend/src/test/java/com/prograngers/backend/service/FollowServiceTest.java
+++ backend/src/test/java/com/prograngers/backend/service/FollowServiceTest.java
@@ -1,5 +1,6 @@
 package com.prograngers.backend.service;
 
+import com.prograngers.backend.entity.Follow;
 import com.prograngers.backend.exception.badrequest.AlreadyFollowException;
 import com.prograngers.backend.repository.follow.FollowRepository;
 import com.prograngers.backend.repository.member.MemberRepository;
@@ -27,7 +28,7 @@ class FollowServiceTest {
 
     @Test
     @DisplayName("이미 팔로우 내역이 있는 회원을 팔로우 했을 때 예외가 터진다.")
-    void 중복_팔로우(){
+    void 중복_팔로우() {
         Long followerId = 1L;
         Long followingId = 2L;
 
@@ -41,6 +42,27 @@ class FollowServiceTest {
                 () -> verify(memberRepository).existsById(followingId),
                 () -> verify(followRepository).existsByFollowerIdAndFollowingId(followerId, followingId)
         );
+    }
 
+    @Test
+    @DisplayName("팔로우와 팔로워가 주어지면 팔로우를 할 수 있다.")
+    void 팔로우_하기(){
+        Long followerId = 1L;
+        Long followingId = 2L;
+        Follow follow = Follow.builder().followerId(followerId).followingId(followingId).build();
+
+        given(memberRepository.existsById(followerId)).willReturn(true);
+        given(memberRepository.existsById(followingId)).willReturn(true);
+        given(followRepository.existsByFollowerIdAndFollowingId(followerId, followingId)).willReturn(false);
+        given(followRepository.save(follow)).willReturn(follow);
+
+        followService.follow(followerId, followingId);
+
+        assertAll(
+                () -> verify(memberRepository).existsById(followerId),
+                () -> verify(memberRepository).existsById(followingId),
+                () -> verify(followRepository).existsByFollowerIdAndFollowingId(followerId, followingId),
+                () -> verify(followRepository).save(follow)
+        );
     }
 }
