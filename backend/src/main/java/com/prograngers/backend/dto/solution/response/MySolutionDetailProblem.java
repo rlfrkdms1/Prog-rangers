438c786b4d32c24b5ef68795d3e54164973b5792
jidam <jidamine87593@gmail.com>
2023-09-23 20:47:27 +0900

    feat: MySolutionDetailProblem 수정
    1. from 추가
    2. tags를 solution dto로 이동
    
    Related to : #132

--- backend/src/main/java/com/prograngers/backend/dto/solution/response/MySolutionDetailProblem.java
+++ backend/src/main/java/com/prograngers/backend/dto/solution/response/MySolutionDetailProblem.java
@@ -1,21 +1,16 @@
 package com.prograngers.backend.dto.solution.response;
 
 import com.prograngers.backend.entity.problem.JudgeConstant;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.util.List;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class MySolutionDetailProblem {
     private String problemTitle;
     private JudgeConstant ojName;
-    private List<Object> tags;
+    public static MySolutionDetailProblem from(String problemTitle, JudgeConstant ojName){
+        return MySolutionDetailProblem.builder()
+                .problemTitle(problemTitle)
+                .ojName(ojName)
+                .build();
+    }
 }
