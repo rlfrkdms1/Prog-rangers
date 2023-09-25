05a339cb969627d3c0178f5fc3547e5046da64ec
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-25 23:40:56 +0900

    fix: 회원 가입 안되는 오류 해결
    Related to: #137

--- backend/src/main/java/com/prograngers/backend/dto/request/auth/SignUpRequest.java
+++ backend/src/main/java/com/prograngers/backend/dto/request/auth/SignUpRequest.java
@@ -4,11 +4,14 @@ import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.entity.member.MemberType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SignUpRequest {
 
 
