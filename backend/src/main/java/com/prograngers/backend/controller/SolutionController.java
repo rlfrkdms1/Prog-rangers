fb9c0c17503f57e83210013a657f5d9fe3162c37
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-29 01:37:53 +0900

    feat: 내 풀이 목록 조회기능 추가
    Related to: #142

--- backend/src/main/java/com/prograngers/backend/controller/SolutionController.java
+++ backend/src/main/java/com/prograngers/backend/controller/SolutionController.java
@@ -3,9 +3,13 @@ package com.prograngers.backend.controller;
 import com.prograngers.backend.controller.auth.LoggedInMember;
 import com.prograngers.backend.controller.auth.Login;
 import com.prograngers.backend.dto.solution.reqeust.ScarpSolutionPostRequest;
+import com.prograngers.backend.dto.solution.response.ShowMySolutionListResponse;
 import com.prograngers.backend.dto.solution.response.SolutionDetailResponse;
 import com.prograngers.backend.dto.solution.reqeust.SolutionPatchRequest;
 import com.prograngers.backend.dto.solution.reqeust.SolutionPostRequest;
+import com.prograngers.backend.entity.solution.AlgorithmConstant;
+import com.prograngers.backend.entity.solution.DataStructureConstant;
+import com.prograngers.backend.entity.solution.LanguageConstant;
 import com.prograngers.backend.service.SolutionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,17 +26,18 @@ import java.net.URI;
 public class SolutionController {
 
     private final SolutionService solutionService;
+    private static final String DEFAULT_PAGE = "1";
 
     @Login
     @PostMapping("/solutions")
-    public ResponseEntity<Void> write(@LoggedInMember Long memberId, @RequestBody @Valid SolutionPostRequest solutionPostRequest){
+    public ResponseEntity<Void> write(@LoggedInMember Long memberId, @RequestBody @Valid SolutionPostRequest solutionPostRequest) {
         Long saveId = solutionService.save(solutionPostRequest, memberId);
         return ResponseEntity.created(URI.create("/api/v1/solutions" + saveId)).build();
     }
 
     @Login
     @PostMapping("/solutions/{scrapId}")
-    public ResponseEntity<Void> scrap(@LoggedInMember Long memberId, @PathVariable Long scrapId, @RequestBody @Valid  ScarpSolutionPostRequest request) {
+    public ResponseEntity<Void> scrap(@LoggedInMember Long memberId, @PathVariable Long scrapId, @RequestBody @Valid ScarpSolutionPostRequest request) {
         Long saveId = solutionService.saveScrap(scrapId, request, memberId);
         return ResponseEntity.created(URI.create("/api/v1/solutions" + saveId)).build();
     }
@@ -40,14 +45,14 @@ public class SolutionController {
     @Login
     @PatchMapping("/solutions/{solutionId}")
     public ResponseEntity<Void> update(@PathVariable Long solutionId, @LoggedInMember Long memberId,
-                                    @RequestBody @Valid SolutionPatchRequest solutionPatchRequest){
+                                       @RequestBody @Valid SolutionPatchRequest solutionPatchRequest) {
         solutionService.update(solutionId, solutionPatchRequest, memberId);
         return ResponseEntity.noContent().build();
     }
 
     @Login
     @DeleteMapping("/solutions/{solutionId}")
-    public ResponseEntity<Void> delete(@PathVariable Long solutionId, @LoggedInMember Long memberId){
+    public ResponseEntity<Void> delete(@PathVariable Long solutionId, @LoggedInMember Long memberId) {
         solutionService.delete(solutionId, memberId);
         return ResponseEntity.noContent().build();
 
@@ -57,6 +62,18 @@ public class SolutionController {
     public ResponseEntity<?> showDetail(@PathVariable Long solutionId, @LoggedInMember(required = false) Long memberId) {
         SolutionDetailResponse solutionDetailResponse = solutionService.getSolutionDetail(solutionId, memberId);
         return ResponseEntity.ok().body(solutionDetailResponse);
+    }
+
+    @Login
+    @GetMapping("/mypage/solutions")
+    public ShowMySolutionListResponse showMyList(@RequestParam(required = false) String keyword,
+                                                 @RequestParam(required = false) LanguageConstant language,
+                                                 @RequestParam(required = false) AlgorithmConstant algorithm,
+                                                 @RequestParam(required = false) DataStructureConstant dataStructure,
+                                                 @RequestParam(required = false) Integer level,
+                                                 @RequestParam(defaultValue = DEFAULT_PAGE) int page,
+                                                 @LoggedInMember Long memberId) {
+        return solutionService.getMyList(keyword, language, algorithm, dataStructure, level, page, memberId);
     }
 
 }
