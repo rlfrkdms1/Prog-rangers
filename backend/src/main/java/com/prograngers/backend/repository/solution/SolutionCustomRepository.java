a31b9fff20b5ff75c7f1cfe47bdb46872dbab0c7
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-29 01:35:54 +0900

    feat: 내 풀이 목록 조회 기능 추가
    Related to: #142

--- backend/src/main/java/com/prograngers/backend/repository/solution/SolutionCustomRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/solution/SolutionCustomRepository.java
@@ -5,6 +5,7 @@ import com.prograngers.backend.entity.solution.AlgorithmConstant;
 import com.prograngers.backend.entity.solution.DataStructureConstant;
 import com.prograngers.backend.entity.solution.LanguageConstant;
 import com.prograngers.backend.entity.sortconstant.SortConstant;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
@@ -16,4 +17,6 @@ public interface QueryDslSolutionRepository {
             Pageable pageable, Long problemId, LanguageConstant language, AlgorithmConstant algorithm,
             DataStructureConstant dataStructure, SortConstant sortBy);
     List<Solution> findProfileSolutions(Long memberId,Long page);
+
+    Page<Solution> getMyList(Pageable pageable, String keyword, LanguageConstant language, AlgorithmConstant algorithm, DataStructureConstant dataStructure, Integer level, Long memberId);
 }
