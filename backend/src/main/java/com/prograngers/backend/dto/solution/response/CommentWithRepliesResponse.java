4eca7d3d3dac003f86888d7f1ee230de03031d45
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-09-28 18:32:30 +0900

    test: 오탈자 수정
    related to : #132

--- backend/src/main/java/com/prograngers/backend/dto/solution/response/CommentWithRepliesResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/solution/response/CommentWithRepliesResponse.java
@@ -2,7 +2,8 @@ package com.prograngers.backend.dto.solution.response;
 
 
 import com.prograngers.backend.entity.comment.Comment;
-import com.prograngers.backend.entity.comment.CommentStatusConStant;
+
+import com.prograngers.backend.entity.comment.CommentStatusConstant;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +21,7 @@ public class CommentWithRepliesResponse {
     Long id;
     String nickname;
     String content;
-    CommentStatusConStant status;
+    CommentStatusConstant status;
     List<CommentWithRepliesResponse> replies;
 
     boolean mine;
