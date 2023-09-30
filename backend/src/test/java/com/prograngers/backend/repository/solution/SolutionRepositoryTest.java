abb9c412eb13fa30c77541474c3588b7d8cd7669
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-09-30 15:47:59 +0900

    test: 메서드 명 변경에 따른 수정
    related to: #150

--- backend/src/test/java/com/prograngers/backend/repository/solution/SolutionRepositoryTest.java
+++ backend/src/test/java/com/prograngers/backend/repository/solution/SolutionRepositoryTest.java
@@ -396,7 +396,7 @@ class SolutionRepositoryTest {
 
         저장(좋아요_생성(member1,solution4));
 
-        List<Solution> result = solutionRepository.findTop6SolutionOfProblemOrderByLikesDesc(problem,3);
+        List<Solution> result = solutionRepository.findTopLimitsSolutionOfProblemOrderByLikesDesc(problem,3);
 
         assertThat(result).containsExactly(solution1,solution2,solution3);
     }
