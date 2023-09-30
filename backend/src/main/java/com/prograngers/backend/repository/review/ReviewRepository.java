c96bc270b65f078dfc2fd9c05e0b35af01a03832
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-09-30 12:58:47 +0900

    feat: 메서드 추가
    related to: #150

--- backend/src/main/java/com/prograngers/backend/repository/review/ReviewRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/review/ReviewRepository.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, QueryDslReviewRepository{
     List<Review> findAllBySolution(Solution solution);
+    List<Review> findAllBySolutionOOrderByCodeLineNumberAsc(Solution solution);
     @Query("select distinct function('date_format', r.createdAt, '%d') from Review r where r.member.id = :memberId and function('date_format', r.createdAt, '%m') = :month")
     List<Integer> findAllByMonth(@Param("memberId") Long memberId, @Param("month") int month);
     List<Review> findAllByCodeLineNumberOrderByCreatedAtAsc(Integer codeLineNumber);
