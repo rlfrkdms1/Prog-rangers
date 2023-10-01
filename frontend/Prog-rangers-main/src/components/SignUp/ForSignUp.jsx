634c336790259c4b8ef1dd3a0b0a927668ea054a
g2Min <jimin9320@gmail.com>
2023-10-01 12:34:56 +0900

    feat: 회원가입페이지 이동 함수 추가

--- frontend/Prog-rangers-main/src/components/SignUp/ForSignUp.jsx
+++ frontend/Prog-rangers-main/src/components/SignUp/ForSignUp.jsx
@@ -14,7 +14,9 @@ const buttonPage = css`
 
 export const ForSignUp = () => {
   const navigate = useNavigate();
-  const handleSignUp = navigate(`/signup`);
+  const handleSignUp = () => {
+    window.location.href = `http://localhost:3000/signup`;
+  }
 
   return(
     <div css={css`
@@ -26,7 +28,7 @@ export const ForSignUp = () => {
       font-size: 12px;
     `}>
       <div css={css`color: #545454;`}>프로그래인저 계정에 아직 가입하지 않으셨나요?</div>
-      <div css={css`${buttonPage}`} onClick={handleSignUp}>
+      <div css={css`${buttonPage} &:hover{ cursor: pointer; }`} onClick={handleSignUp}>
         <div css={css`margin-left: 50px; font-weight: 700; color: #3486A0; width: 55px;`}>회원가입</div>
         <img css={css`margin-left: 10px;`} src={Arrow} alt="next_page"/>
       </div>
