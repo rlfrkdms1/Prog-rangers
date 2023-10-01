6fcf3c6911a693a745ff9c7d6155f558cf63c579
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-01 16:48:12 +0900

    refactor: 띄어쓰기 수정
    related to: #150

--- backend/src/test/java/com/prograngers/backend/support/fixture/ProblemFixture.java
+++ backend/src/test/java/com/prograngers/backend/support/fixture/ProblemFixture.java
@@ -13,7 +13,7 @@ import static com.prograngers.backend.entity.problem.JudgeConstant.프로그래�
 @AllArgsConstructor
 public enum ProblemFixture {
 
-    백준_문제("백준문제", "https://www.acmicpc.net/problem/2557", 백준),
+    백준_문제("백준 문제", "https://www.acmicpc.net/problem/2557", 백준),
     프로그래머스_문제("프로그래머스 문제", "https://school.programmers.co.kr/learn/courses/30/lessons/164673", 프로그래머스);
 
     private final String title;
