c0f842891be1c453afd249e500319a40cd8be481
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-01 23:20:46 +0900

    feat: 알림 찾아오는 기능 추가
    Related to: #158

--- backend/src/main/java/com/prograngers/backend/repository/notification/NotificationRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/notification/NotificationRepository.java
@@ -3,6 +3,8 @@ package com.prograngers.backend.repository.notification;
 import com.prograngers.backend.entity.Notification;
 import com.prograngers.backend.entity.member.Member;
 import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,5 +13,8 @@ import java.util.List;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     List<Notification> findTop9ByReceiverOrderByCreatedAtDesc(Member receiver);
+
+    @Query("select n from Notification n where n.receiver.id = :memberId order by n.createdAt desc")
+    Slice<Notification> findPageByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 
 }
