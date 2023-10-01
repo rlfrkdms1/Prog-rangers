7b31ab75bc7fdd97c1de2ff0b6b7906c10d293aa
g2Min <jimin9320@gmail.com>
2023-10-02 00:27:35 +0900

    feat: 태그에 삭제기능 추가

---
+++ frontend/Prog-rangers-main/src/components/WriteBoard/TagAction.jsx
@@ -0,0 +1,25 @@
+import { tags } from "../Question/tagsform";
+import { css } from "@emotion/react";
+import Delete from '../../assets/icons/solution-tag-delete.svg';
+import { useState } from "react";
+
+export const TagAction = ({data}) => {
+  const [ isDeleted, setIsDeleted ] = useState(false);
+  const deleteHandler = () => {
+    setIsDeleted(true);
+  }
+
+  return(
+    <>
+      {isDeleted == false ? 
+        <div css={css`${tags}`}>
+          {data}
+          <img onClick={deleteHandler} css={css`margin-left: 15px; &:hover{ cursor: pointer; }`} src={Delete} />
+        </div>
+        :
+        ""
+      }
+
+    </>
+  );
+};
