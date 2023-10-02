385691bef80293745172fd1d7776b1d83ab33d3a
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-02 21:12:19 +0900

    feat: 정적 팩터리 메서드 추가
    related to: #163

--- backend/src/main/java/com/prograngers/backend/dto/follow/response/ShowFollowListResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/follow/response/ShowFollowListResponse.java
@@ -1,10 +1,13 @@
 package com.prograngers.backend.dto.follow.response;
 
+import com.prograngers.backend.entity.Follow;
+import com.prograngers.backend.entity.member.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
 @Getter
@@ -12,5 +15,19 @@ import java.util.List;
 public class ShowFollowListResponse {
         private List<MemberResponse> followings;
         private List<MemberResponse> followers;
-        private List<MemberResponse> recommendedFollwers;
+        private List<MemberResponse> recommendedFollowers;
+
+    public static ShowFollowListResponse of(List<Member> followingList, List<Member> followerList, List<Member> recommendedFollows) {
+        return ShowFollowListResponse.builder()
+                .followings(followingList.stream()
+                        .map(MemberResponse::from)
+                        .collect(Collectors.toList()))
+                .followers(followerList.stream()
+                        .map(MemberResponse::from)
+                        .collect(Collectors.toList()))
+                .recommendedFollowers(recommendedFollows.stream()
+                        .map(MemberResponse::from)
+                        .collect(Collectors.toList()))
+                .build();
+    }
 }
