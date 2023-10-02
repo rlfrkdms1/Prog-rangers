e3dfe999add4485d7076dd68ac9f250d20dcbeeb
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-02 15:49:43 +0900

    feat: 주어진 회원이 좋아요를 누른 풀이를 최신순으로 페이징하는 기능
    Related to: #160

--- backend/src/main/java/com/prograngers/backend/repository/solution/SolutionRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/solution/SolutionRepository.java
@@ -3,6 +3,8 @@ package com.prograngers.backend.repository.solution;
 import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.entity.problem.Problem;
 import com.prograngers.backend.entity.solution.Solution;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -25,4 +27,7 @@ public interface SolutionRepository extends JpaRepository<Solution, Long>, Solut
 
     @Query("select distinct function('date_format', s.createdAt, '%d') from Solution s where s.member.id = :memberId and function('date_format', s.createdAt, '%m') = :month")
     List<Integer> findAllByMonth(@Param("memberId") Long memberId, @Param("month") int month);
+
+    @Query("select s from Solution s join Likes l on l.solution.id = s.id where l.member.id = :memberId order by l.createdAt desc")
+    Slice<Solution> findMyLikesPage(@Param("memberId") Long memberId, Pageable pageable);
 }
