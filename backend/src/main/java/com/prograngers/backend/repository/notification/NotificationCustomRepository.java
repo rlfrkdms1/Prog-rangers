13bb8799fa7604deaa5d5e268b67f1dd730ac258
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-02 11:51:29 +0900

    feat: limit로 알림 조회하는 기능 추가
    Related to:

---
+++ backend/src/main/java/com/prograngers/backend/repository/notification/NotificationCustomRepository.java
@@ -0,0 +1,11 @@
+package com.prograngers.backend.repository.notification;
+
+import com.prograngers.backend.entity.Notification;
+
+import java.util.List;
+
+public interface NotificationCustomRepository {
+
+    List<Notification> findByMemberIdAndLimit(Long MemberId, int limit);
+
+}
