b917ac2253c1d461b725ff8cc5ef4ea639df3b55
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-02 20:52:17 +0900

    feat: 쿼리 추가
    related to: #163

---
+++ backend/src/main/java/com/prograngers/backend/repository/member/MemberCustomRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.prograngers.backend.repository.member;
+
+import com.prograngers.backend.entity.member.Member;
+import com.prograngers.backend.entity.problem.Problem;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.prograngers.backend.entity.QFollow.*;
+import static com.prograngers.backend.entity.member.QMember.*;
+import static com.prograngers.backend.entity.solution.QSolution.*;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberCustomRepositoryImpl implements MemberCustomRepository{
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public List<Member> getLimitRecommendedMembers(Problem problem,Long limit){
+        return jpaQueryFactory.select(member)
+                .from(member)
+                .join(solution)
+                .join(follow)
+                .on(follow.followingId.eq(member.id))
+                .where(solution.problem.eq(problem))
+                .groupBy(member)
+                .orderBy(follow.count().desc())
+                .limit(limit)
+                .fetch();
+    }
+
+}
--- backend/src/main/java/com/prograngers/backend/repository/member/MemberRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/member/MemberRepository.java
@@ -1,16 +1,20 @@
 package com.prograngers.backend.repository.member;
 
 import com.prograngers.backend.entity.member.Member;
+import com.prograngers.backend.entity.problem.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
     Optional<Member> findByEmail(String email);
 
     Optional<Member> findByNickname(String nickname);
 
     Optional<Member> findBySocialId(Long socialId);
+
 }
