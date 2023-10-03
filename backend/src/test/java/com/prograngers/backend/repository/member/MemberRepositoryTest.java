2275cd002d144a26133bd7fc834ce0967abd9be1
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-03 17:24:38 +0900

    test: 문제가 없을 경우에 대해 테스트 추가
    related to: #163

--- backend/src/test/java/com/prograngers/backend/repository/member/MemberRepositoryTest.java
+++ backend/src/test/java/com/prograngers/backend/repository/member/MemberRepositoryTest.java
@@ -2,16 +2,13 @@ package com.prograngers.backend.repository.member;
 
 
 import com.prograngers.backend.entity.Follow;
-import com.prograngers.backend.entity.Likes;
 import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.entity.problem.Problem;
 import com.prograngers.backend.entity.solution.Solution;
 import com.prograngers.backend.repository.follow.FollowRepository;
-import com.prograngers.backend.repository.likes.LikesRepository;
 import com.prograngers.backend.repository.problem.ProblemRepository;
 import com.prograngers.backend.repository.solution.SolutionRepository;
 import com.prograngers.backend.support.RepositoryTest;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +17,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.prograngers.backend.entity.solution.LanguageConstant.JAVA;
-import static com.prograngers.backend.support.fixture.MemberFixture.길가은;
 import static com.prograngers.backend.support.fixture.MemberFixture.장지담;
 import static com.prograngers.backend.support.fixture.ProblemFixture.백준_문제;
 import static com.prograngers.backend.support.fixture.SolutionFixture.공개_풀이;
@@ -85,6 +81,62 @@ class MemberRepositoryTest {
 
         //then
         assertThat(result).containsExactly(member4,member3,member5);
+    }
+
+    @Test
+    @DisplayName("문제가 주어지지 않았을 때(회원이 푼 문제가 없을 때) 회원들을 팔로우가 많은 순으로 정렬해서 가져올 수 있다. 이 때 내가 팔로우하지 않은 회원들을 가져온다")
+    void getLimitRecommendedMembersTestWhenNoProblem(){
+
+        //given
+        Member member1 = 저장(장지담.기본_정보_생성());
+        Member member2 = 저장(장지담.기본_정보_생성());
+        Member member3 = 저장(장지담.기본_정보_생성());
+        Member member4 = 저장(장지담.기본_정보_생성());
+        Member member5 = 저장(장지담.기본_정보_생성());
+        Member member6 = 저장(장지담.기본_정보_생성());
+
+        /**
+         *  member1: 나
+         *  member2: 내가 이미 팔로우함(결과에 포함 X)
+         *  member3(1명), member4(2명): 내가 팔로우하지 않음, 다른 팔로워는 있음
+         *  member5 : 팔로워가 없음(결과의 마지막에 포함되어야함)
+         *  member6: 팔로워가 제일 많음
+         */
+        저장(팔로우_생성(member1.getId(),member2.getId()));
+        저장(팔로우_생성(member1.getId(),member3.getId()));
+        저장(팔로우_생성(member2.getId(),member1.getId()));
+
+        저장(팔로우_생성(member4.getId(),member2.getId()));
+        저장(팔로우_생성(member4.getId(),member3.getId()));
+
+        저장(팔로우_생성(member3.getId(),member5.getId()));
+
+        저장(팔로우_생성(member6.getId(),member2.getId()));
+        저장(팔로우_생성(member6.getId(),member3.getId()));
+        저장(팔로우_생성(member6.getId(),member4.getId()));
+        저장(팔로우_생성(member6.getId(),member5.getId()));
+
+        Problem problem1 = 저장(백준_문제.기본_정보_생성());
+        Problem problem2 = 저장(백준_문제.기본_정보_생성());
+
+        저장(공개_풀이.기본_정보_생성(problem1, member2, LocalDateTime.now(), JAVA, 3));
+        저장(공개_풀이.기본_정보_생성(problem1, member3, LocalDateTime.now(), JAVA, 3));
+        저장(공개_풀이.기본_정보_생성(problem1, member4, LocalDateTime.now(), JAVA, 3));
+        저장(공개_풀이.기본_정보_생성(problem1, member5, LocalDateTime.now(), JAVA, 3));
+        저장(공개_풀이.기본_정보_생성(problem2, member6, LocalDateTime.now(), JAVA, 3));
+
+
+
+
+        //when
+        List<Member> result = memberRepository.getLimitRecommendedMembers(null, 4L, member1.getId());
+
+        for (Member m : result){
+            System.out.println(m.getIntroduction());
+        }
+
+        //then
+        assertThat(result).containsExactly(member6,member4,member3,member5);
     }
 
     @Test
