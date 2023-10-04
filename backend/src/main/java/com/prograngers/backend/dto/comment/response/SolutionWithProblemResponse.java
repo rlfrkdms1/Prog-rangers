34091fcfa2dcc0a8917a43e5c990e3d67cf8cd64
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-05 00:01:47 +0900

    refactor: null 값 처리
    Related to: #160

--- backend/src/main/java/com/prograngers/backend/dto/comment/response/SolutionWithProblemResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/comment/response/SolutionWithProblemResponse.java
@@ -1,15 +1,18 @@
 package com.prograngers.backend.dto.comment.response;
 
 import com.prograngers.backend.entity.member.Member;
+import com.prograngers.backend.entity.problem.JudgeConstant;
 import com.prograngers.backend.entity.problem.Problem;
 import com.prograngers.backend.entity.solution.Solution;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SolutionWithProblemResponse {
 
@@ -18,14 +21,31 @@ public class SolutionWithProblemResponse {
     private String algorithm;
     private String dataStructure;
     private String problemTitle;
-    private String ojName;
+    private JudgeConstant ojName;
     private String authorImageUrl;
     private String authorNickname;
 
     public static SolutionWithProblemResponse from(Solution solution) {
         Problem problem = solution.getProblem();
         Member author = solution.getMember();
-        return new SolutionWithProblemResponse(solution.getId(), solution.getTitle(), solution.getAlgorithm().getStringValue(), solution.getDataStructure().getStringValue(),
-                problem.getTitle(), problem.getOjName().name(), author.getPhoto(), author.getNickname());
+        SolutionWithProblemResponse response = SolutionWithProblemResponse.builder()
+                .solutionId(solution.getId())
+                .solutionTitle(solution.getTitle())
+                .problemTitle(problem.getTitle())
+                .ojName(problem.getOjName())
+                .authorImageUrl(author.getPhoto())
+                .authorNickname(author.getNickname())
+                .build();
+        if(solution.getAlgorithm() != null) response.setAlgorithm(solution.getAlgorithm().getView());
+        if(solution.getDataStructure() != null) response.setDataStructure(solution.getDataStructure().getView());
+        return response;
+    }
+
+    private void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    private void setDataStructure(String dataStructure) {
+        this.dataStructure = dataStructure;
     }
 }
