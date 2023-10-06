a0593acee457ea4fb3c1bb4737b919921ecf50b0
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-06 22:50:11 +0900

    feat: 뱃지 상수에 필드값 추가
    related to: #171

--- backend/src/main/java/com/prograngers/backend/entity/badge/BadgeConstant.java
+++ backend/src/main/java/com/prograngers/backend/entity/badge/BadgeConstant.java
@@ -1,9 +1,15 @@
 package com.prograngers.backend.entity.badge;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum BadgeConstant {
-    새싹,
-    잎과줄기,
-    꽃봉오리,
-    꽃,
-    화단;
+    새싹(1),
+    잎과줄기(10),
+    꽃봉오리(50),
+    꽃(100),
+    화단(150);
+
+    private int count;
+
 }
