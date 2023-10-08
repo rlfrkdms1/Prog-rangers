65be583092a7d9530c94fdcc551304a76bdbc044
g2Min <jimin9320@gmail.com>
2023-10-09 03:51:19 +0900

    feat: 작성페이지 UI및 레이아웃 구현

--- frontend/Prog-rangers-main/src/pages/BoardPage/EditSolution.jsx
+++ frontend/Prog-rangers-main/src/pages/BoardPage/EditSolution.jsx
@@ -3,6 +3,7 @@ import { SideBar } from "../../components/SideBar/SideBar";
 import { AddMySolution } from "../../components/WriteBoard";
 import { ButtonDiv, SubmitButton } from "./buttonDiv";
 import { Provider, atom, useAtom } from 'jotai';
+import { useState, useEffect } from 'react';
 
 export const targetScope = Symbol();
 export const targetAtom = atom("");
@@ -15,6 +16,19 @@ export const nameAtom = atom("");
 //mypage 정보를 미리 가져와서 추가수정기능
 
 export const EditSolution = () => {
+  const currentURL = window.location.href.split('/');
+  const indexOfSolution = currentURL.indexOf("solutions");
+  const [ id, setId ] = useState(0);
+
+  //주소에서 solution뒤에 id값 나오면 그 id값이 problem 번호가 됨
+  useEffect(() => {
+    if(indexOfSolution !== -1 && indexOfSolution < currentURL.length - 1){
+      setId(currentURL[indexOfSolution + 1]);
+      console.log(id);
+    }else{
+      alert("해당되는 문제번호가 없습니다.");
+    }
+  }, []);
 
   return(
   <div 
