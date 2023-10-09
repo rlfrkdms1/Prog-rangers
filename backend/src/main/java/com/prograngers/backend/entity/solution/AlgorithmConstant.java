53001deef3299b2d218105f23c82e04c9035dc45
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-09 16:58:09 +0900

    feat: 알고리즘 종류 추가
    related to: #178

--- backend/src/main/java/com/prograngers/backend/entity/solution/AlgorithmConstant.java
+++ backend/src/main/java/com/prograngers/backend/entity/solution/AlgorithmConstant.java
@@ -18,6 +18,17 @@ public enum AlgorithmConstant implements HashTag {
     BINARY_SEARCH("이진 탐색"),
     BFS("BFS"),
     DFS("DFS"),
+    MATH("수학"),
+    DYNAMIC_PROGRAMMING("동적 프로그래밍"),
+    GREEDY("그리디 알고리즘"),
+    BRUTE_FORCE("브루트포스 알고리즘"),
+    PREFIX_SUM("누적 합"),
+    COMBINATORICS("조합론"),
+    DIVIDE_AND_CONQUER("분할 정복"),
+    TWO_POINTER("투 포인터"),
+    RECURSION("재귀"),
+    SLIDING_WINDOW("슬라이딩 윈도우"),
+    BELLMAN_FORD("벨만-포드"),
     DIJKSTRA("다익스트라");
 
     private final String view;
