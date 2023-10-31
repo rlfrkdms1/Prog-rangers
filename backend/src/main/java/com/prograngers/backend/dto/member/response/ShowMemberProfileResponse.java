e700b81429fc51a906bfcbc2f99c85ca0043fb5c
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-10-31 17:39:36 +0900

    refactor: 불필요한 주석 삭제
    related to: #181

--- backend/src/main/java/com/prograngers/backend/dto/member/response/ShowMemberProfileResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/member/response/ShowMemberProfileResponse.java
@@ -13,35 +13,22 @@ import java.util.List;
 @Builder
 public class ShowMemberProfileResponse {
     private static int SIZE_PER_SCROLL = 3;
-    /**
-     * 프로필 사진, 닉네임, 자기소개, 팔로우, 팔로잉, 깃허브
-     */
     private String photo;
     private String nickname;
     private String introduction;
     private Long follow;
     private Long following;
     private String github;
-    /**
-     * 뱃지
-     */
     private List<BadgeConstant> badge;
-    /**
-     *  문제제목, 풀이알고리즘, 풀이 자료구조, 저지명
-     *  풀이설명, 소스코드
-     */
-    List<SolutionWithProblemResponse> list;
-    /**
-     *  무한스크롤 위한 값들
-     */
-    // 다음에 pathvariable을 뭘로 요청해야 하는지 알려주기 위한 값, 커서가 -1L이면 마지막
+    private List<SolutionWithProblemResponse> list;
     private Long cursor;
 
-    public static ShowMemberProfileResponse from(Member member, List<Badge> badges, List<Solution> solutions, Long follow, Long following) {
-
-        // 무한스크롤 isLast, 커서
+    public static ShowMemberProfileResponse from(Member member, List<Badge> badges, List<Solution> solutions,
+                                                 Long follow, Long following) {
         Long cursor = -1L;
-        if (!isLastScroll(solutions)) cursor = getCursor(solutions);
+        if (!isLastScroll(solutions)) {
+            cursor = getCursor(solutions);
+        }
 
         return ShowMemberProfileResponse.builder()
                 .photo(member.getPhoto())
@@ -70,8 +57,8 @@ public class ShowMemberProfileResponse {
 
     private static Long getCursor(List<Solution> solutions) {
         Long cursor;
-        cursor = solutions.get(SIZE_PER_SCROLL-1).getId();
-        solutions.remove(SIZE_PER_SCROLL-1);
+        cursor = solutions.get(SIZE_PER_SCROLL - 1).getId();
+        solutions.remove(SIZE_PER_SCROLL - 1);
         return cursor;
     }
 }
