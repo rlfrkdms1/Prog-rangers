9b40683f695eb71045426821d2a23dc8d243c3dc
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-04 20:36:21 +0900

    feat: 리포지토리 existsById 추가
    풀이가 존재하는지 확인하기 위한 메서드 추가
    
    related to: #186

--- backend/src/main/java/com/prograngers/backend/repository/review/ReviewRepository.java
+++ backend/src/main/java/com/prograngers/backend/repository/review/ReviewRepository.java
@@ -10,10 +10,15 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface ReviewRepository extends JpaRepository<Review, Long>, QueryDslReviewRepository{
+public interface ReviewRepository extends JpaRepository<Review, Long>, QueryDslReviewRepository {
     List<Review> findAllBySolution(Solution solution);
+
     List<Review> findAllBySolutionOrderByCodeLineNumberAsc(Solution solution);
+
     @Query("select distinct function('date_format', r.createdAt, '%d') from Review r where r.member.id = :memberId and function('date_format', r.createdAt, '%m') = :month")
     List<Integer> findAllByMonth(@Param("memberId") Long memberId, @Param("month") int month);
+
     List<Review> findAllByCodeLineNumberOrderByCreatedAtAsc(Integer codeLineNumber);
+
+    boolean existsById(Long id);
 }
