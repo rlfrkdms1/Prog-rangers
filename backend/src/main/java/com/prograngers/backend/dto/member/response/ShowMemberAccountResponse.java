774e35fa65886291b4f72674eebba124f0e4a705
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-06 18:07:12 +0900

    fix: 추상 클래스 빌더 수정
    추상 클래스는 빌더 사용할 수 없고 super로 생성자 호출도 안된다고 해서 수정

--- backend/src/main/java/com/prograngers/backend/dto/member/response/ShowMemberAccountResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/member/response/ShowMemberAccountResponse.java
@@ -3,13 +3,12 @@ package com.prograngers.backend.dto.member.response;
 import com.prograngers.backend.entity.member.MemberType;
 import lombok.Builder;
 
-@Builder
 public abstract class ShowMemberAccountResponse {
 
-    private MemberType type;
-    private String nickname;
-    private String email;
-    private String github;
-    private String introduction;
-    private String photo;
+    protected MemberType type;
+    protected String nickname;
+    protected String email;
+    protected String github;
+    protected String introduction;
+    protected String photo;
 }
--- backend/src/main/java/com/prograngers/backend/dto/member/response/ShowSocialMemberAccountResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/member/response/ShowSocialMemberAccountResponse.java
@@ -8,8 +8,14 @@ import lombok.Builder;
 public class ShowSocialMemberAccountResponse extends ShowMemberAccountResponse {
 
     @Builder
-    ShowSocialMemberAccountResponse(MemberType type, String nickname, String email, String github, String introduction, String photo) {
-        super(type, nickname, email, github, introduction, photo);
+    ShowSocialMemberAccountResponse(MemberType type, String nickname, String email, String github, String introduction,
+                                    String photo) {
+        this.type = type;
+        this.nickname = nickname;
+        this.email = email;
+        this.github = github;
+        this.introduction = introduction;
+        this.photo = photo;
     }
 
     public static ShowSocialMemberAccountResponse from(Member member) {
