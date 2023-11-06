b3834e2679f7643e16d569b90ddbcb478f7d7413
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-07 03:16:13 +0900

    refactor: Builder가 아닌 생성자로 수정

--- backend/src/main/java/com/prograngers/backend/dto/member/response/ShowMemberAccountResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/member/response/ShowMemberAccountResponse.java
@@ -3,7 +3,6 @@ package com.prograngers.backend.dto.member.response;
 import com.prograngers.backend.entity.member.MemberType;
 import lombok.Builder;
 
-@Builder
 public abstract class ShowMemberAccountResponse {
 
     private MemberType type;
@@ -12,4 +11,13 @@ public abstract class ShowMemberAccountResponse {
     private String github;
     private String introduction;
     private String photo;
+
+    public ShowMemberAccountResponse(MemberType type, String nickname, String email, String github, String introduction, String photo) {
+        this.type = type;
+        this.nickname = nickname;
+        this.email = email;
+        this.github = github;
+        this.introduction = introduction;
+        this.photo = photo;
+    }
 }
