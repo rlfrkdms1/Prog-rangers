946b3ec7b53bd0d343587fce98fc32c2d6291ebb
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-20 15:33:12 +0900

    fix: UpdateReviewRequest 기본 생성자 추가
    리뷰 생성이 안되는 버그 수정
    - 기본 생성자가 없어서 bad request였기 때문에 기본 생성자 추가

--- backend/src/main/java/com/prograngers/backend/dto/review/request/UpdateReviewRequest.java
+++ backend/src/main/java/com/prograngers/backend/dto/review/request/UpdateReviewRequest.java
@@ -3,12 +3,14 @@ package com.prograngers.backend.dto.review.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateReviewRequest {
     @NotBlank(message = "내용은 빈칸일 수 없습니다")
-    String  content;
+    String content;
 }
