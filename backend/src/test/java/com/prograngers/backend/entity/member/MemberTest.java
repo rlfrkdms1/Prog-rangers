8b5f5f8d53fa9a13dfbe090c81c99e2fe6b01cc7
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-23 18:44:53 +0900

    test: 회원 탈퇴 테스트와 회원 탈퇴 시 `탈퇴한 사용자` 표기 테스트 추가
    회원 탈퇴 관련 테스트 추가
    - 회원 탈퇴가 정상적으로 되는지 테스트
    - 탈퇴한 회원이면 `탈퇴한 사용자`로 닉네임을 반환하는지 테스트
    
    related to: #210

---
+++ backend/src/test/java/com/prograngers/backend/entity/member/MemberTest.java
@@ -0,0 +1,28 @@
+package com.prograngers.backend.entity.member;
+
+
+import static com.prograngers.backend.support.fixture.MemberFixture.장지담;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MemberTest {
+    private static final String 탈퇴한_사용자 = "탈퇴한 사용자";
+
+    @DisplayName("탈퇴한 회원은 탈퇴한 사용자 라는 이름을 반환한다.")
+    @Test
+    void getNicknameWhenQuitTest() {
+        Member quit = 장지담.기본_정보_생성();
+        quit.delete();
+        Assertions.assertThat(quit.getNickname()).isEqualTo(탈퇴한_사용자);
+    }
+
+    @DisplayName("회원을 탈퇴시킬 수 있다.")
+    @Test
+    void quitTest() {
+        Member quit = 장지담.기본_정보_생성();
+        quit.delete();
+        Assertions.assertThat(quit.isUsable()).isEqualTo(false);
+    }
+}
