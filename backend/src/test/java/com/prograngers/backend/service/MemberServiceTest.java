1ff93cac9a7475f72af202dc7e4d6dbef7455303
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-30 00:11:49 +0900

    test: 계정정보 조회시 소셜 회원가입을 한 회원일 경우 ShowSocialMemberAccountResponse 반환하는 테스트

--- backend/src/test/java/com/prograngers/backend/service/MemberServiceTest.java
+++ backend/src/test/java/com/prograngers/backend/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package com.prograngers.backend.service;
 
 import com.prograngers.backend.dto.member.response.ShowBasicMemberAccountResponse;
 import com.prograngers.backend.dto.member.response.ShowMemberAccountResponse;
+import com.prograngers.backend.dto.member.response.ShowSocialMemberAccountResponse;
 import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.repository.badge.BadgeRepository;
 import com.prograngers.backend.repository.follow.FollowRepository;
@@ -17,6 +18,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static com.prograngers.backend.support.fixture.MemberFixture.길가은;
+import static com.prograngers.backend.support.fixture.MemberFixture.장지담;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.BDDMockito.given;
@@ -51,6 +53,24 @@ class MemberServiceTest {
 
         assertAll(
                 () -> assertThat(memberAccount).isExactlyInstanceOf(ShowBasicMemberAccountResponse.class),
+                () -> verify(memberRepository).findById(member.getId())
+        );
+    }
+
+    @Test
+    void 회원의_로그인_타입에_따라_계정_정보_조회시_반환되는_DTO가_다르다_SOCIAL() {
+        Member member = 장지담.기본_정보_생성(1L);
+        String encodedPassword = "encoded";
+
+        given(memberRepository.findById(member.getId())).willReturn(Optional.of(member));
+        ShowMemberAccountResponse memberAccount;
+        try (MockedStatic<Encrypt> encryptMockedStatic = mockStatic(Encrypt.class)) {
+            encryptMockedStatic.when(() -> Encrypt.decoding(member.getPassword())).thenReturn(encodedPassword);
+            memberAccount = memberService.getMemberAccount(member.getId());
+        }
+
+        assertAll(
+                () -> assertThat(memberAccount).isExactlyInstanceOf(ShowSocialMemberAccountResponse.class),
                 () -> verify(memberRepository).findById(member.getId())
         );
     }
