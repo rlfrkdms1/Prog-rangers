f9b98545aad343f4b221866c93bb3f508f60b676
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-30 00:16:10 +0900

    test: 계정정보 조회시 존재하지 않는 회원인 경우 예외 반환 테스트

--- backend/src/test/java/com/prograngers/backend/service/MemberServiceTest.java
+++ backend/src/test/java/com/prograngers/backend/service/MemberServiceTest.java
@@ -1,9 +1,12 @@
 package com.prograngers.backend.service;
 
+import com.prograngers.backend.dto.auth.request.SignUpRequest;
 import com.prograngers.backend.dto.member.response.ShowBasicMemberAccountResponse;
 import com.prograngers.backend.dto.member.response.ShowMemberAccountResponse;
 import com.prograngers.backend.dto.member.response.ShowSocialMemberAccountResponse;
 import com.prograngers.backend.entity.member.Member;
+import com.prograngers.backend.exception.notfound.MemberNotFoundException;
+import com.prograngers.backend.exception.unauthorization.AlreadyExistNicknameException;
 import com.prograngers.backend.repository.badge.BadgeRepository;
 import com.prograngers.backend.repository.follow.FollowRepository;
 import com.prograngers.backend.repository.member.MemberRepository;
@@ -15,10 +18,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.util.Optional;
 
 import static com.prograngers.backend.support.fixture.MemberFixture.길가은;
 import static com.prograngers.backend.support.fixture.MemberFixture.장지담;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.BDDMockito.given;
@@ -27,14 +32,16 @@ import static org.mockito.Mockito.verify;
 
 
 @ExtendWith(MockitoExtension.class)
-
 class MemberServiceTest {
 
     @Mock
     private MemberRepository memberRepository;
-    @Mock private BadgeRepository badgeRepository;
-    @Mock private SolutionRepository solutionRepository;
-    @Mock private FollowRepository followRepository;
+    @Mock
+    private BadgeRepository badgeRepository;
+    @Mock
+    private SolutionRepository solutionRepository;
+    @Mock
+    private FollowRepository followRepository;
 
     @InjectMocks
     private MemberService memberService;
@@ -71,6 +78,19 @@ class MemberServiceTest {
 
         assertAll(
                 () -> assertThat(memberAccount).isExactlyInstanceOf(ShowSocialMemberAccountResponse.class),
+                () -> verify(memberRepository).findById(member.getId())
+        );
+    }
+
+    @Test
+    void 계정정보_조회시_존재하지_않는_회원인_경우_예외를_반환한다() {
+        Member member = 길가은.기본_정보_생성(1L);
+
+        given(memberRepository.findById(member.getId())).willReturn(Optional.empty());
+
+        assertAll(
+                () -> assertThatThrownBy(() -> memberService.getMemberAccount(member.getId()))
+                        .isExactlyInstanceOf(MemberNotFoundException.class),
                 () -> verify(memberRepository).findById(member.getId())
         );
     }
