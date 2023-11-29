3d24671a1e5933b33a68ea4ee2cb3e17638126ec
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-29 22:39:41 +0900

    test(AlgorithmConstantTest): view->AlgorithmConstant 변환 실패 테스트
    변환 메서드 from 테스트
    - 잘못된 뷰에 대해 `AlgorithmNotFoundException` 예외를 던짐
    
    related to: #222

--- backend/src/test/java/com/prograngers/backend/entity/solution/AlgorithmConstantTest.java
+++ backend/src/test/java/com/prograngers/backend/entity/solution/AlgorithmConstantTest.java
@@ -2,9 +2,12 @@ package com.prograngers.backend.entity.solution;
 
 import static com.prograngers.backend.entity.solution.AlgorithmConstant.from;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.prograngers.backend.exception.enumtype.AlgorithmNotFoundException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AlgorithmConstantTest {
 
@@ -12,5 +15,11 @@ class AlgorithmConstantTest {
     @CsvSource({"버블 정렬,BUBBLE_SORT", "선택 정렬,SELECTION_SORT", "삽입 정렬,INSERTION_SORT"})
     void 주어진_뷰에_알맞은_상수를_얻을_수_있다(String view, AlgorithmConstant constant) {
         assertThat(from(view)).isEqualTo(constant);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"바보 정렬", "", "    "})
+    void 잘못된_뷰에_대해_예외가_발생한다(String view) {
+        assertThatThrownBy(() -> from(view)).isInstanceOf(AlgorithmNotFoundException.class);
     }
 }
