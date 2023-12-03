210c91cf21b4d73062cee33b252560ad5c540841
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-12-03 21:36:13 +0900

    test(SolutionTest): 수정하려는 제목이 null이면 제목이 수정되지 않는지 테스트
    related to: #222

--- backend/src/test/java/com/prograngers/backend/entity/solution/SolutionTest.java
+++ backend/src/test/java/com/prograngers/backend/entity/solution/SolutionTest.java
@@ -1,6 +1,8 @@
 package com.prograngers.backend.entity.solution;
 
+import static com.prograngers.backend.entity.solution.AlgorithmConstant.BELLMAN_FORD;
 import static com.prograngers.backend.entity.solution.AlgorithmConstant.DFS;
+import static com.prograngers.backend.entity.solution.DataStructureConstant.HEAP;
 import static com.prograngers.backend.entity.solution.DataStructureConstant.QUEUE;
 import static com.prograngers.backend.entity.solution.LanguageConstant.JAVA;
 import static com.prograngers.backend.support.fixture.MemberFixture.장지담;
@@ -17,6 +19,17 @@ import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
+    private static final String TITLE = "풀이제목";
+    private static final String CODE = "import\nmain\nhello world\nreturn";
+    private static final String DESCRIPTION = "풀이설명";
+
+    final static String UPDATED_TITLE = "수정제목";
+    final static AlgorithmConstant UPDATED_ALGORITHM = DFS;
+    final static DataStructureConstant UPDATED_DATASTRUCTURE = QUEUE;
+    final static int UPDATED_LEVEL = 2;
+    final static String UPDATED_CODE = "수정코드";
+    final static String UPDATED_DESCRIPTION = "수정설명";
+
     private Solution solution;
     private Member member;
     private Problem problem;
@@ -25,28 +38,38 @@ class SolutionTest {
     void beforeEach() {
         member = 장지담.기본_정보_생성();
         problem = 백준_문제.기본_정보_생성();
-        solution = 공개_풀이.기본_정보_생성(problem, member, LocalDateTime.now(), JAVA, 1);
+        solution = 공개_풀이.알고리즘_자료구조_지정_생성(problem, member, LocalDateTime.now(), JAVA, 1, BELLMAN_FORD, HEAP);
     }
 
     @Test
     void 풀이를_수정_할_수_있다() {
-        // given
-        final String title = "수정제목";
-        final AlgorithmConstant algorithm = DFS;
-        final DataStructureConstant dataStructure = QUEUE;
-        final int level = 2;
-        final String code = "수정코드";
-        final String description = "수정설명";
         // when
-        solution.update("수정제목", algorithm, dataStructure, level, "수정코드", "수정설명");
+        solution.update(UPDATED_TITLE, UPDATED_ALGORITHM, UPDATED_DATASTRUCTURE, UPDATED_LEVEL, UPDATED_CODE, "수정설명");
         // then
         Assertions.assertAll(
-                () -> assertThat(solution.getTitle()).isEqualTo(title),
-                () -> assertThat(solution.getAlgorithm()).isEqualTo(algorithm),
-                () -> assertThat(solution.getDataStructure()).isEqualTo(dataStructure),
-                () -> assertThat(solution.getLevel()).isEqualTo(level),
-                () -> assertThat(solution.getCode()).isEqualTo(code),
-                () -> assertThat(solution.getDescription()).isEqualTo(description)
+                () -> assertThat(solution.getTitle()).isEqualTo(UPDATED_TITLE),
+                () -> assertThat(solution.getAlgorithm()).isEqualTo(UPDATED_ALGORITHM),
+                () -> assertThat(solution.getDataStructure()).isEqualTo(UPDATED_DATASTRUCTURE),
+                () -> assertThat(solution.getLevel()).isEqualTo(UPDATED_LEVEL),
+                () -> assertThat(solution.getCode()).isEqualTo(UPDATED_CODE),
+                () -> assertThat(solution.getDescription()).isEqualTo(UPDATED_DESCRIPTION)
         );
     }
+
+    @Test
+    void 수정하려는_제목이_null이면_제목이_수정되지_않는다() {
+        // when
+        solution.update(null, UPDATED_ALGORITHM, UPDATED_DATASTRUCTURE, UPDATED_LEVEL, UPDATED_CODE, "수정설명");
+        // then
+        Assertions.assertAll(
+                () -> assertThat(solution.getTitle()).isEqualTo(TITLE),
+                () -> assertThat(solution.getAlgorithm()).isEqualTo(UPDATED_ALGORITHM),
+                () -> assertThat(solution.getDataStructure()).isEqualTo(UPDATED_DATASTRUCTURE),
+                () -> assertThat(solution.getLevel()).isEqualTo(UPDATED_LEVEL),
+                () -> assertThat(solution.getCode()).isEqualTo(UPDATED_CODE),
+                () -> assertThat(solution.getDescription()).isEqualTo(UPDATED_DESCRIPTION)
+        );
+    }
+
+
 }
