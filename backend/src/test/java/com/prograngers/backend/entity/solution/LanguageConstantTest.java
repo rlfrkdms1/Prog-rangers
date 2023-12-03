0326222e28268baf58b665453ff97cdd479fe956
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-12-03 20:54:59 +0900

    test: LanguageConstantTest 추가
    부적절한 뷰에 대해 예외가 발생하는 지 테스트 추가
    
    related to: #222

--- backend/src/test/java/com/prograngers/backend/entity/solution/LanguageConstantTest.java
+++ backend/src/test/java/com/prograngers/backend/entity/solution/LanguageConstantTest.java
@@ -1,9 +1,12 @@
 package com.prograngers.backend.entity.solution;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.prograngers.backend.exception.enumtype.LanguageNotFoundException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LanguageConstantTest {
     @ParameterizedTest
@@ -12,4 +15,9 @@ class LanguageConstantTest {
         assertThat(LanguageConstant.from(view)).isEqualTo(languageConstant);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"pytorch", "", "  "})
+    void 잘못된_view에_대해_예외가_발생한다(String wrong) {
+        assertThatThrownBy(() -> LanguageConstant.from(wrong)).isInstanceOf(LanguageNotFoundException.class);
+    }
 }
