b9076e9e8f9130021f2384366b9c3d0dd59160cf
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-12-07 15:56:57 +0900

    refactor(CommentTest): 댓글 도메인 테스트 리팩터링
    기존 테스트는 가독성이 떨어져서 수정
    - 기존에는 CommentFixture의 댓글 내용을 상수로 CommentTest로 가져옴
    - 수정한 댓글 내용, 삭제한 댓글 내용도 CommentTest에서 상수로 지정하고 사용함
    - 지정해놓은 상수가 실제 픽스처에서 사용되는지 확인하기 위해 픽스처로 들어가야 하고 사용된다는 것을 유추해야 함
    - 따라서 픽스처의 수정된 댓글, 삭제된 댓글을 활용해 테스트코드의 given 부분을 작성하고 이를 활용해서 then 부분에서 비교함
    
    related to: #222

--- backend/src/test/java/com/prograngers/backend/entity/comment/CommentTest.java
+++ backend/src/test/java/com/prograngers/backend/entity/comment/CommentTest.java
@@ -1,10 +1,10 @@
 package com.prograngers.backend.entity.comment;
 
 import static com.prograngers.backend.entity.comment.CommentStatusConstant.CREATED;
-import static com.prograngers.backend.entity.comment.CommentStatusConstant.DELETED;
-import static com.prograngers.backend.entity.comment.CommentStatusConstant.FIXED;
 import static com.prograngers.backend.entity.solution.LanguageConstant.JAVA;
+import static com.prograngers.backend.support.fixture.CommentFixture.삭제된_댓글;
 import static com.prograngers.backend.support.fixture.CommentFixture.생성된_댓글;
+import static com.prograngers.backend.support.fixture.CommentFixture.수정된_댓글;
 import static com.prograngers.backend.support.fixture.MemberFixture.장지담;
 import static com.prograngers.backend.support.fixture.ProblemFixture.백준_문제;
 import static com.prograngers.backend.support.fixture.SolutionFixture.공개_풀이;
@@ -22,10 +22,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CommentTest {
-    private static final String FIXED_CONTENT = "수정한 댓글입니다.";
-    private static final String DELETED_CONTENT = "삭제된 댓글입니다";
-
-    private static final String CONTENT = "댓글내용";
     private Comment comment;
     private Solution solution;
     private Member member;
@@ -42,50 +38,62 @@ class CommentTest {
     @DisplayName("댓글을 수정할 수 있다.")
     @Test
     void updateTest() {
+        // given
+        Comment updated = 수정된_댓글.기본_정보_생성(member, solution, LocalDateTime.now());
+
         // when
-        comment.update(FIXED_CONTENT);
+        comment.update(updated.getContent());
 
         // then
         assertAll(
-                () -> assertThat(comment.getContent()).isEqualTo(FIXED_CONTENT),
-                () -> assertThat(comment.getStatus()).isEqualTo(FIXED)
+                () -> assertThat(comment.getContent()).isEqualTo(updated.getContent()),
+                () -> assertThat(comment.getStatus()).isEqualTo(updated.getStatus())
         );
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", "    "})
     void 수정하려는_내용이_blank일_경우_수정할_수_없다(String content) {
+        // given
+        Comment expected = 생성된_댓글.기본_정보_생성(member, solution, LocalDateTime.now());
+
         // when
         comment.update(content);
 
         // then
         assertAll(
-                () -> assertThat(comment.getContent()).isEqualTo(CONTENT),
+                () -> assertThat(comment.getContent()).isEqualTo(expected.getContent()),
                 () -> assertThat(comment.getStatus()).isEqualTo(CREATED)
         );
     }
 
     @Test
     void 수정하려는_내용이_null일_경우_수정할_수_없다() {
+        // given
+        Comment expected = 생성된_댓글.기본_정보_생성(member, solution, LocalDateTime.now());
+
         // when
         comment.update(null);
 
         // then
         assertAll(
-                () -> assertThat(comment.getContent()).isEqualTo(CONTENT),
-                () -> assertThat(comment.getStatus()).isEqualTo(CREATED)
+                () -> assertThat(comment.getContent()).isEqualTo(expected.getContent()),
+                () -> assertThat(comment.getStatus()).isEqualTo(expected.getStatus())
         );
     }
 
     @Test
     void 댓글을_삭제할_수_있다() {
+        // given
+        Comment deleted = 삭제된_댓글.기본_정보_생성(member, solution, LocalDateTime.now());
+
         // when
         comment.delete();
 
         // then
         assertAll(
-                () -> assertThat(comment.getContent()).isEqualTo(DELETED_CONTENT),
-                () -> assertThat(comment.getStatus()).isEqualTo(DELETED)
+                () -> assertThat(comment.getContent()).isEqualTo(deleted.getContent()),
+                () -> assertThat(comment.getStatus()).isEqualTo(deleted.getStatus())
         );
     }
 }
