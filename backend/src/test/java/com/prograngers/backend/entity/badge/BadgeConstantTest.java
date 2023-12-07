19d2bd1044f79375479afcfd404c997640979720
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-12-07 15:04:29 +0900

    refactor(BadgeConstantTest): 변경에 취약한 테스트 수정
    BadgeConstantTest 수정
    - 뱃지에서 getCount를 통해 값을 가져오는 방식으로 변경
    - 뱃지의 값이 변경되어도 테스트는 통과함
    
    related to: #222

--- backend/src/test/java/com/prograngers/backend/entity/badge/BadgeConstantTest.java
+++ backend/src/test/java/com/prograngers/backend/entity/badge/BadgeConstantTest.java
@@ -1,36 +1,25 @@
 package com.prograngers.backend.entity.badge;
 
-import static com.prograngers.backend.entity.badge.BadgeConstant.꽃;
-import static com.prograngers.backend.entity.badge.BadgeConstant.꽃봉오리;
-import static com.prograngers.backend.entity.badge.BadgeConstant.새싹;
-import static com.prograngers.backend.entity.badge.BadgeConstant.잎과줄기;
-import static com.prograngers.backend.entity.badge.BadgeConstant.화단;
+import static java.lang.Math.random;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BadgeConstantTest {
 
     @DisplayName("작성한 풀이 갯수에 알맞게 뱃지를 얻을 수 있다.")
-    @Test
-    void getBadgeTest() {
-        assertAll(
-                () -> assertThat(BadgeConstant.getBadge(1L)).isEqualTo(새싹),
-                () -> assertThat(BadgeConstant.getBadge(10L)).isEqualTo(잎과줄기),
-                () -> assertThat(BadgeConstant.getBadge(50L)).isEqualTo(꽃봉오리),
-                () -> assertThat(BadgeConstant.getBadge(100L)).isEqualTo(꽃),
-                () -> assertThat(BadgeConstant.getBadge(150L)).isEqualTo(화단)
-        );
+    @ParameterizedTest
+    @CsvSource({"새싹", "잎과줄기", "꽃봉오리", "꽃", "화단"})
+    void getBadgeTest(BadgeConstant badgeConstant) {
+        assertThat(BadgeConstant.getBadge(badgeConstant.getCount())).isEqualTo(badgeConstant);
     }
 
     @DisplayName("뱃지를 얻는 풀이 개수에 해당되지 않으면 null을 반환한다")
     @ParameterizedTest
-    @ValueSource(longs = {0L, 9L, 49L, 99L, 149L})
-    void cantGetBadgeTest(Long solutionCount) {
-        assertThat(BadgeConstant.getBadge(solutionCount)).isEqualTo(null);
+    @CsvSource({"새싹", "잎과줄기", "꽃봉오리", "꽃", "화단"})
+    void cantGetBadgeTest(BadgeConstant badgeConstant) {
+        assertThat(BadgeConstant.getBadge(badgeConstant.getCount() + (int) random() * 5 + 1)).isEqualTo(null);
     }
 }
