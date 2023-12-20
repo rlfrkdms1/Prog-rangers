49a767294e94ba83107d01b4f6a5796e01cdbf43
ddongguri-bing <ckdalssnsk@naver.com>
2023-12-20 14:34:22 +0900

    fix : 로그인과 회원가입의 주소 변경
    로그인과 회원가입의 api_end_point가 이전 주소로 설정되어 있어 변경
    
    related to : #244

--- frontend/Prog-rangers-main/src/apis/singup.js
+++ frontend/Prog-rangers-main/src/apis/singup.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 
-export const API_END_POINT = 'http://13.124.131.171:8080/api/v1'
+export const API_END_POINT =
+  'http://13.125.13.131:8080/api/v1';
 
 // TODO response 처리 로직
-export const signup= async (data)=>{
-    const response = await axios.post(`${API_END_POINT}/sign-up`,{...data});
-    console.log(response)
-}
+export const signup = async (data) => {
+  const response = await axios.post(
+    `${API_END_POINT}/sign-up`,
+    { ...data }
+  );
+  console.log(response);
+};
 
 export const checkNicknameDuplication = async (params) => {
-    await axios.get(`${API_END_POINT}/auth?nickname=${params.nickname}`, { params });
-  };  
+  await axios.get(
+    `${API_END_POINT}/auth?nickname=${params.nickname}`,
+    { params }
+  );
+};
