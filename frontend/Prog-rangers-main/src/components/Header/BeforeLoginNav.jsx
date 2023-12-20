d5df0b6c65b341c73a47cdee5a70dbd3670f998a
ddongguri-bing <ckdalssnsk@naver.com>
2023-12-20 14:06:46 +0900

    fix : 헤더 메뉴 수정
    로그인 전 헤더 메뉴의 풀이 보러가기를 문제 보러가기로 변경
    
    related to : #244

--- frontend/Prog-rangers-main/src/components/Header/BeforeLoginNav.jsx
+++ frontend/Prog-rangers-main/src/components/Header/BeforeLoginNav.jsx
@@ -1,22 +1,11 @@
-import {
-  navStyle,
-  StyledLink
-} from './headerStyle';
+import { navStyle, StyledLink } from './headerStyle';
 
 export const BeforeLoginNav = () => {
-
   return (
     <div className="BeforeLoginNav" css={navStyle}>
-      <StyledLink to="/problems">
-        풀이 보러가기
-      </StyledLink>
-      <StyledLink to="login" >
-        로그인
-      </StyledLink>
-      <StyledLink to="signUp" >
-        회원가입
-      </StyledLink>
+      <StyledLink to="/problems">문제 보러가기</StyledLink>
+      <StyledLink to="login">로그인</StyledLink>
+      <StyledLink to="signUp">회원가입</StyledLink>
     </div>
   );
 };
-
