d98134a105b7ed6e88044cd2f2a34ca1b39c0551
ddongguri-bing <ckdalssnsk@naver.com>
2023-12-20 15:08:51 +0900

    fix : components 디렉토리에 있는 ip 주소 변경
    components 디렉토리에 있는 파일에 이전 ip 주소를 최신의 주소로 변경
    
    related to : #244

--- frontend/Prog-rangers-main/src/components/MySolutionDetail/RightBar.jsx
+++ frontend/Prog-rangers-main/src/components/MySolutionDetail/RightBar.jsx
@@ -7,85 +7,94 @@ import { css } from '@emotion/react';
 import { theme } from '../Header/theme';
 
 export const RightBar = () => {
+  const navigate = useNavigate();
+  const onClickSols = (solutionId) => {
+    navigate(`/mySolution/${solutionId}`);
+  };
 
-    const navigate = useNavigate();
-    const onClickSols = (solutionId) => {
-      navigate(`/mySolution/${solutionId}`); 
-    };
+  const navigate1 = useNavigate();
+  const onClickScrape = (solutionId) => {
+    navigate1(`/solutions/${solutionId}`);
+  };
 
-    const navigate1 = useNavigate();
-    const onClickScrape = (solutionId) => {
-      navigate1(`/solutions/${solutionId}`); 
-    };
+  const navigate2 = useNavigate();
+  const onClickAddSol = () => {
+    navigate2(`/myPage/addsolution`);
+  };
 
-    const navigate2 = useNavigate();
-    const onClickAddSol = () => {
-      navigate2(`/myPage/addsolution`); 
-    };
+  const { solutionId } = useParams();
+  const [MySolList, setMySolList] = useState([]);
+  const [MyScrapeList, setScrapeList] = useState([]);
 
-    const { solutionId } = useParams();
-    const [ MySolList, setMySolList ] = useState([]);
-    const [ MyScrapeList, setScrapeList ] = useState([]);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const apiUrl = `http://13.125.13.131:8080/api/v1/solutions/${solutionId}`;
 
-    useEffect(() => {
-
-      const token = localStorage.getItem('token');
-      const apiUrl = `http://13.124.131.171:8080/api/v1/solutions/${solutionId}`;
-
-      axios
-        .get(apiUrl, {
-          method: "GET",
-          headers: {Authorization: `Bearer ${token}`}
-        })
-        .then((response) => {
-          setMySolList(response.data.mySolutionList);
-          setScrapeList(response.data.myScrapSolutionList);
-        })
-        .catch((error) => {
-          console.error('API 요청 오류:', error);
-        });
-    }, []);
+    axios
+      .get(apiUrl, {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        setMySolList(response.data.mySolutionList);
+        setScrapeList(response.data.myScrapSolutionList);
+      })
+      .catch((error) => {
+        console.error('API 요청 오류:', error);
+      });
+  }, []);
 
   return (
     <div className="RightbarWarp">
-      <div css={css`
-        height: 100%;
-        alignItems: stretch;
-        border-left: 1px solid ${theme.colors.light3};
-      `}>
-        <div css={css`
-           padding-top: 220px;
-           margin-left: 20px;
-           font-size: 16px;
-           `}>
-            <div css={css`
-            font-weight: 700;
-            padding-bottom: 10px;
-            `}> 
-             풀이목록 
-            </div>
-            
-
-            <button 
+      <div
+        css={css`
+          height: 100%;
+          alignitems: stretch;
+          border-left: 1px solid ${theme.colors.light3};
+        `}
+      >
+        <div
+          css={css`
+            padding-top: 220px;
+            margin-left: 20px;
+            font-size: 16px;
+          `}
+        >
+          <div
             css={css`
-            width: 170px;
-            height: 40px;
-            border-radius: 20px;
-            margin-top: 30px;
-            text-align: center;
-            background-color: ${theme.colors.main30}
+              font-weight: 700;
+              padding-bottom: 10px;
+            `}
+          >
+            풀이목록
+          </div>
+
+          <button
+            css={css`
+              width: 170px;
+              height: 40px;
+              border-radius: 20px;
+              margin-top: 30px;
+              text-align: center;
+              background-color: ${theme.colors.main30};
             `}
             onClick={() => onClickAddSol()}
-            > 풀이 추가하기</button>
-            
-            <div css={css`
-            font-weight: 700;
-            margin-top: 100px;
-            padding-bottom: 10px;
-            `}> 
-                스크랩한 풀이
-            </div>
-        </div>        
-      </div>      
+          >
+            {' '}
+            풀이 추가하기
+          </button>
+
+          <div
+            css={css`
+              font-weight: 700;
+              margin-top: 100px;
+              padding-bottom: 10px;
+            `}
+          >
+            스크랩한 풀이
+          </div>
+        </div>
+      </div>
     </div>
-  )}
+  );
+};
--- frontend/Prog-rangers-main/src/components/SolutionDetail/SolutionDetailHeader.jsx
+++ frontend/Prog-rangers-main/src/components/SolutionDetail/SolutionDetailHeader.jsx
@@ -2,33 +2,29 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-
 import { css } from '@emotion/react';
 import { theme } from '../Header/theme';
-
-import { BiSolidLockAlt } from 'react-icons/bi';
-import { BiSolidLockOpenAlt } from 'react-icons/bi';
-
+// import { BiSolidLockAlt } from 'react-icons/bi';
+// import { BiSolidLockOpenAlt } from 'react-icons/bi';
 import {
   HeaderLayout,
   colFlex,
   dot,
-  rowFlex
+  rowFlex,
 } from './headerStyle';
 
 export const SolutionDetailHeader = () => {
-
   const navigate = useNavigate();
-    const onClickName = (nickname) => {
-      navigate(`/profile/${nickname}`); 
-    };
-  
+  const onClickName = (nickname) => {
+    navigate(`/profile/${nickname}`);
+  };
+
   const { solutionId } = useParams();
   const [problem, setProblem] = useState({});
   const [solution, setSolution] = useState({});
 
   useEffect(() => {
-    const apiUrl = `http://13.124.131.171:8080/api/v1/solutions/${solutionId}`;
+    const apiUrl = `http://13.125.13.131:8080/api/v1/solutions/${solutionId}`;
 
     axios
       .get(apiUrl)
@@ -38,7 +34,7 @@ export const SolutionDetailHeader = () => {
       })
       .catch((error) => {
         console.error('API 요청 오류:', error);
-        alert("비공개 풀이를 열람할 수 없습니다.");
+        alert('비공개 풀이를 열람할 수 없습니다.');
         navigate(-1);
       });
   }, []);
@@ -50,22 +46,23 @@ export const SolutionDetailHeader = () => {
           <div className="headerLeft">
             <div
               className="problemTitleArea rowFlex"
-              css={css`${rowFlex}
-                  gap : 10px;
-                  `}>
+              css={css`
+                ${rowFlex}
+                gap : 10px;
+              `}
+            >
               <div
                 className="problemTitle"
                 css={css`
                   font-size: 20px;
                   font-weight: bold;
                   color: ${theme.colors.dark1};
-
                 `}
               >
                 {problem.title}
               </div>
               <div className="icon">
-              {/* {isPublic ? <BiSolidLockAlt size="18" color="#D9D9D9" /> : <BiSolidLockOpenAlt size="18" color="#D9D9D9" />} */}
+                {/* {isPublic ? <BiSolidLockAlt size="18" color="#D9D9D9" /> : <BiSolidLockOpenAlt size="18" color="#D9D9D9" />} */}
               </div>
             </div>
             <div
@@ -86,7 +83,9 @@ export const SolutionDetailHeader = () => {
                 css={css`
                   color: ${theme.colors.light1};
                 `}
-                onClick={() => onClickName(solution.nickname)} 
+                onClick={() =>
+                  onClickName(solution.nickname)
+                }
               >
                 {solution.nickname}
               </span>
@@ -101,38 +100,43 @@ export const SolutionDetailHeader = () => {
                 text-align: center;
                 line-height: 36px;
                 color: ${theme.colors.dark1};
-                ${solution.algorithm === null ? 'display: none;' : ''}
+                ${solution.algorithm === null
+                  ? 'display: none;'
+                  : ''}
               `}
             >
               {solution.algorithm}
             </div>
           </div>
           <div css={colFlex}>
-          <div
-            className="headerRight rowFlex"
-            css={rowFlex}
-          >
             <div
-              className="siteName"
-              css={css`
-                background-color: ${problem.ojName === "프로그래머스" ? "#6AB4AC" : "#3578BF"};
-                box-sizing: border-box;
-                padding: 8px 20px;
-                border-radius: 20px;
-                color: ${theme.colors.white};
-              `}
+              className="headerRight rowFlex"
+              css={rowFlex}
             >
-              {problem.ojName}
-            </div>
+              <div
+                className="siteName"
+                css={css`
+                  background-color: ${problem.ojName ===
+                  '프로그래머스'
+                    ? '#6AB4AC'
+                    : '#3578BF'};
+                  box-sizing: border-box;
+                  padding: 8px 20px;
+                  border-radius: 20px;
+                  color: ${theme.colors.white};
+                `}
+              >
+                {problem.ojName}
+              </div>
               <div className="dropMenu" css={colFlex}>
                 <span css={dot}></span>
                 <span css={dot}></span>
                 <span css={dot}></span>
               </div>
+            </div>
           </div>
         </div>
-      </div>
-    )}
-  </>
+      )}
+    </>
   );
 };
