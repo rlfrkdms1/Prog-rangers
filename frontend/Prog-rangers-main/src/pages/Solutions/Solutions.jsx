417401488d8a10d941bd3e15b491c6bdedeb4709
ddongguri-bing <ckdalssnsk@naver.com>
2023-12-20 14:47:05 +0900

    fix : pages 디렉토리 ip 주소 변경
    pages 디렉토리 파일 중 ip 주소가 과거인 것을 최신의 주소로 변경
    
    related to : #244

--- frontend/Prog-rangers-main/src/pages/Solutions/Solutions.jsx
+++ frontend/Prog-rangers-main/src/pages/Solutions/Solutions.jsx
@@ -6,20 +6,20 @@ import { QSolving } from '../../components/Solving';
 import { Pagination } from '../../components/Pagination/Pagination';
 import { useLocation, useParams } from 'react-router-dom';
 import axios from 'axios';
-import{ 
+import {
   fontSize24,
   filterStyle,
-  ojNameTag
+  ojNameTag,
 } from './SolutionsStyle';
 import sort from '../../db/autocomplete.json';
 
 const Solutions = () => {
   const location = useLocation();
-  const [ page, setPage ] = useState(1);
-  const [ Solvings, setSolvings ] = useState([]);
-  const [ totalPages, setTotalPages ] = useState(1);
-  const [ questionTitle, setQuestionTitle ] = useState(""); 
-  const [ ojName, setOjName ] = useState("");
+  const [page, setPage] = useState(1);
+  const [Solvings, setSolvings] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [questionTitle, setQuestionTitle] = useState('');
+  const [ojName, setOjName] = useState('');
   const currentPath = window.location.pathname;
   const { problemId } = useParams();
 
@@ -27,64 +27,96 @@ const Solutions = () => {
     setPage(page);
   };
 
-  const WantSolutions = async() => {
-    const response = await axios.get(`http://13.124.131.171:8080/api/v1/problems/${problemId}/solutions?page=${page-1}&size=5`);
+  const WantSolutions = async () => {
+    const response = await axios.get(
+      `http://13.125.13.131:8080/api/v1/problems/${problemId}/solutions?page=${
+        page - 1
+      }&size=5`
+    );
     setTotalPages(response.data.totalPages);
     setSolvings(response.data.solutions);
     setQuestionTitle(response.data.problemName);
     setOjName(response.data.ojName);
-  }
+  };
 
   useEffect(() => {
     WantSolutions();
   }, [page]);
-  
-  return( 
-    <div css={css`
-      display: flex; 
-      justify-content: center;
-    `}>
-      <div css={css`
+
+  return (
+    <div
+      css={css`
+        display: flex;
+        justify-content: center;
+      `}
+    >
+      <div
+        css={css`
           ${MainBody}
-        `}>
-        <div css={css`${fontSize24}`}>
+        `}
+      >
+        <div
+          css={css`
+            ${fontSize24}
+          `}
+        >
           원하는 조건으로 풀이를 찾아보세요
         </div>
-        <div className="filter"
-          css={css`${filterStyle}`}>
-              <FilterBar options={sort.LANGUAGE}/>
-              <FilterBar options={sort.ALGORITHM}/>
-              <FilterBar options={sort.DATASTRUCTURE}/>
-              <FilterBar options={sort.SORT}/>
+        <div
+          className="filter"
+          css={css`
+            ${filterStyle}
+          `}
+        >
+          <FilterBar options={sort.LANGUAGE} />
+          <FilterBar options={sort.ALGORITHM} />
+          <FilterBar options={sort.DATASTRUCTURE} />
+          <FilterBar options={sort.SORT} />
         </div>
-        <div css={css`
-          display: flex;
-          margin-top: 53px;`
-        }>
-          <div css={css`
-            ${fontSize24}
-            margin-right: 20px;
-          `}> 
-            {questionTitle} 
+        <div
+          css={css`
+            display: flex;
+            margin-top: 53px;
+          `}
+        >
+          <div
+            css={css`
+              ${fontSize24}
+              margin-right: 20px;
+            `}
+          >
+            {questionTitle}
           </div>
-          <div css={css`
-            ${ojNameTag}
-            background-color: ${ojName === "프로그래머스" ? "#6AB4AC" : "#3578BF"};
-            color: white;
-          `}> 
+          <div
+            css={css`
+              ${ojNameTag}
+              background-color: ${ojName === '프로그래머스'
+                ? '#6AB4AC'
+                : '#3578BF'};
+              color: white;
+            `}
+          >
             {ojName}
-          </div>  
-        </div>   
-        <div css={css`height: 690px; width: 980px;  margin-top: 20px;`}>
-          <QSolving data={Solvings}/>
+          </div>
         </div>
-        <div css={css`
-            margin-top: 110px; 
-            height: 50px; 
-            display: flex; 
-            justify-content: center; 
-            align-items:center;
-        `}>
+        <div
+          css={css`
+            height: 690px;
+            width: 980px;
+            margin-top: 20px;
+          `}
+        >
+          <QSolving data={Solvings} />
+        </div>
+        <div
+          css={css`
+            margin-top: 110px;
+            height: 50px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          `}
+        >
           <Pagination
             page={page}
             totalPages={totalPages}
