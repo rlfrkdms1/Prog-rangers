a2a52af034040c6412824d8208c87beb942d7f0f
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-12-31 16:34:17 +0900

    feat(SolutionTitleAndIdResponse): equals, hashcode 재정의
    distinct()로 중복제거를 가능하게 하기 위해 equals와 hashcode 재정의
    - id값을 통해 비교하도록 equals 재정의
    - id값으로 해시값을 얻도록 hashCode 재정의
    
    related to: #255

--- backend/src/main/java/com/prograngers/backend/dto/solution/response/SolutionTitleAndIdResponse.java
+++ backend/src/main/java/com/prograngers/backend/dto/solution/response/SolutionTitleAndIdResponse.java
@@ -1,5 +1,6 @@
 package com.prograngers.backend.dto.solution.response;
 
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,23 @@ import lombok.Setter;
 public class SolutionTitleAndIdResponse {
     private String title;
     private Long id;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        SolutionTitleAndIdResponse solutionTitleAndIdResponse = (SolutionTitleAndIdResponse) obj;
+        return Objects.equals(id, solutionTitleAndIdResponse.id);
+    }
 
     public static SolutionTitleAndIdResponse from(String title, Long id) {
         return SolutionTitleAndIdResponse.builder()
