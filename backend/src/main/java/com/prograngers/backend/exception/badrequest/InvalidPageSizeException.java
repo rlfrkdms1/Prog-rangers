68587108b8e71286d43d6852032a09491d0c4907
rlfrkdms1 <rlfrkdms1@naver.com>
2024-01-11 03:48:23 +0900

    refactor: page size가 0이상의 정수인 예외로 변경

--- backend/src/main/java/com/prograngers/backend/exception/badrequest/InvalidPageSizeException.java
+++ backend/src/main/java/com/prograngers/backend/exception/badrequest/InvalidPageSizeException.java
@@ -1,9 +1,9 @@
 package com.prograngers.backend.exception.badrequest;
 
-import static com.prograngers.backend.exception.ErrorCode.PAGE_SIZE_UNDER_THAN_ONE;
+import static com.prograngers.backend.exception.ErrorCode.PAGE_SIZE_MUST_NUMBER_OVER_THAN_ONE;
 
 public class InvalidPageSizeException extends InvalidValueException{
     public InvalidPageSizeException() {
-        super(PAGE_SIZE_UNDER_THAN_ONE, "page의 size는 1이상이어야 합니다.");
+        super(PAGE_SIZE_MUST_NUMBER_OVER_THAN_ONE, "page의 size는 0이상의 숫자형태여야 합니다.");
     }
 }
