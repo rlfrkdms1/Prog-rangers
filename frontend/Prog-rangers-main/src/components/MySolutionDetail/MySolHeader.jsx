db20f0a9437e92d4d75637ecceeb1986da7789a6
yyyujinnn <1209yujin@naver.com>
2024-01-16 01:58:34 +0900

    fix: 풀이 상세보기 태그 짤림 현상 오류 수정

--- frontend/Prog-rangers-main/src/components/MySolutionDetail/MySolHeader.jsx
+++ frontend/Prog-rangers-main/src/components/MySolutionDetail/MySolHeader.jsx
@@ -149,12 +149,10 @@ export const MySolHeader = () => {
               <div
                 className="AlgoOptions"
                 css={css`
-                  width: 89px;
-                  height: 36px;
+                  padding: 8px 20px;
                   background-color: ${theme.colors.light3};
                   border-radius: 20px;
                   text-align: center;
-                  line-height: 36px;
                   color: ${theme.colors.dark1};
                   ${solution.algorithm === null
                     ? 'display: none;'
