ef2ad3fe1b0b273eff7733beca889b0862461ae3
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-01-26 12:57:43 +0900

    fix: comment, review 필드 변경
    불린에서 tinyint로 변경
    
    related to: #299

--- backend/src/main/java/com/prograngers/backend/entity/comment/Comment.java
+++ backend/src/main/java/com/prograngers/backend/entity/comment/Comment.java
@@ -1,15 +1,10 @@
 package com.prograngers.backend.entity.comment;
 
-import static com.prograngers.backend.entity.comment.CommentStatusConstant.DELETED;
-import static com.prograngers.backend.entity.comment.CommentStatusConstant.FIXED;
-
 import com.prograngers.backend.entity.member.Member;
 import com.prograngers.backend.entity.solution.Solution;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -55,7 +50,7 @@ public class Comment {
 
     private Long parentId;
 
-    @Column(nullable = false, columnDefinition = "BOOLEAN")
+    @Column(nullable = false)
     private boolean updated;
 
     public void update(String content) {
